f6b1ece3b7c9ba676a5735e77b9b392f4b1c2720
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2022-11-06 12:01:25 +0530

    Time: 36 ms (39.26%), Space: 11.5 MB (30.09%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -10,31 +10,64 @@
  */
 class Solution {
 public:
-    void reve(vector<int> v, ListNode* head){
-        reverse(v.begin(),v.end());
-        for(auto it: v){
-            head->val = it;
-            head=head->next;   
-        }
-        return;
-    }
+    
+    // my approach
+        
+    // void reve(vector<int> v, ListNode* head){
+    //     reverse(v.begin(),v.end());
+    //     for(auto it: v){
+    //         head->val = it;
+    //         head=head->next;   
+    //     }
+    //     return;
+    // }
+    // ListNode* reverseKGroup(ListNode* head, int k) {
+    //     int i=0;
+    //     ListNode *ans=head,*temp=head;
+    //     vector<int> v;
+    //     while(ans){
+    //         i++;
+    //         v.push_back(ans->val);
+    //         if(i==k){
+    //             // cout<<i<<" "<<ans->val<<" "<<temp->val<<endl;
+    //             reve(v,temp);
+    //             v.clear();
+    //             if(ans->next != NULL)
+    //             temp=ans->next;
+    //             i=0;
+    //         }
+    //         ans=ans->next;
+    //     }
+    //     return head;
+    // }
+    
+    // striver's approach 
     ListNode* reverseKGroup(ListNode* head, int k) {
-        int i=0;
-        ListNode *ans=head,*temp=head;
-        vector<int> v;
-        while(ans){
-            i++;
-            v.push_back(ans->val);
-            if(i==k){
-                cout<<i<<" "<<ans->val<<" "<<temp->val<<endl;
-                reve(v,temp);
-                v.clear();
-                if(ans->next != NULL)
-                temp=ans->next;
-                i=0;
-            }
-            ans=ans->next;
+        if(head == NULL || k==1){
+            return head;
         }
-        return head;
+        ListNode *dummy = new ListNode(0);
+        ListNode *cur=dummy , *nex=dummy , *pre=dummy;
+        dummy->next = head;
+        int count=0;
+        
+        while(cur->next != NULL){
+            cur=cur->next;
+            count++;
+        }
+        
+        while(count>=k){
+            cur = pre->next;
+            nex = cur->next;
+            for(int i=1;i<k;i++){
+                cur->next = nex->next;
+                nex->next = pre->next;
+                pre->next=nex;
+                nex = cur->next;
+            }
+            pre=cur;
+            count-=k;
+        }
+        return dummy->next;
     }
 };
