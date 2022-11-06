e081681853215126d40619126f5599670870bab9
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2022-11-06 12:28:39 +0530

    Time: 8 ms (93.44%), Space: 7.6 MB (75.62%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -8,16 +8,39 @@
  */
 class Solution {
 public:
-    ListNode *detectCycle(ListNode *head) {
-        ListNode* temp = head;
-        unordered_map<ListNode* , int> mp;
-        while(temp){
-            if(mp.find(temp) != mp.end()){
-                return temp;
+    // Brute force approach 
+    // ListNode *detectCycle(ListNode *head) {
+    //     ListNode* temp = head;
+    //     unordered_map<ListNode* , int> mp;
+    //     while(temp){
+    //         if(mp.find(temp) != mp.end()){
+    //             return temp;
+    //         }
+    //         mp[temp]=1;
+    //         temp=temp->next;
+    //     }
+    //     return NULL;
+    // }
+    
+    // optimized using fast and slow pointer 
+     ListNode *detectCycle(ListNode *head) {
+        ListNode *slow = head , *fast=head;
+         int flag=0;
+        while(fast && fast->next){
+            if(flag==1 && slow == fast){
+                // cout<<slow->val<<"slow"<<endl;
+                ListNode* entry=head;
+                while(entry != slow){
+                    entry=entry->next;
+                    slow=slow->next;
+                }
+                return entry;
             }
-            mp[temp]=1;
-            temp=temp->next;
+            flag=1;
+            slow=slow->next;
+            fast = fast->next->next;
         }
         return NULL;
     }
+    
 };
