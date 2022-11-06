4815d34d749015d0ec01dab44e0e4f37c44e3ed6
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2022-11-06 15:20:44 +0530

    Time: 16 ms (43.57%), Space: 11.8 MB (55.23%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -10,28 +10,65 @@
  */
 class Solution {
 public:
+    
+    // BruteForce approach 
+    
+    // ListNode* rotateRight(ListNode* head, int k) {
+    //     if(head== NULL || head->next == NULL || k==0){
+    //         return head;
+    //     }
+    //     int count=0;
+    //     ListNode* temp = head;
+    //     vector<int> v;
+    //     while(temp){
+    //         v.push_back(temp->val);
+    //         count++;
+    //         temp=temp->next;
+    //     }
+    //     k=(k%count);
+    //     k=count-k;
+    //     rotate(v.begin(),v.begin()+k,v.end());
+    //     temp=head;
+    //     int i=0;
+    //     while(temp){
+    //         temp->val = v[i];
+    //         temp=temp->next;
+    //         i++;
+    //     }
+    //     return head;
+    // }
+    
+    // Optimized approach 
     ListNode* rotateRight(ListNode* head, int k) {
         if(head== NULL || head->next == NULL || k==0){
             return head;
         }
         int count=0;
         ListNode* temp = head;
-        vector<int> v;
         while(temp){
-            v.push_back(temp->val);
             count++;
             temp=temp->next;
         }
-        k=(k%count);
+        k=k%count;
+        if(k==0){
+            return head;
+        }
         k=count-k;
-        rotate(v.begin(),v.begin()+k,v.end());
         temp=head;
         int i=0;
-        while(temp){
-            temp->val = v[i];
+        while(i<k-1){
             temp=temp->next;
             i++;
         }
+        ListNode *ans = head;
+        head = temp->next;
+        temp->next = NULL;
+        ListNode *more = head;
+        while(more && more->next != NULL){
+            more= more->next;
+        }
+        more->next = ans;
         return head;
+        
     }
 };
