a2f0304fa6c28c5323e260fe91c52cba3145bf75
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2022-11-06 11:18:21 +0530

    Time: 3 ms (94.83%), Space: 6.3 MB (91.95%) - LeetHub

--- 0899-orderly-queue/0899-orderly-queue.cpp
+++ 0899-orderly-queue/0899-orderly-queue.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
     string orderlyQueue(string S, int K) {
-        if (K > 1) {
-            sort(S.begin(), S.end());
+        if(K>1){
+            sort(S.begin(),S.end());
             return S;
+        }else{
+            // int j=0;
+            string s = S;
+            for(int i=0;i<S.length();i++){
+                rotate(S.begin(),S.begin()+1,S.end());
+                if(s>S){
+                    s=S;
+                }
+            }
+            return s;
         }
-        string res = S;
-        for (int i = 1; i < S.length(); i++)
-            res = min(res, S.substr(i) + S.substr(0, i));
-        return res;
     }
 };
