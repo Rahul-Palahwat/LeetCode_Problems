fc2b2bc324fca738b0f1583bdd68356104e2641a
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2022-11-08 10:08:08 +0530

    Time: 39 ms (36.24%), Space: 21.1 MB (5.55%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,12 +1,49 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-    int l = 0, r = height.size()-1, level = 0, water = 0;
-    while (l < r) {
-        int lower = height[height[l] < height[r] ? l++ : r--];
-        level = max(level, lower);
-        water += level - lower;
+        
+        // Brute force
+        // int ans = 0;
+        // for(int i=1;i<height.size()-1;i++){
+        //     int f=i-1,l=i+1;
+        //     int maxil=0,maxir=0;
+        //     while(f>=0){
+        //         maxil = max(maxil,height[f]);
+        //         f--;
+        //     }
+        //     while(l<height.size()){
+        //         maxir = max(maxir,height[l]);
+        //         l++;
+        //     }
+        //     maxil = min(maxil,maxir);
+        //     if(maxil > height[i]){
+        //         ans+=(maxil-height[i]);
+        //     }
+        // }
+        // return ans;
+        
+        
+        
+       // Better using prefix max 
+        int ans=0;
+        vector<int> prefix;
+        vector<int> postfix(height.size(),0);
+        prefix.push_back(height[0]);
+        
+        for(int i=1;i<height.size();i++){
+            prefix.push_back(max(prefix[i-1],height[i]));
+        }
+        postfix[height.size()-1] = height[height.size()-1];
+        for(int i=height.size()-2;i>=0;i--){
+            postfix[i] = max(postfix[i+1],height[i]);
+        }
+        for(int i=1;i<height.size()-1;i++){
+            int h = min(prefix[i],postfix[i]);
+            if(h>height[i]){
+                ans+=(h-height[i]);
+            }
+        }
+        return ans;
+        
     }
-    return water;
-}
 };
