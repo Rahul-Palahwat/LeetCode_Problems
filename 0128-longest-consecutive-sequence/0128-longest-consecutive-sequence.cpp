fc32d7317d946d8ef47c4290150818a4e95a3394
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2022-11-09 17:36:29 +0530

    Time: 2893 ms (5.00%), Space: 51.2 MB (28.09%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,20 +1,45 @@
 class Solution {
 public:
+    
+    // neive approach
+    // int longestConsecutive(vector<int>& nums) {
+    //     if(nums.size()==0){
+    //         return 0;
+    //     }
+    //     sort(nums.begin(),nums.end());
+    //     int ans = 1;
+    //     int temp=1;
+    //     for(int i=0;i<nums.size()-1;i++){
+    //         if(nums[i]+1 == nums[i+1]){
+    //             temp++;
+    //             ans = max(ans,temp);
+    //         }else if(nums[i] == nums[i+1]){
+    //             continue;
+    //         }else{
+    //             temp=1;
+    //         }
+    //     }
+    //     return ans;
+    // }
+    
+    
+    // hashset solution 
     int longestConsecutive(vector<int>& nums) {
-        if(nums.size()==0){
-            return 0;
-        }
-        sort(nums.begin(),nums.end());
-        int ans = 1;
-        int temp=1;
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i]+1 == nums[i+1]){
-                temp++;
-                ans = max(ans,temp);
-            }else if(nums[i] == nums[i+1]){
+        set<int> s;
+        for(auto it:nums) s.insert(it);
+        int ans = 0;
+        for(auto it:nums){
+            // as we will start from minimal element 
+            if(s.count(it-1)){
                 continue;
             }else{
-                temp=1;
+                int curr = it;
+                curr++;
+                int temp = 1;
+                while(s.count(curr++)){
+                    temp++;
+                }
+                ans = max(ans,temp);
             }
         }
         return ans;
