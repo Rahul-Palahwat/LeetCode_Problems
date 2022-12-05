ab5087b94d326f54afc88127e6576516f6148f76
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2022-12-05 23:01:55 +0530

    Time: 10 ms (22.39%), Space: 7.6 MB (78.27%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,23 +1,43 @@
 class Solution {
 public:
-    void solve(vector<int> nums , vector<vector<int>> &ans , vector<int> temp){
-        if(nums.size()==0){
-            ans.push_back(temp);
+    // 1st method
+    // void solve(vector<int> nums , vector<vector<int>> &ans , vector<int> temp){
+    //     if(nums.size()==0){
+    //         ans.push_back(temp);
+    //         return;
+    //     }
+    //     for(int i=0;i<nums.size();i++){
+    //         temp.push_back(nums[i]);
+    //         vector<int> t = nums;
+    //         t.erase(t.begin()+i);
+    //         solve(t,ans,temp);
+    //         temp.pop_back();
+    //     }
+    //     return;
+    // }
+    // vector<vector<int>> permute(vector<int>& nums) {
+    //     vector<vector<int>> ans;
+    //     vector<int> temp;
+    //     solve(nums,ans,temp);
+    //     return ans;
+    // }
+    
+    // 2nd method 
+    void solve(vector<int>& nums, vector<vector<int>> &ans,int index){
+        if(index == nums.size()){
+            ans.push_back(nums);
             return;
         }
-        for(int i=0;i<nums.size();i++){
-            temp.push_back(nums[i]);
-            vector<int> t = nums;
-            t.erase(t.begin()+i);
-            solve(t,ans,temp);
-            temp.pop_back();
+        for(int i=index;i<nums.size();i++){
+            swap(nums[index],nums[i]);
+            solve(nums,ans,index+1);
+            swap(nums[index],nums[i]);
         }
         return;
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> temp;
-        solve(nums,ans,temp);
+        solve(nums,ans,0);
         return ans;
     }
 };
