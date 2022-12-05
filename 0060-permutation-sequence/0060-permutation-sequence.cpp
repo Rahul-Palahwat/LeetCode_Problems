243ebc49304b11c8f5fe9db49691a9ea5dcff262
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2022-12-05 18:14:36 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (17.45%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
-    // vector<string> ans;
-    // void solve(int n,string s);
+    
+    // first approach brute force 
+        
     int fact(int n){
         int ans=1;
         for(int i=1;i<=n;i++){
@@ -10,8 +11,6 @@ public:
         return ans;
     }
     string getPermutation(int n, int k) {
-        // solve(n);
-        // return ans[k-1];
         vector<string> v;
         string ans="";
         for(int i=1;i<=n;i++){
@@ -30,4 +29,38 @@ public:
         }
         return ans;
     }
+    
+    
+    
+    // 2nd by recursion but my first is the optimal
+    // vector<string> ans;
+    // int sz;
+    // void solve(vector<string> s,string out){
+    //     if(s.size() ==0){
+    //         ans.push_back(out);
+    //         return;
+    //     }
+    //     if(ans.size() >= sz){
+    //         return;
+    //     }
+    //     for(int i=0;i<s.size();i++){
+    //         vector<string> temp = s;
+    //         string o = out;
+    //         o+=s[i];
+    //         temp.erase(temp.begin()+i);
+    //         solve(temp,o);
+    //     }
+    //     return;
+    // }
+    // string getPermutation(int n, int k) {
+    //     // sort(ans.begin(),ans.end());
+    //     vector<string> s;
+    //     sz=k;
+    //     for(int i=1;i<=n;i++){
+    //         string t=to_string(i);
+    //         s.push_back(t);
+    //     }
+    //     solve(s,"");
+    //     return ans[k-1];
+    // }
 };
