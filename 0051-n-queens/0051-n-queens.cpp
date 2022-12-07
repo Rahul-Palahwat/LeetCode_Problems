0501a192e41bb61c86a409e766c44f2eeb83cf0b
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2022-12-07 13:06:37 +0530

    Time: 7 ms (82.75%), Space: 9.2 MB (26.82%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -63,66 +63,115 @@ public:
     
     
     
-    // 2nd approach striver 
+    // 2nd approach striver time complexity O(n^2)
+    // int queen[10];
+    // vector<vector<int>> ans;
+    // bool check(int col,int row,int n){
+    //     // this is to check same column 
+    //     if(queen[col] != -1){
+    //         // cout<<col<<"samehello"<<row<<endl;
+    //         return false;
+    //     }
+    //     int tcol=col;
+    //     int trow = row;
+    //     // this is to check left upward diagonal 
+    //     while(trow >=0 && tcol >=0){
+    //         if(queen[tcol] == trow){
+    //             // cout<<col<<"hello1"<<row<<endl;
+    //             return false;
+    //         }
+    //         trow--;
+    //         tcol--;
+    //     }
+    //     trow = row;
+    //     tcol = col;
+    //     // this is to check right upward diagonal 
+    //     while(trow>=0 && tcol < n){
+    //         // cout<<queen[tcol]<<" h "<<trow<<endl;
+    //         if(queen[tcol] == trow){
+    //             // cout<<col<<"hello2"<<row<<endl;
+    //             return false;
+    //         }
+    //         trow--;
+    //         tcol++;
+    //     }
+    //     return true;
+    // }
+    // void solve(int row,int col , vector<int> temp){
+    //     if(row == col){
+    //         if(temp.size() == col)
+    //         ans.push_back(temp);
+    //         return;
+    //     }
+    //     for(int i=0;i<col;i++){
+    //         if(check(i,row,col)){
+    //             // cout<<"hello"<<endl;
+    //             queen[i] = row;
+    //             temp.push_back(i);
+    //             cout<<temp.size()<<endl;
+    //             solve(row+1,col,temp);
+    //             // cout<<temp.size()<<endl;
+    //             temp.pop_back();
+    //             queen[i] = -1;
+    //         }
+    //     }
+    //     return;
+    // }
+    // vector<vector<string>> solveNQueens(int n) {
+    //     vector<vector<string>> finalans;
+    //     memset(queen,-1,sizeof(queen));
+    //     vector<int> temp;
+    //     solve(0,n,temp);
+    //     // cout<<ans.size()<<endl;
+    //     for(auto it:ans){
+    //         vector<string> st;
+    //         for(auto i: it){
+    //             string s(n,'.');
+    //             s[i]='Q';
+    //             st.push_back(s);
+    //         }
+    //         finalans.push_back(st);
+    //     }
+    //     return finalans;
+    // }
+    
+    
+    // optimization best approach
     int queen[10];
+    int rightupper[21];
+    int leftupper[21];
     vector<vector<int>> ans;
-    bool check(int col,int row,int n){
-        if(queen[col] != -1){
-            cout<<col<<"samehello"<<row<<endl;
-            return false;
-        }
-        int tcol=col;
-        int trow = row;
-        while(trow >=0 && tcol >=0){
-            if(queen[tcol] == trow){
-                cout<<col<<"hello1"<<row<<endl;
-                return false;
-            }
-            trow--;
-            tcol--;
-        }
-        trow = row;
-        tcol = col;
-        while(trow>=0 && tcol < n){
-            cout<<queen[tcol]<<" h "<<trow<<endl;
-            if(queen[tcol] == trow){
-                cout<<col<<"hello2"<<row<<endl;
-                return false;
-            }
-            trow--;
-            tcol++;
-        }
-        return true;
-    }
-    void solve(int row,int col , vector<int> temp){
-        if(row == col){
-            if(temp.size() == col)
+    void solve(int label, int n,vector<int> temp){
+        if(label == n){
             ans.push_back(temp);
             return;
         }
-        for(int i=0;i<col;i++){
-            if(check(i,row,col)){
-                // cout<<"hello"<<endl;
-                queen[i] = row;
+        for(int i=0;i<n;i++){
+            if((queen[i] == 0) && (rightupper[i+label] == 0) && (leftupper[n+label-i] == 0)){
+                queen[i]=1;
                 temp.push_back(i);
-                cout<<temp.size()<<endl;
-                solve(row+1,col,temp);
-                // cout<<temp.size()<<endl;
+                rightupper[i+label]=1;
+                leftupper[n+label-i]=1;
+                solve(label+1,n,temp);
                 temp.pop_back();
-                queen[i] = -1;
+                rightupper[i+label]=0;
+                leftupper[n+label-i]=0;
+                queen[i]=0;
             }
         }
         return;
     }
     vector<vector<string>> solveNQueens(int n) {
+        memset(queen,0,sizeof(queen));
+        memset(rightupper,0,sizeof(rightupper));
+        memset(leftupper,0,sizeof(leftupper));
         vector<vector<string>> finalans;
-        memset(queen,-1,sizeof(queen));
         vector<int> temp;
         solve(0,n,temp);
         // cout<<ans.size()<<endl;
-        for(auto it:ans){
+        for(auto it: ans){
             vector<string> st;
-            for(auto i: it){
+            for(auto i:it){
                 string s(n,'.');
                 s[i]='Q';
                 st.push_back(s);
