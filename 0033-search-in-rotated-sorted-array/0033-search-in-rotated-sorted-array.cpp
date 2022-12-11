e405ef9e1b71159405b7e1f4491e50a2f1accb22
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2022-12-11 14:34:34 +0530

    Time: 12 ms (19.68%), Space: 11.2 MB (29.17%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,9 +1,49 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==target){
-                return i;
+    int search(vector<int>& nums, int k) {
+        int l=0;
+        int h=nums.size()-1;
+        if(nums.size() == 2){
+            if(k == nums[0]){
+                return 0;
+            }else if(k == nums[1]){
+                return 1;
+            }return -1;
+        }
+        while(l<=h){
+            int mid = (l+h)/2;
+            if(nums[mid] > nums[l]){
+                if( k == nums[mid]){
+                    return mid;
+                }else if(k == nums[l]){
+                    return l;
+                }else if(k== nums[h]){
+                    return h;
+                }
+                else if(k<nums[mid] && k>nums[l]){
+                    h=mid-1;
+                }
+                else{
+                    l=mid+1;
+                }
+            }else{
+                if( k == nums[mid]){
+                    return mid;
+                }else if(k == nums[l]){
+                    return l;
+                }else if(k== nums[h]){
+                    return h;
+                }
+                else if(k<nums[mid]){
+                    h=mid-1;
+                }else if(k>nums[l]){
+                    h=mid-1;
+                }else if(k>nums[mid] && k<nums[l]){
+                    l=mid+1;
+                }
+                else{
+                    return mid;
+                }
             }
         }
         return -1;
