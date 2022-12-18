6bcc4124bc1bd8edc87e63973fe534e95e737996
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2022-12-18 21:58:13 +0530

    Time: 503 ms (11.67%), Space: 98.7 MB (58.21%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,34 +1,53 @@
 class Solution {
 public:
-    vector<int> dailyTemperatures(vector<int>& temperatures) {
-        int n = temperatures.size();
-        vector<int> post(n,0);
-        for(int i=n-2;i>=0;i--){
-            if(temperatures[i]<temperatures[i+1]){
-                post[i] = 1;
-            }else{
-                if(post[i+1] > 0){
-                    int j = i+post[i+1];
-                    int flag=0;
-                    while(temperatures[j] <= temperatures[i]){
-                        if(post[j] == 0){
-                            post[i] = 0;
-                            flag=1;
-                            break;
-                        }else{
-                            j = j+post[j];
-                        }
-                    }
-                    if(flag==0){
-                        // cout<<j<<" j "<<i<<endl;
-                        post[i] = j-i;
-                    }
+    // Dp approach 
+//     vector<int> dailyTemperatures(vector<int>& temperatures) {
+//         int n = temperatures.size();
+//         vector<int> post(n,0);
+//         for(int i=n-2;i>=0;i--){
+//             if(temperatures[i]<temperatures[i+1]){
+//                 post[i] = 1;
+//             }else{
+//                 if(post[i+1] > 0){
+//                     int j = i+post[i+1];
+//                     int flag=0;
+//                     while(temperatures[j] <= temperatures[i]){
+//                         if(post[j] == 0){
+//                             post[i] = 0;
+//                             flag=1;
+//                             break;
+//                         }else{
+//                             j = j+post[j];
+//                         }
+//                     }
+//                     if(flag==0){
+//                         // cout<<j<<" j "<<i<<endl;
+//                         post[i] = j-i;
+//                     }
                     
-                }else{
-                    post[i] = 0;
-                }
+//                 }else{
+//                     post[i] = 0;
+//                 }
+//             }
+//         }
+//         return post;
+//     }
+    
+    // stack approach 
+    vector<int> dailyTemperatures(vector<int>& t) {
+        int n = t.size();
+        vector<int> ans(n,0);
+        stack<pair<int,int>> st;
+        st.push({t[n-1],n-1});
+        for(int i = n-2;i>=0;i--){
+            while((st.size() > 0)&& (st.top().first <= t[i])){
+                st.pop();
             }
+            if(st.size() > 0){
+                ans[i] = st.top().second - i;
+            }
+            st.push({t[i],i});
         }
-        return post;
+        return ans;
     }
 };
