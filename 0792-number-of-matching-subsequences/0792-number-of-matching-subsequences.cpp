c15cb6073cdd3c44f203ec374681ff58feb8a306
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-01-31 15:40:27 +0530

    Time: 414 ms (42.58%), Space: 338.1 MB (10.44%) - LeetHub

--- 0792-number-of-matching-subsequences/0792-number-of-matching-subsequences.cpp
+++ 0792-number-of-matching-subsequences/0792-number-of-matching-subsequences.cpp
@@ -1,45 +1,59 @@
 class Solution {
 public:
-    bool solve(string s1, string s2){
-        int j=0;
-        for(int i=0;i<s2.length();i++){
-            if(s2[i] == s1[j]){
-                j++;
+    // unordered_map<string , int> mp;
+    // void solve(string s , int index , string temp){
+    //     if(index >= s.length()){
+    //         if(temp.length() > 0){
+    //             mp[temp]++;
+    //         }
+    //         return;
+    //     }
+    //     solve(s , index+1 , temp);
+    //     temp+=s[index];
+    //     solve(s , index+1 , temp);
+    //     return;
+    // }
+    // int numMatchingSubseq(string s, vector<string>& words) {
+    //     solve(s , 0 , "");
+    //     int ans = 0;
+    //     for(auto it: words){
+    //         if(mp.find(it) != mp.end()){
+    //             ans++;
+    //         }
+    //     }
+    //     return ans;
+    // }
+    
+    
+    bool solve(string f , string s){
+        int i=0,j=0;
+        while(j<s.length() && i<f.length()){
+            if(f[i] == s[j]){
+                i++;
             }
-            if(j == s1.length()){
-                return true;
-            }
+            j++;
+        }
+        if(i == f.length()){
+            return true;
         }
         return false;
-        
     }
+    
     int numMatchingSubseq(string s, vector<string>& words) {
-        
-        int matchCount = 0; // variable to keep track of the number of matching subsequences
-    
-    unordered_map<string, int> cache; // hash map to store previously checked words and their result
-    
-    // iterate through all words in the given vector
-    for(auto &word : words) {
-        // if the word has already been checked, add the result to matchCount
-        if(cache.count(word)) {
-            matchCount += cache[word];
-        } else {
-            // if the word has not been checked yet, check it and add the result to matchCount
-            matchCount += cache[word] = solve(word,s);
+        int ans = 0;
+        unordered_map<string,int> mp;
+        for(auto it: words){
+            if(mp.find(it) != mp.end()){
+                ans+=mp[it];
+            }else{
+                if(solve(it , s)){
+                    ans++;
+                    mp[it]=1;
+                }else{
+                    mp[it] = 0;
+                }
+            }
         }
-    }
-    return matchCount;
-        // int ans = 0;
-        // unordered_map<string,int> mp;
-        // for(auto it: words){
-        //     if(mp.find(it) != mp.end()){
-        //         ans++;
-        //     }else if(solve(it,s)){
-        //         mp[it]++;
-        //         ans++;
-        //     }
-        // }
-        // return ans;
+        return ans;
     }
 };
