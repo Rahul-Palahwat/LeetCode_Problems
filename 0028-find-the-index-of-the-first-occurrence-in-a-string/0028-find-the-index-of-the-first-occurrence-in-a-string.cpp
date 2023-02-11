f75df07aa0086ba609b9d5f1120eddbd8f18cb6d
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-02-11 17:07:12 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (30.83%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,6 +1,49 @@
 class Solution {
 public:
+    // using stl
+    // int strStr(string h, string needle) {
+    //     return h.find(needle);
+    // }
+    
+    // using rolling hash
+    bool check(string needle , string h , int j){
+        // cout<<j<<endl;
+        for(int x=0;x<needle.length();x++){
+            if(needle[x]!=h[x+j]){
+                return false;
+            }
+        }
+        return true;
+    }
     int strStr(string h, string needle) {
-        return h.find(needle);
+        if(h.length() < needle.length()){
+            return -1;
+        }
+        int total = 0;
+        for(auto it: needle){
+            total+=(it-'a');
+        }
+        // cout<<total<<"first"<<endl;
+        int i=0,j=0;
+        for(i=0;i<needle.length();i++){
+            total-=(h[i]-'a');
+        }
+        // cout<<total<<"first loop"<<endl;
+        if(total == 0){
+            if(check(needle , h ,j)){
+                return j;
+            }
+        }
+        while(i<h.length()){
+            total-=(h[i]-'a');
+            total+=(h[j]-'a');
+            j++;
+            i++;
+            // cout<<total<<"total"<<endl;
+            if(total == 0 && check(needle , h ,j)){
+                return j;
+            }
+        }
+        return -1;
     }
 };
