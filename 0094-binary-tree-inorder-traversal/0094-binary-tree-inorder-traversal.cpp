ffeb7b602d00d240908f90d996b52073669c1962
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-02-14 11:27:47 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (78.08%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -11,18 +11,46 @@
  */
 class Solution {
 public:
-    vector<int> ans;
-    void solve(TreeNode* root){
-        if(!root){
-            return;
-        }
-        solve(root->left);
-        ans.push_back(root->val);
-        solve(root->right);
-        return;
-    }
+    // vector<int> ans;
+    // void solve(TreeNode* root){
+    //     if(!root){
+    //         return;
+    //     }
+    //     solve(root->left);
+    //     ans.push_back(root->val);
+    //     solve(root->right);
+    //     return;
+    // }
+    
+    
+    
+    // Morris Traversal
     vector<int> inorderTraversal(TreeNode* root) {
-        solve(root);
-        return ans;
+        vector<int> inorder;
+        TreeNode* cur = root;
+        while(cur){
+            // when there is no one in left 
+            if(cur->left == NULL){
+                inorder.push_back(cur->val);
+                cur = cur->right;
+            }
+            // when there are nodes in left so we will move to left and then bottom of right to connect to cur node by threading 
+            else{
+                TreeNode* prev = cur->left;
+                while(prev->right && prev->right != cur){
+                    prev = prev->right;
+                }
+                if(prev->right == NULL){
+                    prev->right = cur;
+                    cur = cur->left;
+                }else{
+                    cur = prev->right;
+                    inorder.push_back(cur->val);
+                    prev->right = NULL;
+                    cur = cur->right;
+                }
+            }
+        }
+        return inorder;
     }
 };
