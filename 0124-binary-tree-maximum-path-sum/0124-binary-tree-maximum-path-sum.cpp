1af24e308ae1660cd5d5f03d45052c2446af991b
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-02-17 11:16:21 +0530

    Time: 19 ms (96.68%), Space: 27.7 MB (69.84%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -11,73 +11,18 @@
  */
 class Solution {
 public:
-    // int res=INT_MIN;
-    // int solve(TreeNode* root){
-    //     if(root == NULL){
-    //         return 0;
-    //     }
-    //     int l = solve(root->left);
-    //     int r = solve(root->right);
-    //     int temp = max(root->val + max( l , r) , root->val);
-    //     int ans = max(temp , (root->val + l + r));
-    //     res=max(res,ans);
-    //     return temp;
-    // }
-    // int maxPathSum(TreeNode* root) {
-    //     solve(root);
-    //     return res;
-    // }
-    
-    
-    // 2nd time try method 
-//     int maxi = INT_MIN;
-//     int solve(TreeNode* root){
-//         if(root == NULL){
-//             return 0;
-//         }
-//         int left = solve(root->left);
-//         int right = solve(root->right);
-//         if(right<0 && left<0){
-//             maxi = max(maxi , root->val);
-//         }
-//         else if(left<0){
-//             maxi = max(maxi , root->val + right);
-//         }else if(right<0){
-//             cout<<"hello";
-//             maxi = max(maxi , root->val + left);
-//         }else{
-//             maxi = max(maxi , root->val + left + right);
-//         }
-        
-//         return max(root->val , root->val + max(left , right));
-//     }
-//     int maxPathSum(TreeNode* root) {
-//         solve(root);
-//         return maxi;
-//     }
-    
-    // 3rd time
-    int maxi = INT_MIN;
+    int ans = INT_MIN;
     int solve(TreeNode* root){
         if(!root){
             return 0;
         }
         int left = solve(root->left);
         int right = solve(root->right);
-        if(left < 0 && right <0){
-            maxi = max(maxi ,root->val);
-        }else if(left < 0){
-            maxi = max(maxi , right+root->val);
-        }else if(right < 0){
-            maxi = max(maxi , left+root->val);
-        }else{
-            maxi = max(maxi , left+right+root->val);
-        }
-        return max(root->val , root->val+max(left,right));
-        
+        ans = max(ans , root->val + left+ right);
+        return max(0 , root->val + max(left , max(right , 0)));
     }
     int maxPathSum(TreeNode* root) {
         solve(root);
-        return maxi;
+        return ans;
     }
 };
