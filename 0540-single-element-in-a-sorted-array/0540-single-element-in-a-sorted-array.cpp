7070931990f3838c7b66fa9d6b76f8f390ce4753
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-02-21 10:29:06 +0530

    Time: 36 ms (25.60%), Space: 22.3 MB (62.74%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,40 +1,10 @@
 class Solution {
 public:
-    // using property of xor
-    // int singleNonDuplicate(vector<int>& nums) {
-    //     int xo=0;
-    //     for(auto it: nums){
-    //         xo^=it;
-    //     }
-    //     return xo;
-    // }
-    
-    // using binary search 
     int singleNonDuplicate(vector<int>& nums) {
-        int start = 0;
-        int end = nums.size()-1;
-        while(start < end){
-            int mid = (start+end)/2;
-            if((end-mid)%2 ==0){
-                if(mid+1 < nums.size() && mid-1 >=0)
-                if(nums[mid+1] == nums[mid]){
-                    start = mid+2;
-                }else if(nums[mid-1] == nums[mid]){
-                    end = mid-2;
-                }else{
-                    return nums[mid];
-                }
-            }else{
-                if(mid+1 < nums.size() && mid-1 >=0)
-                if(nums[mid+1] == nums[mid]){
-                    end = mid-1;
-                }else if(nums[mid-1] == nums[mid]){
-                    start = mid+1;
-                }else{
-                    return nums[mid];
-                }
-            }
+        int ans = 0;
+        for(auto it: nums){
+            ans^=it;
         }
-        return nums[start];
+        return ans;
     }
 };
