9c7f830b4821147afe24f3bc8e5ef5abe8a88ce0
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-03-03 22:49:13 +0530

    Added solution - LeetHub

---
+++ Cutting Rectangles - GFG/cutting-rectangles.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    vector<long long int> minimumSquares(long long int L, long long int B)
+    {
+        // code here
+        vector<long long int> ans;
+        long long int mini = min(L , B);
+        long long int sz=1;
+        sz = __gcd(L,B);
+        ans.push_back((L/sz)*(B/sz));
+        ans.push_back(sz);
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long int L, B;
+        cin>>L>>B;
+        
+        Solution ob;
+        vector<long long int> ans = ob.minimumSquares(L, B);
+        cout<<ans[0]<<" "<<ans[1]<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
