a2ed34f1d12064771ed3eae5f9952ede440e5e98
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-03-16 22:33:16 +0530

    Time: 24 ms (50.69%), Space: 26.6 MB (16.57%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -11,53 +11,60 @@
  */
 class Solution {
 public:
-    // unordered_map<int,int> mp;
-    // TreeNode* solve(vector<int> in ,int inStart ,int inEnd , vector<int> p , int pStart , int pEnd){
-    //     if(inStart > inEnd){
+    
+    // Brute force , Create all Inorder and PostOrder arrays
+    // TreeNode* solve(vector<int> in, vector<int> p){
+    //     if(in.size() == 0 || p.size() == 0){
     //         return NULL;
     //     }
-    //     if(pStart == pEnd){
-    //         return NULL;
+    //     unordered_map<int,int> mp;
+    //     for(int i=0;i<in.size();i++){
+    //         mp[in[i]] = i;
     //     }
-    //     TreeNode* node = new TreeNode(p[pEnd]);
-    //     int index = mp[p[pEnd]];
-    //     node->left = solve(in , inStart , index-1 , p , pStart , pStart+index-1);
-    //     node->right = solve(in , index+1 , inEnd , p , pStart+index , pEnd-1);
+    //     TreeNode* node = new TreeNode(p[p.size()-1]);
+    //     int index = mp[p[p.size()-1]];
+    //     vector<int> leftIn , rightIn , leftP , rightP;
+    //     for(int i=0;i<index;i++){
+    //         leftIn.push_back(in[i]);
+    //     }
+    //     for(int i=index+1;i<in.size();i++){
+    //         rightIn.push_back(in[i]);
+    //     }
+    //     for(int i=0;i<index;i++){
+    //         leftP.push_back(p[i]);
+    //     }
+    //     for(int i=index;i<in.size()-1;i++){
+    //         rightP.push_back(p[i]);
+    //     }
+    //     node->left = solve(leftIn , leftP);
+    //     node->right = solve(rightIn , rightP);
     //     return node;
     // }
-    TreeNode* solve(vector<int> in, vector<int> p){
-        if(in.size() == 0 || p.size() == 0){
+    // TreeNode* buildTree(vector<int>& inorder, vector<int>& post) {
+    //     return solve(inorder ,post);
+    // }
+    
+    
+    // Optimized using same array
+    unordered_map<int,int> mp;
+    TreeNode* solve(vector<int>& in,int inStart ,int inEnd, vector<int>& p ,int pStart ,int pEnd){
+        if(inStart > inEnd){
             return NULL;
         }
-        unordered_map<int,int> mp;
-        for(int i=0;i<in.size();i++){
-            mp[in[i]] = i;
+        if(pStart > pEnd){
+            return NULL;
         }
-        TreeNode* node = new TreeNode(p[p.size()-1]);
-        int index = mp[p[p.size()-1]];
-        vector<int> leftIn , rightIn , leftP , rightP;
-        for(int i=0;i<index;i++){
-            leftIn.push_back(in[i]);
-        }
-        for(int i=index+1;i<in.size();i++){
-            rightIn.push_back(in[i]);
-        }
-        for(int i=0;i<index;i++){
-            leftP.push_back(p[i]);
-        }
-        for(int i=index;i<in.size()-1;i++){
-            rightP.push_back(p[i]);
-        }
-        node->left = solve(leftIn , leftP);
-        node->right = solve(rightIn , rightP);
+        TreeNode* node = new TreeNode(p[pEnd]);
+        int index = mp[p[pEnd]];
+        node->left = solve(in , inStart , index-1,p , pStart , pStart+index-inStart-1);
+        node->right = solve(in , index+1 , inEnd , p , pStart+index-inStart,pEnd-1);
         return node;
     }
     TreeNode* buildTree(vector<int>& inorder, vector<int>& post) {
         int n = inorder.size();
-        // for(int i=0;i<n;i++){
-        //     mp[inorder[i]] = i;
-        // }
-        // return solve(inorder , 0 , n-1 , post , 0 , n-1);
-        return solve(inorder ,post);
+        for(int i=0;i<n;i++){
+            mp[inorder[i]] = i;
+        }
+        return solve(inorder , 0 , n-1 ,post , 0 , n-1);
     }
 };
