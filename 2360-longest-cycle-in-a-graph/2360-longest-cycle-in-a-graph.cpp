d993869a9ed6ccb107ec3764c2f400cfe10a99e8
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-03-27 23:18:03 +0530

    Time: 583 ms (28.13%), Space: 218.4 MB (17.68%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
@@ -1,66 +1,31 @@
 class Solution {
 public:
-//     int ans = -1;
-//     void dfs(int node , vector<int>& edges , vector<bool> vis){
-//         int dis = 1;
-//         queue<pair<int,int>> q;
-//         q.push({node , dis});
-//         while(!q.empty()){
-            
-//         }
-//     }
-//     int longestCycle(vector<int>& edges) {
-//         int n = edges.size();
-//         vector<bool> vis(n , false);
-//         for(int i=0;i<n;i++){
-//             // if(!vis[i]){
-//                 dfs(i , edges , vis);
-//             // }
-//         }
-//         return ans;
-//     }
-    int maxLength = -1;
-    
-    void getcycle(vector<int> &edges,int si,vector<bool>& visit,vector<int>& store){
-        if(si == -1)return ;
-        if(visit[si]){
-            int count = -1;
-            for(int i =0;i<store.size();i++){
-                if(store[i]==si){
-                    count = i;
-                    break;
-                } }
-            
-            if(count==-1)return;
-            int size = (store.size()-count);
-            maxLength = max(maxLength,size);
-            return ;
+ 
+    int answer = -1;
+
+    void dfs(int node, vector<int>& edges, unordered_map<int, int>& dist, vector<bool>& visit) {
+        visit[node] = true;
+        int neighbor = edges[node];
+
+        if (neighbor != -1 && !visit[neighbor]) {
+            dist[neighbor] = dist[node] + 1;
+            dfs(neighbor, edges, dist, visit);
+        } else if (neighbor != -1 && dist.count(neighbor)) {
+            answer = max(answer, dist[node] - dist[neighbor] + 1);
         }
-        
-        visit[si] = true;
-        store.push_back(si);
-        getcycle(edges,edges[si],visit,store);
-        
-        
-        return ;
-        
     }
-    
-    
-    
+
     int longestCycle(vector<int>& edges) {
-        
-        vector<bool> visit(edges.size(),0);
-        
-        for(int i =0;i<edges.size();i++){
-            
-            if(visit[i])continue;
-            vector<int> store;
-            getcycle(edges,i,visit,store);
-     
+        int n = edges.size();
+        vector<bool> visit(n);
+
+        for (int i = 0; i < n; i++) {
+            if (!visit[i]) {
+                unordered_map<int, int> dist;
+                dist[i] = 1;
+                dfs(i, edges, dist, visit);
+            }
         }
-        
-        return maxLength;
-        
+        return answer;
     }
 };
