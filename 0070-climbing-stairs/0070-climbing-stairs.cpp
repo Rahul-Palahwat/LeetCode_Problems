8220b8746b4556aeeb0a2b541e637c1148b57f94
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-03-28 10:04:05 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (21.52%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,19 +1,29 @@
 class Solution {
 public:
-    int ans = 0;
-    int dp[50] = {-1};
-    void reset(){
-        memset(dp , -1 , sizeof(dp));
+    
+    // Brute force Recursion
+    // int solve(int n){
+    //     if(n<=1){
+    //         return 1;
+    //     }
+    //     return solve(n-1)+solve(n-2);
+    // }
+    // int climbStairs(int n) {
+    //     return solve(n);
+    // }
+    
+    // Now memoization
+    int solve(int n, vector<int>& dp){
+        if(n<=1){
+            return 1;
+        }
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        return dp[n] = solve(n-1,dp)+solve(n-2,dp);
     }
     int climbStairs(int n) {
-        reset();
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 2;
-        for(int i=3 ;i<=n;i++){
-            dp[i] = dp[i-1]+dp[i-2];
-        }
-        return dp[n];
-        // return ans;
+        vector<int> dp(n+1 , -1);
+        return solve(n , dp);
     }
 };
