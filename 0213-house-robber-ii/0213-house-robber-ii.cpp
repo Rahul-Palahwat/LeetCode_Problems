ec4a5afaca5bb6914b06582292bb3d5d5fd210ff
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-04-06 23:25:16 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (5.10%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+    int solveWithout(vector<int>& nums , int ind , int flag){
+        if(ind <= 0){
+            return 0;
+        }
+        // if(ind == 0){
+        //     if()
+        // }
+        if(dp[ind][flag] != -1){
+            return dp[ind][flag];
+        }
+        int take = 0 , leave = 0;
+        if(flag == 0){
+            take = nums[ind]+solveWithout(nums , ind - 1 , 1);
+        }
+        leave = solveWithout(nums , ind-1 , 0);
+        return dp[ind][flag] = max(take , leave);
+    }
+    int solveWith(vector<int>& nums , int ind , int flag){
+        if(ind < 0){
+            return 0;
+        }
+        // if(ind == 0){
+        //     if()
+        // }
+        if(dp[ind][flag] != -1){
+            return dp[ind][flag];
+        }
+        int take = 0 , leave = 0;
+        if(flag == 0){
+            take = nums[ind]+solveWith(nums , ind - 1 , 1);
+        }
+        leave = solveWith(nums , ind-1 , 0);
+        return dp[ind][flag] = max(take , leave);
+    }
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        dp.resize(n , vector<int>(2,-1));
+        if(n == 1){
+            return nums[0];
+        }
+        if(n == 2){
+            return max(nums[0] , nums[1]);
+        }
+        if(n==3){
+            return max(nums[0] , max(nums[1] , nums[2]));
+        }
+        int withoutzero = solveWithout(nums , n-1 , 0);
+        dp.resize(0 , vector<int>(0,-1));
+        dp.resize(n , vector<int>(2,-1));
+        int withzero = solveWith(nums , n-2 , 0);
+        return max(withoutzero,withzero);
+    }
+};
