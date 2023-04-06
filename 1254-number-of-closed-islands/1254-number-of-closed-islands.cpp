e4b75103e715ba7ee2b5f1fec09ca68dff11085a
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-04-06 09:47:26 +0530

    Time: 13 ms (71.49%), Space: 9.5 MB (77.35%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
-    bool solve(vector<vector<int>>& grid , int i,int j){
-        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size()){
-            // cout<<i<<"----i"<<j<<endl;
-            return false;
-        }
-        if(grid[i][j] == 1){
-            return true;
-        }
+    bool solve(int i , int j , vector<vector<int>>& grid , int n , int m){
         if(grid[i][j] == 1){
             return true;
         }
         grid[i][j] = 1;
-        bool r = solve(grid , i+1,j);
-        bool l = solve(grid , i-1,j);
-        bool t = solve(grid , i, j-1);
-        bool b = solve(grid , i, j+1);
-        return r&&l&&t&&b;
+        bool l = false , r = false , t = false , b = false;
+        if(i-1 >= 0){
+            t = solve(i-1 , j , grid , n , m);
+        }
+        if(i+1 < n){
+            b = solve(i+1 , j , grid , n , m);
+        }
+        if(j-1 >= 0){
+            l = solve(i , j-1 , grid , n , m);
+        }
+        if(j+1 < m){
+            r = solve(i , j+1 , grid , n , m);
+        }
+        return l&&r&&t&&b;
     }
     int closedIsland(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        int ans=0;
+        int ans = 0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j] == 0){
-                    // cout<<i<<"i"<<j<<endl;
-                    if(solve(grid , i , j)){
-                        ans++;
-                    }
+                    ans+=solve(i , j , grid , n , m);
                 }
             }
         }
