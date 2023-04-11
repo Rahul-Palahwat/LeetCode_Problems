5fb9c05462273304447a55826aece15738a8a4a5
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-04-11 08:29:14 +0530

    Time: 73 ms (92.95%), Space: 24.2 MB (94.03%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -1,15 +1,29 @@
 class Solution {
 public:
+    // Bruteforce
+    // string removeStars(string s) {
+    //     string ans="";
+    //     int n = s.length();
+    //     for(int i=0;i<n;i++){
+    //         if(s[i] == '*'){
+    //             ans.pop_back();
+    //         }else{
+    //             ans+=s[i];
+    //         }
+    //     }
+    //     return ans;
+    // }
+    
+    // Now using two pointers
     string removeStars(string s) {
-        string ans="";
-        int n = s.length();
-        for(int i=0;i<n;i++){
+        int j = 0;
+        for(int i=0;i<s.length();i++){
             if(s[i] == '*'){
-                ans.pop_back();
+                j--;
             }else{
-                ans+=s[i];
+                s[j++] = s[i];
             }
         }
-        return ans;
+        return s.substr(0,j);
     }
 };
