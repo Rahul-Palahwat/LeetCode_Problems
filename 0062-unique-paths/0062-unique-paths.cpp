a6d4b7bb453928b8800eb1e51c34cf5a08bac5e9
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-04-12 16:11:30 +0530

    Time: 4 ms (23.57%), Space: 6.5 MB (55.90%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,55 +1,27 @@
 class Solution {
 public:
-    
-    // my first approach dp with memoization
-    // int memo[101][101];
-    // int solve(int i,int j,int m,int n){
-    //     if(i==m && j==n){
-    //         return 1;
-    //     }
-    //     if(i>m){
-    //         return 0;
-    //     }
-    //     if(j>n){
-    //         return 0;
-    //     }
-    //     if(memo[i][j] != -1){
-    //         return memo[i][j];
-    //     }
-    //     int t1=solve(i+1,j,m,n);
-    //     int t2=solve(i,j+1,m,n);
-    //     return memo[i][j]=t1+t2;
-    // }
-    // int uniquePaths(int m, int n) {
-    //     memset(memo,-1,sizeof(memo));
-    //     int ans = solve(0,0,m-1,n-1);
-    //     return ans;
-    // }
-    
-    
-    // dp with matrix
+    int ans = 1;
+    int solve(int i , int j , int n , int m,vector<vector<int>>& dp){
+        if(i == n-1 && j == m-1){
+            return 1;
+        }
+        if(i>= n || j>=m){
+            return 0;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        int first =solve(i+1 , j , n , m , dp);
+        int second =solve(i , j+1 , n , m , dp);
+        dp[i][j] = first+second;
+        return ans=dp[i][j];
+    }
     int uniquePaths(int m, int n) {
-        int dp[m+1][n+1];
-        memset(dp,0,sizeof(dp));
-        for(int i=0;i<m;i++){
-            dp[i][0] = 1;
+        if(m == 1 || n==1){
+            return 1;
         }
-        for(int i=0;i<n;i++){
-            dp[0][i]=1;
-        }
-        dp[0][0]=1;
-        
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                dp[i][j] = dp[i-1][j]+dp[i][j-1];
-            }
-        }
-        // for(int i=0;i<m;i++){
-        //     for(int j=0;j<n;j++){
-        //         cout<<dp[i][j]<<" ";
-        //     }
-        //     cout<<endl;
-        // }
-        return dp[m-1][n-1];
+        vector<vector<int>> dp(n, vector<int>(m , -1));
+        solve(0 , 0 , n , m , dp);
+        return ans;
     }
 };
