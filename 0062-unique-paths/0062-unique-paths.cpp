34289c6c50d6fe529a9dea5aad93fe1cf738859c
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-04-12 16:23:41 +0530

    Time: 4 ms (23.57%), Space: 6.3 MB (64.29%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,25 +1,51 @@
 class Solution {
 public:
-    int solve(int i , int j , int n , int m,vector<vector<int>>& dp){
-        if(i == n-1 && j == m-1){
-            return 1;
-        }
-        if(i>= n || j>=m){
-            return 0;
-        }
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        int first =solve(i+1 , j , n , m , dp);
-        int second =solve(i , j+1 , n , m , dp);
-        dp[i][j] = first+second;
-        return dp[i][j];
-    }
+    
+    // Recursion + memoization
+    // int solve(int i , int j , int n , int m,vector<vector<int>>& dp){
+    //     if(i == n-1 && j == m-1){
+    //         return 1;
+    //     }
+    //     if(i>= n || j>=m){
+    //         return 0;
+    //     }
+    //     if(dp[i][j] != -1){
+    //         return dp[i][j];
+    //     }
+    //     int first =solve(i+1 , j , n , m , dp);
+    //     int second =solve(i , j+1 , n , m , dp);
+    //     dp[i][j] = first+second;
+    //     return dp[i][j];
+    // }
+    // int uniquePaths(int m, int n) {
+    //     if(m == 1 || n==1){
+    //         return 1;
+    //     }
+    //     vector<vector<int>> dp(n, vector<int>(m , -1));
+    //     return solve(0 , 0 , n , m , dp);
+    // }
+    
+    
+    
+    
+    // Now tabulation
     int uniquePaths(int m, int n) {
         if(m == 1 || n==1){
             return 1;
         }
-        vector<vector<int>> dp(n, vector<int>(m , -1));
-        return solve(0 , 0 , n , m , dp);
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(i == 1 || j==1){
+                    dp[i][j] = 1;
+                }
+            }
+        }
+        for(int i=2;i<n+1;i++){
+            for(int j=2;j<m+1;j++){
+                dp[i][j] = (dp[i-1][j]+dp[i][j-1]);
+            }
+        }
+        return dp[n][m];
     }
 };
