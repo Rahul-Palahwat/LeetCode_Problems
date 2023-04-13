9f3a83e16c02d00887d6529cb0677bc9c3a385b4
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-04-13 18:46:57 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (62.42%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -30,22 +30,12 @@ public:
     
     // Now tabulation
     int uniquePaths(int m, int n) {
-        if(m == 1 || n==1){
-            return 1;
-        }
-        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
-        for(int i=1;i<n+1;i++){
-            for(int j=1;j<m+1;j++){
-                if(i == 1 || j==1){
-                    dp[i][j] = 1;
-                }
-            }
-        }
-        for(int i=2;i<n+1;i++){
-            for(int j=2;j<m+1;j++){
+        vector<vector<int>> dp(n, vector<int>(m, 1));
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
                 dp[i][j] = (dp[i-1][j]+dp[i][j-1]);
             }
         }
-        return dp[n][m];
+        return dp[n-1][m-1];
     }
 };
