1d18a6f9824fd8b7163a78cbe5f24ee7953c98d1
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-04-19 18:08:27 +0530

    Time: 7 ms (89.18%), Space: 10.1 MB (50.91%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -54,26 +54,48 @@ public:
     
     
     // Now recursion and then memoization 
-    int solve(vector<vector<int>>& grid , int i,int j , vector<vector<int>>& dp){
-        if(i==0 && j==0){
-            return grid[i][j];
-        }
-        if(i<0 || j<0){
-            return INT_MAX;
-        }
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        int ans1 = solve(grid , i-1 , j ,dp);
-        int ans2 = solve(grid , i , j-1 , dp);
-        if(ans1 != INT_MAX || ans2 != INT_MAX)
-            return dp[i][j] = grid[i][j]+min(ans1 , ans2);
-        else
-            return INT_MAX;
-    }
+    // int solve(vector<vector<int>>& grid , int i,int j , vector<vector<int>>& dp){
+    //     if(i==0 && j==0){
+    //         return grid[i][j];
+    //     }
+    //     if(i<0 || j<0){
+    //         return INT_MAX;
+    //     }
+    //     if(dp[i][j] != -1){
+    //         return dp[i][j];
+    //     }
+    //     int ans1 = solve(grid , i-1 , j ,dp);
+    //     int ans2 = solve(grid , i , j-1 , dp);
+    //     if(ans1 != INT_MAX || ans2 != INT_MAX)
+    //         return dp[i][j] = grid[i][j]+min(ans1 , ans2);
+    //     else
+    //         return INT_MAX;
+    // }
+    // int minPathSum(vector<vector<int>>& grid) {
+    //     int n = grid.size() , m = grid[0].size();
+    //     vector<vector<int>> dp(n , vector<int>(m , -1));
+    //     return solve(grid, n-1 , m-1 , dp);
+    // }
+    
+    
+    
+    // Now tabulation 
     int minPathSum(vector<vector<int>>& grid) {
         int n = grid.size() , m = grid[0].size();
-        vector<vector<int>> dp(n , vector<int>(m , -1));
-        return solve(grid, n-1 , m-1 , dp);
+        vector<vector<int>> dp(n , vector<int>(m , 0));
+        dp[0][0] = grid[0][0];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 && j==0)
+                    continue;
+                else if(i == 0)
+                    dp[i][j] = grid[i][j]+dp[i][j-1];
+                else if(j==0)
+                    dp[i][j] = grid[i][j]+dp[i-1][j];
+                else
+                    dp[i][j] = grid[i][j]+min(dp[i-1][j] , dp[i][j-1]);
+            }
+        }
+        return dp[n-1][m-1];
     }
 };
