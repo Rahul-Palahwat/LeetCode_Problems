07803d693817f7f1f9470e37c5a38bf88013507b
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-04-20 07:23:39 +0530

    Time: 20 ms (21.19%), Space: 10.1 MB (75.09%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -30,33 +30,61 @@ public:
     
     
     // Recursion + memoization
-    int solve(vector<vector<int>>& matrix , int i , int j , vector<vector<int>>& dp){
-        int n = matrix.size();
-        if(i == 0){
-            return matrix[i][j];
-        }
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        int ans = INT_MAX;
-        if(j==0 && n>1){
-            ans =matrix[i][j] + min(solve(matrix , i-1 , j , dp) , solve(matrix , i-1 , j+1 , dp));
-        }else if(j==0){
-            ans = matrix[i][j]+solve(matrix , i-1 , j , dp);
-        }else if(j==n-1){
-            ans = matrix[i][j]+min(solve(matrix , i-1 , j , dp) , solve(matrix , i-1 , j-1 , dp));
-        }else{
-            ans = matrix[i][j]+min(solve(matrix , i-1 , j , dp) , min(solve(matrix , i-1 , j+1 , dp) , solve(matrix , i-1 , j-1 , dp)));
-        }
-        return dp[i][j] = ans;
+//     int solve(vector<vector<int>>& matrix , int i , int j , vector<vector<int>>& dp){
+//         int n = matrix.size();
+//         if(i == 0){
+//             return matrix[i][j];
+//         }
+//         if(dp[i][j] != -1){
+//             return dp[i][j];
+//         }
+//         int ans = INT_MAX;
+//         if(j==0 && n>1){
+//             ans =matrix[i][j] + min(solve(matrix , i-1 , j , dp) , solve(matrix , i-1 , j+1 , dp));
+//         }else if(j==0){
+//             ans = matrix[i][j]+solve(matrix , i-1 , j , dp);
+//         }else if(j==n-1){
+//             ans = matrix[i][j]+min(solve(matrix , i-1 , j , dp) , solve(matrix , i-1 , j-1 , dp));
+//         }else{
+//             ans = matrix[i][j]+min(solve(matrix , i-1 , j , dp) , min(solve(matrix , i-1 , j+1 , dp) , solve(matrix , i-1 , j-1 , dp)));
+//         }
+//         return dp[i][j] = ans;
         
-    }
+//     }
+//     int minFallingPathSum(vector<vector<int>>& matrix) {
+//         int n = matrix.size();
+//         vector<vector<int>> dp(n , vector<int>(n , -1));
+//         int ans = INT_MAX;
+//         for(int i=0;i<n;i++){
+//             ans = min(ans , solve(matrix , n-1 , i , dp));
+//         }
+//         return ans;
+//     }
+    
+    
+    
+    // Tabluation DP , bottom up
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        vector<vector<int>> dp(n , vector<int>(n , -1));
-        int ans = INT_MAX;
+        vector<vector<int>> dp(n , vector<int>(n,0));
         for(int i=0;i<n;i++){
-            ans = min(ans , solve(matrix , n-1 , i , dp));
+            for(int j=0;j<n;j++){
+                if(i==0){
+                    dp[i][j] = matrix[i][j];
+                }else{
+                    if(j == 0){
+                        dp[i][j] = matrix[i][j]+min(dp[i-1][j] , dp[i-1][j+1]);
+                    }else if(j == n-1){
+                        dp[i][j] = matrix[i][j]+min(dp[i-1][j] , dp[i-1][j-1]);
+                    }else{
+                        dp[i][j] = matrix[i][j]+min(dp[i-1][j] , min(dp[i-1][j-1] , dp[i-1][j+1]));
+                    }
+                }
+            }
+        }
+        int ans = INT_MAX;
+        for(auto it: dp[n-1]){
+            ans = min(ans , it);
         }
         return ans;
     }
