dd2df91c41a07ae5a11d75f4811d193d7e21e4b7
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-04-21 09:10:09 +0530

    Time: 11 ms (52.11%), Space: 17.5 MB (25.94%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -13,65 +13,74 @@ class Solution {
 public:
     
     // my previous solution 
-    int widthOfBinaryTree(TreeNode* root) {
-        if(!root){
-            return 0;
-        }
-        queue<pair<TreeNode* , long long>> q;
-        q.push({root , 0});
-        int ans = 0;
-        while(!q.empty()){
-            int t = q.size();
-            long long start=0,end=0,flag=0;
-            while(t--){
-                TreeNode* node = q.front().first;
-                int pos = q.front().second;
-                if(flag==0){
-                    // cout<<pos<<" start"<<endl;
-                    start = pos;
-                    flag=1;
-                }
-                if(t==0){
-                    // cout<<end<<" end"<<endl;
-                    end = pos;
-                }
-                q.pop();
-                if(node->left){
-                    q.push({node->left , 2*(pos-start)});
-                }
-                if(node->right){
-                    q.push({node->right , 2*(pos-start)+1});
-                }
-            }
-            int tt = (int)(end-start+1);
-            ans = max(ans , tt);
-        }
-        return ans;
-    }
-    
-    
-    
-    // Now my new solution 
     // int widthOfBinaryTree(TreeNode* root) {
     //     if(!root){
     //         return 0;
     //     }
-    //     int ans = 0;
-    //     queue<pair<TreeNode* , int>> q;
+    //     queue<pair<TreeNode* , long long>> q;
     //     q.push({root , 0});
+    //     int ans = 0;
     //     while(!q.empty()){
-    //         int start , end;
-    //         int sz = q.size();
-    //         while(sz--){
-    //             auto top = q.front();
-    //             q.pop();
-    //             if(top.first->left){
-    //                 q.push({top.first->left , top.second-1});
+    //         int t = q.size();
+    //         long long start=0,end=0,flag=0;
+    //         while(t--){
+    //             TreeNode* node = q.front().first;
+    //             int pos = q.front().second;
+    //             if(flag==0){
+    //                 // cout<<pos<<" start"<<endl;
+    //                 start = pos;
+    //                 flag=1;
     //             }
-    //             if(top.first->right){
-    //                 q.push({top.first->right , top.second+1});
+    //             if(t==0){
+    //                 // cout<<end<<" end"<<endl;
+    //                 end = pos;
+    //             }
+    //             q.pop();
+    //             if(node->left){
+    //                 q.push({node->left , 2*(pos-start)});
+    //             }
+    //             if(node->right){
+    //                 q.push({node->right , 2*(pos-start)+1});
     //             }
     //         }
+    //         int tt = (int)(end-start+1);
+    //         ans = max(ans , tt);
     //     }
+    //     return ans;
     // }
+    
+    
+    
+    // Now my new solution 
+    int widthOfBinaryTree(TreeNode* root) {
+        if(!root){
+            return 0;
+        }
+        long long ans = 0;
+        queue<pair<TreeNode* , long long>> q;
+        q.push({root , 0});
+        while(!q.empty()){
+            long long start , end , flag=0;
+            long long sz = q.size();
+            while(sz--){
+                auto top = q.front();
+                q.pop();
+                if(flag==0){
+                    start = top.second;
+                    flag=1;
+                }
+                if(sz == 0){
+                    end = top.second;
+                }
+                if(top.first->left){
+                    q.push({top.first->left , 2*(top.second-start)});
+                }
+                if(top.first->right){
+                    q.push({top.first->right , 2*(top.second-start)+1});
+                }
+            }
+            ans = max(ans , (end-start+1));
+        }
+        return ans;
+    }
 };
