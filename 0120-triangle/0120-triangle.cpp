3d6740f9b3a7abaa715d865536293e4924f7437d
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-04-21 19:17:41 +0530

    Time: 3 ms (96.47%), Space: 8.8 MB (69.58%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -21,24 +21,39 @@ public:
     
     
     // dp bottom up , tabulation 
+    // int minimumTotal(vector<vector<int>>& triangle) {
+    //     int n = triangle.size();
+    //     for(int i=1;i<n;i++){
+    //         int sz = triangle[i].size();
+    //         for(int j=0;j<sz;j++){
+    //             if(j == 0){
+    //                 triangle[i][j]+=triangle[i-1][j];
+    //             }else if(j == sz-1){
+    //                 triangle[i][j]+=triangle[i-1][j-1];
+    //             }else{
+    //                 triangle[i][j]+=min(triangle[i-1][j-1] , triangle[i-1][j]);
+    //             }
+    //         }
+    //     }
+    //     int ans = INT_MAX;
+    //     for(auto it: triangle[n-1]){
+    //         ans = min(ans , it);
+    //     }
+    //     return ans;
+    // }
+    
+    
+    // Another approach 
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        for(int i=1;i<n;i++){
-            int sz = triangle[i].size();
-            for(int j=0;j<sz;j++){
-                if(j == 0){
-                    triangle[i][j]+=triangle[i-1][j];
-                }else if(j == sz-1){
-                    triangle[i][j]+=triangle[i-1][j-1];
-                }else{
-                    triangle[i][j]+=min(triangle[i-1][j-1] , triangle[i-1][j]);
-                }
+        vector<int> prev = triangle[n-1];
+        for(int i=n-2;i>=0;i--){
+            vector<int> curr=triangle[i];
+            for(int j=0;j<curr.size();j++){
+                curr[j]+=min(prev[j] , prev[j+1]);
             }
+            prev = curr;
         }
-        int ans = INT_MAX;
-        for(auto it: triangle[n-1]){
-            ans = min(ans , it);
-        }
-        return ans;
+        return prev[0];
     }
 };
