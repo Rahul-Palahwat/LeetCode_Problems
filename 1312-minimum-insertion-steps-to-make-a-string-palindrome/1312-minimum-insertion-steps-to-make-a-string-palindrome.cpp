06a72b960a1eb090459ca65d1c459afe32febfe6
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-04-22 08:07:33 +0530

    Time: 118 ms (13.27%), Space: 28.5 MB (24.45%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -56,7 +56,7 @@ public:
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
                 if(temp[i-1] == s[j-1]){
-                    dp[i][j] = 1+dp[i-1][j-1];
+                    dp[i][j] = 1 + dp[i-1][j-1];
                 }else{
                     dp[i][j] = max(dp[i-1][j] , dp[i][j-1]);
                 }
