404ca27adf29f60429462818159d2cb9e79f5f84
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-05-07 13:52:40 +0530

    Added solution - LeetHub

--- Powerfull Integer - GFG/powerfull-integer.cpp
+++ Powerfull Integer - GFG/powerfull-integer.cpp
@@ -10,22 +10,50 @@ using namespace std;
 
 class Solution{
 public:
+
+    // optimized solution 
+    // int powerfullInteger(int n,vector<vector<int>> &intervals,int k){
+    //     int ans = -1;
+    //     int maxi = 0;
+    //     for(auto it:intervals){
+    //         maxi = max(maxi , it[1]);
+    //     }
+    //     vector<int> arr(maxi+10 , 0);
+    //     for(auto it: intervals){
+    //         arr[it[0]]++;
+    //         arr[it[1]+1]--;
+    //     }
+    //     int sum = 0;
+    //     for(int i=0;i<=maxi;i++){
+    //         sum+=arr[i];
+    //         if(sum>=k){
+    //             ans = i;
+    //         }
+    //     }
+    //     return ans;
+    // }
+    
+    // more optimized 
     int powerfullInteger(int n,vector<vector<int>> &intervals,int k){
         int ans = -1;
         int maxi = 0;
-        for(auto it:intervals){
-            maxi = max(maxi , it[1]);
-        }
-        vector<int> arr(maxi+10 , 0);
+        map<int,int> mp;
         for(auto it: intervals){
-            arr[it[0]]++;
-            arr[it[1]+1]--;
+            mp[it[0]]++;
+            mp[it[1]+1]--;
         }
-        int sum = 0;
-        for(int i=0;i<=maxi;i++){
-            sum+=arr[i];
-            if(sum>=k){
-                ans = i;
+        int sum = 0 , flag=0;
+        for(auto it: mp){
+            sum+=it.second;
+            if(sum >= k){
+                ans = it.first;
+                flag=1;
+            }
+            else if(flag == 1){
+                ans = it.first-1;
+                flag=0;
+            }else{
+                flag=0;
             }
         }
         return ans;
