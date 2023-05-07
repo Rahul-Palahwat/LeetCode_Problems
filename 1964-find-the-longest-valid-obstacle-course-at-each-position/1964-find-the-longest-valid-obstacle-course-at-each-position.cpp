501800c43757923d4011df6f82fd8af1ea32f69c
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-05-07 13:11:30 +0530

    Time: 303 ms (70.14%), Space: 117.7 MB (69.44%) - LeetHub

--- 1964-find-the-longest-valid-obstacle-course-at-each-position/1964-find-the-longest-valid-obstacle-course-at-each-position.cpp
+++ 1964-find-the-longest-valid-obstacle-course-at-each-position/1964-find-the-longest-valid-obstacle-course-at-each-position.cpp
@@ -1,21 +1,44 @@
 class Solution {
 public:
+    
+    
+    // O(N^2) solution 
+    // vector<int> longestObstacleCourseAtEachPosition(vector<int>& A) {
+    //     int n = A.size();
+    //     vector<int> ans(n,1);
+    //     for(int i=1;i<n;i++){
+    //         int j=i-1;
+    //         int maxi = 0;
+    //         while(j>=0){
+    //             if(A[j] <= A[i]){
+    //                 maxi = max(maxi , ans[j]);
+    //             }
+    //             j--;
+    //         }
+    //         // cout<<maxi<<"maxi"<<endl;
+    //         ans[i]+=maxi;
+    //     }
+    //     return ans;
+    // }
+    
+    
+    
+    
+    // O(NlogN) solution 
     vector<int> longestObstacleCourseAtEachPosition(vector<int>& A) {
-        vector<int> res, mono;
-        for (int a : A) {
-            int l = 0, r = mono.size();
-            while (l < r) {
-                int m = (l + r) / 2;
-                if (mono[m] <= a)
-                    l = m + 1;
-                else
-                    r = m;
+        int n = A.size();
+        vector<int> ans(n) , curr;
+        for(int i=0;i<n;i++){
+            if(curr.size() == 0 || curr.back() <= A[i]){
+                curr.push_back(A[i]);
+                ans[i] = curr.size();
+            }else{
+                int index = upper_bound(curr.begin() , curr.end() , A[i])-curr.begin();
+                curr[index] = A[i];
+                ans[i] = index+1;
             }
-            res.push_back(l + 1);
-            if (mono.size() == l)
-                mono.push_back(a);
-            mono[l] = a;
         }
-        return res;
+        return ans;
     }
+    
 };
