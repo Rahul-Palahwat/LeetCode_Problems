8ea606ce733e98a3567c7e614d7ab0ca52ae424a
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-05-22 09:18:23 +0530

    Time: 25 ms (15.81%), Space: 13.8 MB (42.78%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,26 +1,50 @@
 class Solution {
 public:
+    // using map
+    
+//     vector<int> topKFrequent(vector<int>& nums, int k) {
+//         map<int , int> mp;
+//         for(auto it: nums){
+//             mp[it]++;
+//         }
+//         map<int, vector<int> , greater<int>> maxi;
+//         for(auto it: mp){
+//             maxi[it.second].push_back(it.first);
+//         }
+//         vector<int> ans;
+//         for(auto it: maxi){
+//             for(int i=0;i<it.second.size();i++){
+//                 ans.push_back(it.second[i]);
+//                 if(ans.size() == k){
+//                     return ans;
+//                 }
+//             }
+//             // ans.push_back(it.second);
+            
+//         }
+//         // cout<<endl;
+//         return ans;
+//     }
+    
+    
+    
+    
+    // Now using priority queue maxh
     vector<int> topKFrequent(vector<int>& nums, int k) {
         map<int , int> mp;
+        vector<int> ans;
         for(auto it: nums){
             mp[it]++;
         }
-        map<int, vector<int> , greater<int>> maxi;
+        priority_queue<pair<int,int>> pq;
         for(auto it: mp){
-            maxi[it.second].push_back(it.first);
+            pq.push({it.second , it.first});
         }
-        vector<int> ans;
-        for(auto it: maxi){
-            for(int i=0;i<it.second.size();i++){
-                ans.push_back(it.second[i]);
-                if(ans.size() == k){
-                    return ans;
-                }
-            }
-            // ans.push_back(it.second);
-            
+        while(k--){
+            auto top = pq.top();
+            pq.pop();
+            ans.push_back(top.second);
         }
-        // cout<<endl;
         return ans;
     }
 };
