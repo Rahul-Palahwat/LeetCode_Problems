06e5f4803d82ce1a254eebeb2be4dd2568853d67
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-05-24 22:41:56 +0530

    Time: 43 ms (41.35%), Space: 18.2 MB (60.09%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -23,38 +23,45 @@ public:
     //     return solve(amount , coins , n-1 , dp);
     // }
     
+    
+    
     // Tabulation
-    int solve(int amount , vector<int>& coins , int index , vector<vector<int>>& dp){
-        if(index < 0){
-            if(amount == 0){
-                return 1;
-            }
-            return 0;
-        }
-        if(dp[index][amount] != -1){
-            return dp[index][amount];
-        }
-        int notTake = solve(amount , coins , index-1 ,dp) , take = 0;
-        if(coins[index] <= amount){
-            take = solve(amount-coins[index] , coins , index ,dp);
-        }
-        return dp[index][amount] = take+notTake;
-    }
+    // int change(int amount, vector<int>& coins) {
+    //     int n = coins.size();
+    //     vector<vector<int>> dp(n+1 , vector<int>(amount+1 , 0));
+    //     for(int i=0;i<n+1;i++){
+    //         dp[i][0] = 1;
+    //     }
+    //     for(int i=1;i<n+1;i++){
+    //         for(int j=1;j<amount+1;j++){
+    //             int notTake = dp[i-1][j] , take = 0;
+    //             if(coins[i-1]<=j){
+    //                 take = dp[i][j-coins[i-1]];
+    //             }
+    //             dp[i][j] = take+notTake;
+    //         }
+    //     }
+    //     return dp[n][amount];
+    // }
+    
+    
+    // Tabulation + Optimization
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>> dp(n+1 , vector<int>(amount+1 , 0));
-        for(int i=0;i<n+1;i++){
-            dp[i][0] = 1;
-        }
+        vector<int> dp(amount+1 , 0);
+        dp[0] = 1;
         for(int i=1;i<n+1;i++){
+            vector<int> temp(amount+1 , 0);
+            temp[0] = 1;
             for(int j=1;j<amount+1;j++){
-                int notTake = dp[i-1][j] , take = 0;
+                int notTake = dp[j] , take = 0;
                 if(coins[i-1]<=j){
-                    take = dp[i][j-coins[i-1]];
+                    take = temp[j-coins[i-1]];
                 }
-                dp[i][j] = take+notTake;
+                temp[j] = take+notTake;
             }
+            dp = temp;
         }
-        return dp[n][amount];
+        return dp[amount];
     }
 };
