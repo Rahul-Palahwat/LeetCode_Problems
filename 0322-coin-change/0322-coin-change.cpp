b9d1f7f4790d748d4d16b3ee0d7a1d1b195ddb45
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-05-24 20:15:53 +0530

    Time: 251 ms (7.97%), Space: 309.7 MB (5.01%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,41 +1,40 @@
 class Solution {
 public:
     // Recursion + Optimization
-    // int solve(vector<int>& coins , int amount , int index , vector<vector<int>>& dp){
-    //     if(amount == 0){
-    //         return 0;
-    //     }
-    //     if(index < 0){
-    //         if(amount == 0){
-    //             return 0;
-    //         }
-    //         return INT_MAX;
-    //     }
-    //     if(dp[index][amount] != -1){
-    //         return dp[index][amount];
-    //     }
-    //     int notTake = solve(coins , amount , index-1 ,dp) , take = INT_MAX;
-    //     if(amount >= coins[index]){
-    //         int same = solve(coins,amount-coins[index],index ,dp);
-    //         int change = solve(coins,amount-coins[index],index-1 ,dp);
-    //         if(same != INT_MAX || change != INT_MAX){
-    //             take = 1+ min(same , change);
-    //         }
-    //     }
-    //     if(notTake != INT_MAX || take != INT_MAX){
-    //         return dp[index][amount] = min(take , notTake);
-    //     }
-    //     return dp[index][amount] = INT_MAX;
-    // }
-    // int coinChange(vector<int>& coins, int amount) {
-    //     int n = coins.size();
-    //     vector<vector<int>> dp(n , vector<int>(1e5,-1));
-    //     int ans = solve(coins , amount , n-1 ,dp);
-    //     if(ans != INT_MAX){
-    //         return ans;
-    //     }
-    //     return -1;
-    // }
+    int solve(vector<int>& coins , int amount , int index , vector<vector<int>>& dp){
+        if(amount == 0){
+            return 0;
+        }
+        if(index < 0){
+            if(amount == 0){
+                return 0;
+            }
+            return INT_MAX;
+        }
+        if(dp[index][amount] != -1){
+            return dp[index][amount];
+        }
+        int notTake = solve(coins , amount , index-1 ,dp) , take = INT_MAX;
+        if(amount >= coins[index]){
+            int same = solve(coins,amount-coins[index],index ,dp);
+            if(same != INT_MAX){
+                take = 1+ same;
+            }
+        }
+        if(notTake != INT_MAX || take != INT_MAX){
+            return dp[index][amount] = min(take , notTake);
+        }
+        return dp[index][amount] = INT_MAX;
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<vector<int>> dp(n , vector<int>(1e5,-1));
+        int ans = solve(coins , amount , n-1 ,dp);
+        if(ans != INT_MAX){
+            return ans;
+        }
+        return -1;
+    }
     
     
     
@@ -71,25 +70,25 @@ public:
     
     
     // Tabulation + optimization
-    int coinChange(vector<int>& coins, int amount) {
-        int n = coins.size();
-        vector<int> dp(amount+1 , INT_MAX);
-        dp[0] = 0;
-        for(int i=1;i<n+1;i++){
-            vector<int> temp(amount+1 , INT_MAX);
-            temp[0] = 0;
-            for(int j=1;j<amount+1;j++){
-                int notTake = dp[j] , take = INT_MAX;
-                if(coins[i-1] <= j){
-                    take = min(temp[j-coins[i-1]] , dp[j-coins[i-1]]);
-                    if(take < INT_MAX){
-                        take+=1;
-                    }
-                }
-                temp[j] = min(take , notTake);
-            }
-            dp = temp;
-        }
-        return dp[amount] == INT_MAX ? -1 : dp[amount];
-    }
+    // int coinChange(vector<int>& coins, int amount) {
+    //     int n = coins.size();
+    //     vector<int> dp(amount+1 , INT_MAX);
+    //     dp[0] = 0;
+    //     for(int i=1;i<n+1;i++){
+    //         vector<int> temp(amount+1 , INT_MAX);
+    //         temp[0] = 0;
+    //         for(int j=1;j<amount+1;j++){
+    //             int notTake = dp[j] , take = INT_MAX;
+    //             if(coins[i-1] <= j){
+    //                 take = min(temp[j-coins[i-1]] , dp[j-coins[i-1]]);
+    //                 if(take < INT_MAX){
+    //                     take+=1;
+    //                 }
+    //             }
+    //             temp[j] = min(take , notTake);
+    //         }
+    //         dp = temp;
+    //     }
+    //     return dp[amount] == INT_MAX ? -1 : dp[amount];
+    // }
 };
