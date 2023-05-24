c458bc3a8f8e38820ce33b797a1ea5fa34d207c5
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-05-24 20:41:29 +0530

    Time: 15 ms (70.75%), Space: 12.3 MB (37.69%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,20 +1,32 @@
 class Solution {
 public:
-    int solve(vector<int>& nums , int target , int index , map<pair<int , int> , int>& dp){
+    int solve(vector<int>& nums , int target , int index , vector<vector<int>>& dp){
         if(index < 0){
             if(target == 0){
                 return 1;
             }
             return 0;
         }
-        if(dp.find({index , target}) != dp.end()){
-            return dp[{index , target}];
+        if(dp[index][target] != -1){
+            return dp[index][target];
         }
-        return dp[{index , target}] = solve(nums , target-nums[index] , index-1 ,dp)+solve(nums , target + nums[index] , index-1 , dp);
+        int notTake = solve(nums , target , index-1 , dp),take = 0;
+        if(nums[index]<= target){
+            take = solve(nums , target-nums[index] , index-1 , dp);
+        }
+        return dp[index][target] = notTake+take;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
-        map<pair<int,int> , int> dp;
+        int sum = 0;
+        for(auto it: nums){
+            sum+=it;
+        }
+        if(abs(sum+target)%2 != 0 || abs(target) > sum){
+            return 0;
+        }
+        target = (abs(sum-target))/2;
+        vector<vector<int>> dp(n+1 , vector<int>(target+1 , -1));
         return solve(nums , target , n-1 , dp);
     }
 };
