2d8597a042d056de0b41e9e1fa3ece7c328ba53c
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-05-29 17:41:01 +0530

    Added solution - LeetHub

---
+++ CamelCase Pattern Matching - GFG/camel-case-pattern-matching.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<string> CamelCase(int N, vector<string> Dictionary, string Pattern) {
+        // code here
+        vector<string> ans;
+        ans.push_back("-1");
+        map<string , vector<string>> mp;
+        for(auto it: Dictionary){
+            string temp = "";
+            for(auto i:it){
+                if((i-'Z') <= 0){
+                    temp+=i;
+                    mp[temp].push_back(it);
+                }
+            }
+        }
+        if(mp.find(Pattern) != mp.end()){
+            return mp[Pattern];
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        vector<string> Dictionary(N);
+        for (int i = 0; i < N; i++) cin >> Dictionary[i];
+        string Pattern;
+        cin >> Pattern;
+        Solution ob;
+        vector<string> ans = ob.CamelCase(N, Dictionary, Pattern);
+        sort(ans.begin(), ans.end());
+        for (auto u : ans) cout << u << " ";
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
