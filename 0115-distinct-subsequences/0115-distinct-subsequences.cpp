9a8811e6820306c6cc7a43ba7b7cdf6b8c42a950
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-06-01 07:38:16 +0530

    Time: 51 ms (56.06%), Space: 30.1 MB (27.68%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -25,33 +25,22 @@ public:
     
     
     // Tabulation
-    long long MOD = 1e9+7;
     int numDistinct(string s, string t) {
-        int n = s.length() , m = t.length();
-        vector<vector<long long>> dp(m+1, vector<long long>(n+1 , 0));
+        int n = s.size() , m = t.size();
+        vector<vector<double>> dp(n+1 , vector<double>(m+1 , 0));
         for(int i=0;i<n+1;i++){
-            dp[0][i] = 1;
+            dp[i][0] = 1;
         }
-        for(int i=1;i<m+1;i++){
-            for(int j=1;j<n+1;j++){
-                if(s[j-1] == t[i-1]){
-                    if(dp[i][j-1] == 0){
-                        dp[i][j] = dp[i-1][j-1];
-                    }else{
-                        dp[i][j] = (dp[i-1][j-1]+dp[i][j-1])%MOD;
-                    }
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(s[i-1] == t[j-1]){
+                    dp[i][j] = dp[i-1][j-1]+dp[i-1][j];
                 }else{
-                    dp[i][j] = dp[i][j-1];
+                    dp[i][j] = dp[i-1][j];
                 }
             }
         }
-        // for(int i=1;i<m+1;i++){
-        //     for(int j=1;j<n+1;j++){
-        //         cout<<dp[i][j]<<" ";
-        //     }
-        //     cout<<endl;
-        // }
-        return dp[m][n];
+        return (int)dp[n][m];
     }
     
 };
