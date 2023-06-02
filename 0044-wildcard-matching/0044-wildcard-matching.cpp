80e358a9a3452c88d2d5220cb0c55ccdd9dc19e9
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-06-02 08:20:37 +0530

    Time: 67 ms (61.22%), Space: 27.5 MB (35.10%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     bool solve(string &s1 , string &s2 , int i , int j , vector<vector<int>>& dp){
-        int n = s1.size() , m = s2.size();
-        if(i==n-1 && j==m-1){
+        if(i==0 && j==0){
             if(s2[j] == '*' || s1[i] == s2[j] || s2[j] == '?'){
                 return true;
             }
             return false;
         }
-        if(i>=n || j>=m){
-            if(j<m){
-                while(s2[j] == '*'){
-                    j++;
+        if(i<0 || j<0){
+            if(j>=0){
+                // cout<<"Hello"<<endl;
+                while(j>= 0 && s2[j] == '*'){
+                    j--;
                 }
-                if(j == m){
+                if(j < 0){
                     return true;
                 }
             }
@@ -25,17 +25,17 @@ public:
         bool ans = false;
         if(s1[i] != s2[j]){
             if(s2[j] == '?'){
-                ans |= solve(s1,s2,i+1,j+1 , dp);
+                ans |= solve(s1,s2,i-1,j-1 , dp);
             }else if(s2[j] == '*'){
-                ans |= solve(s1,s2,i+1,j,dp);
-                ans |= solve(s1,s2,i+1,j+1 , dp);
-                ans |= solve(s1,s2,i,j+1 , dp);
+                ans |= solve(s1,s2,i-1,j,dp);
+                ans |= solve(s1,s2,i-1,j-1 , dp);
+                ans |= solve(s1,s2,i,j-1 , dp);
             }
             else{
                 return false;
             }
         }else{
-            ans |= solve(s1,s2,i+1,j+1 , dp);
+            ans |= solve(s1,s2,i-1,j-1 , dp);
         }
         return dp[i][j] = ans;
     }
@@ -53,6 +53,9 @@ public:
             return s.size() == 0;
         }
         vector<vector<int>> dp(n , vector<int>(m , -1));
-        return solve(s, p , 0 , 0 , dp);
+        return solve(s, p , n-1, m-1 , dp);
     }
 };
+
+
+
