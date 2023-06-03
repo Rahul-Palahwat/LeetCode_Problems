b9fd332cfa2bb62ef970d5653f81c9f3cad493c9
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-06-03 12:05:35 +0530

    Time: 12 ms (33.08%), Space: 15.3 MB (21.59%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,29 +1,54 @@
 class Solution {
 public:
+    // Recursion + Memoization
+    // int solve(vector<int>& prices , int index , bool check , vector<vector<int>>& dp){
+    //     if(index >= prices.size()){
+    //         return 0;
+    //     }
+    //     if(dp[index][check] != -1){
+    //         return dp[index][check];
+    //     }
+    //     int profit = 0;
+    //     if(check){
+    //         int notTaken = solve(prices , index+1 , check , dp);
+    //         int taken =prices[index] + solve(prices , index+1 , false , dp);
+    //         profit = max(notTaken , taken);
+    //     }else{
+    //         int notTaken = solve(prices , index+1 , check , dp);
+    //         int taken = - prices[index] + solve(prices , index+1 , true , dp);
+    //         profit = max(taken , notTaken);
+    //     }
+    //     return dp[index][check] = profit;
+    // }
+    // int maxProfit(vector<int>& prices) {
+    //     int n = prices.size();
+    //     vector<vector<int>> dp(n , vector<int>(2,-1)); 
+    //     return solve(prices , 0 , false , dp);
+    // }
     
-    int solve(vector<int>& prices , int index , bool check , vector<vector<int>>& dp){
-        if(index >= prices.size()){
-            return 0;
-        }
-        if(dp[index][check] != -1){
-            return dp[index][check];
-        }
-        int profit = 0;
-        if(check){
-            int notTaken = solve(prices , index+1 , check , dp);
-            int taken =prices[index] + solve(prices , index+1 , false , dp);
-            profit = max(notTaken , taken);
-        }else{
-            int notTaken = solve(prices , index+1 , check , dp);
-            int taken = - prices[index] + solve(prices , index+1 , true , dp);
-            profit = max(taken , notTaken);
-        }
-        return dp[index][check] = profit;
-    }
+    
+    
+    // Tabulation
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n , vector<int>(2,-1)); 
-        return solve(prices , 0 , false , dp);
+        vector<vector<int>> dp(n+1 , vector<int>(2,0));
+        dp[n][0] = 0 , dp[n][1] = 0;
+        for(int index=n-1;index>=0;index--){
+            for(int check=0;check<=1;check++){
+                int profit = INT_MIN;
+                if(check){
+                    int notTaken = dp[index+1][1];
+                    int taken =prices[index] + dp[index+1][0];
+                    profit = max(notTaken , taken);
+                }else{
+                    int notTaken = dp[index+1][0];
+                    int taken = - prices[index] + dp[index+1][1];
+                    profit = max(taken , notTaken);
+                }
+                dp[index][check] = profit;
+            }
+        }
+        return dp[0][0];
     }
     
     
