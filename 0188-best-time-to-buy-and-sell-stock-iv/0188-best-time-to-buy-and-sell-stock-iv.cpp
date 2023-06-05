6fd9b1f7cb97f50b994b3c2468c9913a57e8e431
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-06-05 09:06:29 +0530

    Time: 11 ms (87.53%), Space: 11 MB (82.35%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -25,36 +25,41 @@ public:
     
     
     // Tabulation
-    int solve(int ind , int k , bool buy , vector<int>& prices , vector<vector<vector<int>>> &dp){
-        if(ind >= prices.size()){
-            return 0;
-        }
-        if(k<=0){
-            return 0;
-        }
-        if(dp[ind][buy][k] != -1){
-            return dp[ind][buy][k];
-        }
-        if(buy){
-            return dp[ind][buy][k] = max((-prices[ind] + solve(ind+1 , k , false , prices , dp)) , (solve(ind+1 , k , buy , prices , dp)));
-        }else{
-            return dp[ind][buy][k] =max((prices[ind]+solve(ind+1 , k-1 , true , prices , dp)) , solve(ind+1 , k , buy , prices , dp));
-        }
-    }
+    // int maxProfit(int cnt, vector<int>& prices) {
+    //     int n = prices.size();
+    //     vector<vector<vector<int>>> dp(n+1 , vector<vector<int>>(2 , vector<int>(cnt+1 , 0)));
+    //     for(int ind = n-1;ind>=0;ind--){
+    //         for(int buy = 0;buy<=1;buy++){
+    //             for(int k=cnt;k>0;k--){
+    //                 if(buy){
+    //                     dp[ind][buy][k] = max((-prices[ind] + dp[ind+1][false][k]),dp[ind+1][buy][k]);
+    //                 }else{
+    //                     dp[ind][buy][k] =max((prices[ind]+dp[ind+1][true][k-1]) , dp[ind+1][buy][k]);
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     return dp[0][true][cnt];
+    // }
+    
+    
+    // Tabulation + Optimization
     int maxProfit(int cnt, vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n+1 , vector<vector<int>>(2 , vector<int>(cnt+1 , 0)));
+        vector<vector<int>> prev(2 , vector<int>(cnt+1 , 0));
+        vector<vector<int>> cur(2 , vector<int>(cnt+1 , 0));
         for(int ind = n-1;ind>=0;ind--){
             for(int buy = 0;buy<=1;buy++){
                 for(int k=cnt;k>0;k--){
                     if(buy){
-                        dp[ind][buy][k] = max((-prices[ind] + dp[ind+1][false][k]),dp[ind+1][buy][k]);
+                        cur[buy][k] = max((-prices[ind] + prev[false][k]),prev[buy][k]);
                     }else{
-                        dp[ind][buy][k] =max((prices[ind]+dp[ind+1][true][k-1]) , dp[ind+1][buy][k]);
+                        cur[buy][k] =max((prices[ind]+prev[true][k-1]) , prev[buy][k]);
                     }
                 }
             }
+            prev = cur;
         }
-        return dp[0][true][cnt];
+        return cur[true][cnt];
     }
 };
