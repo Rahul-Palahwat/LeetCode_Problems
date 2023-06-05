9bd61e26c5e8cf0ad9df80555276ef4893135317
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-06-05 23:38:09 +0530

    Time: 13 ms (13.74%), Space: 11.8 MB (63.11%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,5 +1,35 @@
 class Solution {
 public:
+    
+    // Recursion + Memoization
+    // int solve(int index , bool buy, vector<int>& prices , vector<vector<int>>& dp){
+    //     if(index >= prices.size()){
+    //         return 0;
+    //     }
+    //     int profit = 0;
+    //     if(dp[index][buy] != -1){
+    //         return dp[index][buy];
+    //     }
+    //     if(buy){
+    //         int taken = -prices[index] + solve(index+1 , false , prices , dp);
+    //         int notTaken = solve(index+1 , buy , prices , dp);
+    //         profit = max(taken , notTaken);
+    //     }else{
+    //         int taken = prices[index]+solve(index+2 , true , prices , dp);
+    //         int notTaken = solve(index+1 , false , prices , dp);
+    //         profit = max(taken , notTaken);
+    //     }
+    //     return dp[index][buy] = profit;
+    // }
+    // int maxProfit(vector<int>& prices) {
+    //     int n = prices.size();
+    //     vector<vector<int>> dp(n , vector<int>(2,-1));
+    //     return solve(0 , true , prices , dp);
+    // }
+    
+    
+    
+    // Tabulation
     int solve(int index , bool buy, vector<int>& prices , vector<vector<int>>& dp){
         if(index >= prices.size()){
             return 0;
@@ -21,7 +51,22 @@ public:
     }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n , vector<int>(2,-1));
-        return solve(0 , true , prices , dp);
+        vector<vector<int>> dp(n+2 , vector<int>(2,0));
+        for(int index = n-1;index>=0;index--){
+            for(int buy=1;buy>=0;buy--){
+                int profit = 0;
+                if(buy){
+                    int taken = -prices[index] + dp[index+1][false];
+                    int notTaken = dp[index+1][buy];
+                    profit = max(taken , notTaken);
+                }else{
+                    int taken = prices[index]+dp[index+2][true];
+                    int notTaken = dp[index+1][false];
+                    profit = max(taken , notTaken);
+                }
+                dp[index][buy] = profit;
+            }
+        }
+        return dp[0][true];
     }
 };
