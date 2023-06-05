2fbbea3890796a091015739852d8f4691ad87b64
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-06-05 08:15:14 +0530

    Time: 785 ms (35.11%), Space: 207.1 MB (44.80%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -33,28 +33,6 @@ public:
     
     
     // Tabulation
-    int solve(vector<int>& prices , int index , int buy , int cnt,vector<vector<vector<int>>>& dp){
-        if(index == prices.size()){
-            return 0;
-        }
-        if(cnt >= 2){
-            return 0;
-        }
-        if(dp[index][buy][cnt] != -1){
-            return dp[index][buy][cnt];
-        }
-        int profit = 0;
-        if(buy){
-            int take = -prices[index] + solve(prices , index+1 , false , cnt,dp);
-            int notTake = solve(prices , index+1 , buy , cnt,dp);
-            profit = max(take , notTake);
-        }else{
-            int take = prices[index]+solve(prices , index+1 , true , cnt+1,dp);
-            int notTake = solve(prices , index+1 , buy , cnt,dp);
-            profit = max(take , notTake);
-        }
-        return dp[index][buy][cnt] = profit;
-    }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
         vector<vector<vector<int>>> dp(n+1 , vector<vector<int>>(2 , vector<int>(3,0)));
