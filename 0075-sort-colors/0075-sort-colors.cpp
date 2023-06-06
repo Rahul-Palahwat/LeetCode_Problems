6fe6bfbd81cc0f9aa759815535a251ed406b476c
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-06-07 00:01:45 +0530

    Time: 6 ms (24.03%), Space: 8.3 MB (51.40%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,40 +1,28 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        // 1st method 
-        // int zero=0,one=0,two=0;
-        // for(auto i=0;i<nums.size();i++){
-        //     if(nums[i]==0){
-        //         zero++;
-        //     }else if(nums[i]==1){
-        //         one++;
-        //     }else{
-        //         two++;
-        //     }
-        // }
-        // for(auto i=0;i<nums.size();i++){
-        //     if(zero>0){
-        //         nums[i]=0;
-        //         zero--;
-        //     }else if(one>0){
-        //         nums[i]=1;
-        //         one--;
-        //     }else{
-        //         nums[i]=2;
-        //     }
-        // }
-        
-        // 2nd method
-        map<int,int> mp;
-        for(int i=0;i<nums.size();i++){
-            mp[nums[i]]++;
+        // Variables to keep the track of count of zero , ones , twos
+        int zero=0,one=0,two=0;
+        // Iterate over array nums
+        for(auto i=0;i<nums.size();i++){
+            if(nums[i]==0){
+                zero++;
+            }else if(nums[i]==1){
+                one++;
+            }else{
+                two++;
+            }
         }
-        int i=0;
-        for(auto it: mp){
-            int count = it.second;
-            while(count--){
-                nums[i] = it.first;
-                i++;
+        // Now update the nums array with varibales
+        for(auto i=0;i<nums.size();i++){
+            if(zero>0){
+                nums[i]=0;
+                zero--;
+            }else if(one>0){
+                nums[i]=1;
+                one--;
+            }else{
+                nums[i]=2;
             }
         }
         return;
