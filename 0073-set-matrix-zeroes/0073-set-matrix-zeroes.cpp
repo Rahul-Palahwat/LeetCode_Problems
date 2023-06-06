ee25f50d00a2e6c619591bf05556d3b9ab75a6bc
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-06-06 23:30:54 +0530

    Time: 20 ms (33.21%), Space: 13.4 MB (15.73%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int row[matrix.size()];
-        int col[matrix[0].size()];
-        memset(row,0,sizeof(row));
-        memset(col,0,sizeof(col));
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[0].size();j++){
+        int n = matrix.size();
+        int m = matrix[0].size();
+        // Vector for storing rows that will be zero
+        vector<int> row(n , 0);
+        // Vector for string column that will have zero
+        vector<int> col(m , 0);
+        // loop to update the row and col vector
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(matrix[i][j] == 0){
+                    // Increment the value from 0 to track the row
                     row[i]++;
+                    // Increment the value at jth column from 0 to track the column
                     col[j]++;
                 }
             }
         }
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[0].size();j++){
-                if(row[i]>0 || col[j]>0){
+        // Now updation to the matrix
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                // If we have encountered any row i or col j then we will update
+                if(row[i] > 0 || col[j] > 0){
                     matrix[i][j] = 0;
                 }
             }
         }
-        
-        
         return;
     }
 };
