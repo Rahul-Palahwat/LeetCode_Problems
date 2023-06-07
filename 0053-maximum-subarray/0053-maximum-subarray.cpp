f34dab5dd61a2952c1b57c43af5956f1c1f37bce
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-06-07 22:18:26 +0530

    Time: 303 ms (5.14%), Space: 117.9 MB (6.23%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,17 +1,48 @@
 class Solution {
-public: 
-    int maxSubArray(vector<int>& nums) {
-        int dp[nums.size()];
-        dp[0]=nums[0];
-        int ans= nums[0];
-        for(int i=1;i<nums.size();i++){
-            if((dp[i-1] + nums[i]) > 0){
-                dp[i] = max(nums[i],dp[i-1]+nums[i]);
-            }else{
-                dp[i]=nums[i];
+public:
+    // int solve(int index , bool taken , vector<int>& nums){
+    //     if(index < 0){
+    //         if(taken){
+    //             return 0;
+    //         }
+    //         return INT_MIN;
+    //     }
+    //     int ans = 0;
+    //     if(taken){
+    //         ans = max(nums[index]+solve(index-1 , taken , nums) , 0);
+    //     }else{
+    //         ans = max(nums[index]+solve(index-1 , true , nums) , solve(index-1 , taken , nums));
+    //     }
+    //     return ans;
+    // }
+    // int maxSubArray(vector<int>& nums) {
+    //     int n = nums.size();
+    //     return solve(n-1 , false , nums);
+    // }
+    
+    
+    
+    int solve(int index , bool taken , vector<int>& nums , vector<vector<int>>& dp){
+        if(index < 0){
+            if(taken){
+                return 0;
             }
-            ans=max(ans,dp[i]);
+            return INT_MIN;
         }
-        return ans;
+        if(dp[index][taken] != -1){
+            return dp[index][taken];
+        }
+        int ans = 0;
+        if(taken){
+            ans = max(nums[index]+solve(index-1 , taken , nums , dp) , 0);
+        }else{
+            ans = max(nums[index]+solve(index-1 , true , nums , dp) , solve(index-1 , taken , nums , dp));
+        }
+        return dp[index][taken] = ans;
+    }
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n , vector<int>(2,-1));
+        return solve(n-1 , false , nums , dp);
     }
 };
