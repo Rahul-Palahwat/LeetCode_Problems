e5d7ee986421b2a78b1aaf569089f3c29b3ade2a
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-06-08 00:12:52 +0530

    Time: 135 ms (17.17%), Space: 67.7 MB (45.88%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -22,27 +22,40 @@ public:
     
     
     
-    int solve(int index , bool taken , vector<int>& nums , vector<vector<int>>& dp){
-        if(index < 0){
-            if(taken){
-                return 0;
-            }
-            return INT_MIN;
-        }
-        if(dp[index][taken] != -1){
-            return dp[index][taken];
-        }
-        int ans = 0;
-        if(taken){
-            ans = max(nums[index]+solve(index-1 , taken , nums ,dp) , 0);
-        }else{
-            ans = max(nums[index]+solve(index-1 , true , nums , dp) , solve(index-1 , taken , nums , dp));
-        }
-        return dp[index][taken] = ans;
-    }
+    // int solve(int index , bool taken , vector<int>& nums , vector<vector<int>>& dp){
+    //     if(index < 0){
+    //         if(taken){
+    //             return 0;
+    //         }
+    //         return INT_MIN;
+    //     }
+    //     if(dp[index][taken] != -1){
+    //         return dp[index][taken];
+    //     }
+    //     int ans = 0;
+    //     if(taken){
+    //         ans = max(nums[index]+solve(index-1 , taken , nums ,dp) , 0);
+    //     }else{
+    //         ans = max(nums[index]+solve(index-1 , true , nums , dp) , solve(index-1 , taken , nums , dp));
+    //     }
+    //     return dp[index][taken] = ans;
+    // }
+    // int maxSubArray(vector<int>& nums) {
+    //     int n = nums.size();
+    //     vector<vector<int>> dp(n , vector<int>(2,-1));
+    //     return solve(n-1 , false , nums , dp);
+    // }
+    
+    
+    
+    // Tabulation 
     int maxSubArray(vector<int>& nums) {
+        int ans = nums[0];
         int n = nums.size();
-        vector<vector<int>> dp(n , vector<int>(2,-1));
-        return solve(n-1 , false , nums , dp);
+        for(int i=1;i<n;i++){
+            nums[i] = max(nums[i] , nums[i]+nums[i-1]);
+            ans = max(ans , nums[i]);
+        }
+        return ans;
     }
 };
