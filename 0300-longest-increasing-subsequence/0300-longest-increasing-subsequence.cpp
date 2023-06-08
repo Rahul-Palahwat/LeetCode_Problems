c4dea9ebaa966f91da5218c216de563d638e1dbd
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-06-08 07:37:17 +0530

    Time: 359 ms (49.27%), Space: 10.5 MB (66.71%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -29,28 +29,49 @@
 
 
 // Tabulation 
+// class Solution {
+// public:
+//     int lengthOfLIS(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<vector<int>> dp(n+1 , vector<int>(n+1 , 0));
+//         for(int ind = n-1; ind>=0; ind --){
+//             for (int prev_index = ind-1; prev_index >=-1; prev_index--){
+
+//                 int notTake = 0 + dp[ind+1][prev_index +1];
+
+//                 int take = 0;
+
+//                 if(prev_index == -1 || nums[ind] > nums[prev_index]){
+
+//                     take = 1 + dp[ind+1][ind+1];
+//                 }
+
+//                 dp[ind][prev_index+1] = max(notTake,take);
+
+//             }
+//         }
+    
+//         return dp[0][0];
+//     }
+// };
+
+
+
+// Tabulation 1D DP
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n+1 , vector<int>(n+1 , 0));
-        for(int ind = n-1; ind>=0; ind --){
-            for (int prev_index = ind-1; prev_index >=-1; prev_index--){
-
-                int notTake = 0 + dp[ind+1][prev_index +1];
-
-                int take = 0;
-
-                if(prev_index == -1 || nums[ind] > nums[prev_index]){
-
-                    take = 1 + dp[ind+1][ind+1];
+        vector<int> dp(n , 1);
+        int ans = 1;
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[j] < nums[i]){
+                    dp[i] = max(dp[i] , 1+dp[j]);
+                    ans=max(ans , dp[i]);
                 }
-
-                dp[ind][prev_index+1] = max(notTake,take);
-
             }
         }
-    
-        return dp[0][0];
+        return ans;
     }
 };
