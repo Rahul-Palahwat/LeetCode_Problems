1fe2606e57be61e312a058f16c4bf469395cede3
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-06-08 07:39:37 +0530

    Time: 10 ms (85.82%), Space: 10.3 MB (85.66%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -58,20 +58,41 @@
 
 
 // Tabulation 1D DP
+// class Solution {
+// public:
+//     int lengthOfLIS(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int> dp(n , 1);
+//         int ans = 1;
+//         for(int i=1;i<n;i++){
+//             for(int j=0;j<i;j++){
+//                 if(nums[j] < nums[i]){
+//                     dp[i] = max(dp[i] , 1+dp[j]);
+//                     ans=max(ans , dp[i]);
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+
+
+// Tabulation + Recursion
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n , 1);
-        int ans = 1;
+        vector<int> dp;
+        dp.push_back(nums[0]);
         for(int i=1;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(nums[j] < nums[i]){
-                    dp[i] = max(dp[i] , 1+dp[j]);
-                    ans=max(ans , dp[i]);
-                }
+            if(nums[i] > dp.back()){
+                dp.push_back(nums[i]);
+            }else{
+                int index = lower_bound(dp.begin() , dp.end() , nums[i]) - dp.begin();
+                dp[index] = nums[i];
             }
         }
-        return ans;
+        return dp.size();
     }
 };
