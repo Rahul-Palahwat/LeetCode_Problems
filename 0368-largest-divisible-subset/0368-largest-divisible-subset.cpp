52cc44ace556fc9d7a1cfbd376e09e382a0ed719
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-06-09 09:33:21 +0530

    Time: 43 ms (98.20%), Space: 8.9 MB (66.54%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -1,83 +1,35 @@
 class Solution {
 public:
-//     vector<vector<int>> ans;
-//     bool check(vector<int> temp, int num){
-//         for(auto it: temp){
-//             if((it%num != 0) &&(num%it !=0)){
-//                 return false;
-//             }
-//         }
-//         return true;
-//     }
-//     void solve(vector<int> nums, int index,vector<int> temp){
-//         if(index == nums.size()){
-//             ans.push_back(temp);
-//             return;
-//         }
-//         if(check(temp,nums[index])){
-//             solve(nums,index+1,temp);
-//             temp.push_back(nums[index]);
-//             solve(nums,index+1,temp);
-//         }else{
-//             solve(nums,index+1,temp);
-//         }
-        
-//         return;
-//     }
-//     vector<int> largestDivisibleSubset(vector<int>& nums) {
-//         vector<int> temp;
-//         solve(nums,0,temp);
-//         int sz = 0;
-//         for(auto it: ans){
-//             if(it.size() > sz){
-//                 temp = it;
-//                 sz = it.size();
-//             }
-//         }
-//         return temp;
-//     }
-    
     vector<int> largestDivisibleSubset(vector<int>& nums) {
-        if(nums.size()==0){
-            return nums;
-        }
-        sort(nums.begin(),nums.end());
-        int flag=1;
-        if(nums[0]==1){
-            flag=0;
-        }
-        else{
-            nums.push_back(1);
-            flag=1;
-        }
-        sort(nums.begin(),nums.end());
-        int i,j;
-        int dp[nums.size()][2];
-        for(i=nums.size()-1;i>=0;i--){
-            dp[i][0]=0;
-            dp[i][1]=i;
-            for(j=i+1;j<nums.size();j++){
-                if((nums[j]%nums[i])==0){
-                    if(dp[j][0]>dp[i][0]){
-                        dp[i][0]=dp[j][0];
-                        dp[i][1]=j;
-                    }  
+        int n = nums.size();
+        sort(nums.begin() , nums.end());
+        vector<int> dp(n , 1);
+        vector<int> ans;
+        int index=0,maxi = 1;
+        vector<int> track(n,0);
+        for(int i=1;i<n;i++){
+            track[i] = i;
+            for(int j=0;j<i;j++){
+                if(nums[i]%nums[j] == 0){
+                    if(dp[i] < 1+dp[j]){
+                        track[i] = j;
+                    }
+                    dp[i] = max(dp[i] , 1+dp[j]);
+                    if(maxi < dp[i]){
+                        maxi = dp[i];
+                        index = i;
+                    }
                 }
             }
-            dp[i][0]++;
         }
-        i=0;
-        vector<int> t;
-        t.push_back(nums[i]);
-        while(dp[i][1]!=i){
-            i=dp[i][1];
-            t.push_back(nums[i]);
-            
+        while(index >= 0){
+            ans.push_back(nums[index]);
+            if(track[index] == index){
+                break;
+            }
+            index = track[index];
         }
-        if(flag==1){
-            t.erase(t.begin());
-        }
-        return t;
-        
+        reverse(ans.begin() , ans.end());
+        return ans;
     }
 };
