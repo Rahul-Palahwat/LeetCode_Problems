9c524df8aa4937d68e514650b34468e90415a622
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-06-09 22:56:46 +0530

    Time: 21 ms (63.34%), Space: 19.5 MB (61.77%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,24 +1,37 @@
 class Solution {
 public:
+    // Bruteforce TC -> O(N^2)
+    // int majorityElement(vector<int>& nums) {
+    //     int cnt = 0;
+    //     int n = nums.size();
+    //     for(int i=0;i<n;i++){
+    //         cnt=1;
+    //         for(int j=i+1;j<n;j++){
+    //             if(nums[i] == nums[j]){
+    //                 cnt++;
+    //             }
+    //         }
+    //         if(cnt > n/2){
+    //             return nums[i];
+    //         }
+    //     }
+    //     return -1;
+    // }
+    
+    
+    // HashMap TC-> O(NlogN) , SC-> O(N)
     int majorityElement(vector<int>& nums) {
-        long long int count=1,max=0,element;
-        sort(nums.begin(),nums.end());
-        if(nums.size()==1){
-            return nums[0];
+        int n = nums.size();
+        map<int,int> mp;
+        for(auto it: nums){
+            mp[it]++;
         }
-        for(int i=1;i<nums.size();i++){
-            if(nums[i]==nums[i-1]){
-                count++;
-                if(count>max){
-                    max=count;
-                    element=nums[i];
-                }
-            }else{
-                count=2;
+        for(auto it: mp){
+            if(it.second > n/2){
+                return it.first;
             }
-            
         }
-        return element;
+        return -1;
     }
         
 };
