bcfa5f06aca1a605310b58108b9557b4265e82e3
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-06-09 23:17:07 +0530

    Time: 27 ms (25.02%), Space: 19.5 MB (61.77%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -20,16 +20,46 @@ public:
     
     
     // HashMap TC-> O(NlogN) , SC-> O(N)
+    // int majorityElement(vector<int>& nums) {
+    //     int n = nums.size();
+    //     map<int,int> mp;
+    //     for(auto it: nums){
+    //         mp[it]++;
+    //     }
+    //     for(auto it: mp){
+    //         if(it.second > n/2){
+    //             return it.first;
+    //         }
+    //     }
+    //     return -1;
+    // }
+    
+    
+    // Using Moore's Voting Algorithm
     int majorityElement(vector<int>& nums) {
+        int cnt=1;
         int n = nums.size();
-        map<int,int> mp;
-        for(auto it: nums){
-            mp[it]++;
-        }
-        for(auto it: mp){
-            if(it.second > n/2){
-                return it.first;
+        int start=nums[0];
+        for(int i=1;i<n;i++){
+            if(nums[i] != start){
+                cnt--;
+            }else{
+                cnt++;
             }
+            if(cnt == 0 && i<n){
+                start = nums[i+1];
+                cnt=1;
+                i++;
+            }
+        }
+        cnt=0;
+        for(auto it: nums){
+            if(it == start){
+                cnt++;
+            }
+        }
+        if(cnt > n/2){
+            return start;
         }
         return -1;
     }
