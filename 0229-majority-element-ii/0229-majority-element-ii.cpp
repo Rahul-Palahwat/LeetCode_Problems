ce8579be525a4b269c634c39715670eb6552a64c
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-06-09 23:33:33 +0530

    Time: 21 ms (14.98%), Space: 16 MB (35.93%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,65 +1,40 @@
 class Solution {
 public:
-    // First approach 
-//     vector<int> majorityElement(vector<int>& nums) {
-//         long long int count=1,max,flag=0,i,j=0,k,l,ans;
-//         vector<int> a;
-//         sort(nums.begin(),nums.end());
-//         if(nums.size()==1){
-//             a.push_back(nums[0]);
-//             return a;
-//         }
-//         if(nums.size()==2){
-//             if(nums[1]==nums[0]){
-//                 a.push_back(nums[0]);
-//                 return a;
-//             }else{
-//                 a.push_back(nums[0]);
-//                 a.push_back(nums[1]);
-//                 return a;
-//             }
-//         }
-        
-//         for(int i=1;i<nums.size();i++){
-//             if(nums[i]==nums[i-1]){
-//                 count++;
-//                 if(count>(nums.size()/3)&&flag==0){
-//                     a.push_back(nums[i]);
-//                     flag=1;
-//                 }
-//             }
-//             else{
-//                 count=1;
-//                 flag=0;
-//             }
-//         }
-//         return a;
-//     }
+    // BruteForce TC -> O(N^2) 
+    // vector<int> majorityElement(vector<int>& nums) {
+    //     set<int> ans;
+    //     int n = nums.size();
+    //     int cnt = 0;
+    //     for(int i=0;i<n;i++){
+    //         cnt = 1;
+    //         for(int j=i+1;j<n;j++){
+    //             if(nums[i] == nums[j]){
+    //                 cnt++;
+    //             }
+    //         }
+    //         if(cnt > n/3){
+    //             ans.insert(nums[i]);
+    //         }
+    //     }
+    //     vector<int> finalAns;
+    //     for(auto it: ans){
+    //         finalAns.push_back(it);
+    //     }
+    //     return finalAns;
+    // }
     
     
-    // again solving 
+    // HashMap TC-> O(NlogN) , SC -> O(N)
     vector<int> majorityElement(vector<int>& nums) {
         vector<int> ans;
-        sort(nums.begin(),nums.end());
-        int count=1;
-        if(nums.size()==1){
-            return nums;
+        int n = nums.size();
+        map<int,int> mp;
+        for(int i=0;i<n;i++){
+            mp[nums[i]]++;
         }
-        for(int i=0;i<nums.size();i++){
-            if(count>nums.size()/3){
-                if(ans.size()>0){
-                    if(ans[ans.size()-1] != nums[i]){
-                        ans.push_back(nums[i]);
-                    }
-                }else{
-                    ans.push_back(nums[i]);
-                }
-            }
-            if(i<nums.size()-1)
-            if(nums[i] == nums[i+1]){
-                count++;
-            }else{
-                count=1;
+        for(auto it: mp){
+            if(it.second > n/3){
+                ans.push_back(it.first);
             }
         }
         return ans;
