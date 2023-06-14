a715f28643e62d0432f59170020ebc1dc20f546c
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-06-14 06:47:12 +0530

    Time: 346 ms (86.33%), Space: 10.3 MB (41.45%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -81,20 +81,6 @@ public:
     
     
     // Tabulation
-    int solve(vector<int>& nums , int i , int j, vector<vector<int>> &dp){
-        if(i>j){
-            return 0;
-        }
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        int ans = INT_MIN;
-        for(int k=i;k<=j;k++){
-            int temp = nums[i-1]*nums[k]*nums[j+1] + solve(nums,i,k-1,dp)+solve(nums , k+1 , j , dp);
-            ans = max(ans , temp);
-        }
-        return dp[i][j] = ans;
-    }
     int maxCoins(vector<int>& nums) {
         int sz = nums.size();
         nums.push_back(1);
