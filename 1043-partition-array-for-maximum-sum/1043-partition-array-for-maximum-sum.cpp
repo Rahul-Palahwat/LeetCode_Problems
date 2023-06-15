cd2281525daf70c52565a23cec43993f7af158f2
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-06-15 16:22:57 +0530

    Time: 15 ms (78.68%), Space: 8.5 MB (84.39%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -1,26 +1,48 @@
 class Solution {
 public:
-    int solve(int i, int k , vector<int>& arr , vector<int> &dp){
-        if(i == arr.size()){
-            return 0;
-        }
-        if(dp[i] != -1){
-            return dp[i];
-        }
-        int ans = INT_MIN;
-        int j=0,maxi = arr[i];
-        for(int index=i;index<arr.size() && j<k;index++){
-            maxi = max(maxi , arr[index]);
-            int temp = (index+1-i)*maxi + solve(index+1 , k , arr , dp);
-            // cout<<temp<<" "<<i<<endl;
-            ans = max(ans , temp);
-            j++;
-        }
-        return dp[i] = ans;
-    }
+    // Recursion + Memoization
+    // int solve(int i, int k , vector<int>& arr , vector<int> &dp){
+    //     if(i == arr.size()){
+    //         return 0;
+    //     }
+    //     if(dp[i] != -1){
+    //         return dp[i];
+    //     }
+    //     int ans = INT_MIN;
+    //     int j=0,maxi = arr[i];
+    //     for(int index=i;index<arr.size() && j<k;index++){
+    //         maxi = max(maxi , arr[index]);
+    //         int temp = (index+1-i)*maxi + solve(index+1 , k , arr , dp);
+    //         // cout<<temp<<" "<<i<<endl;
+    //         ans = max(ans , temp);
+    //         j++;
+    //     }
+    //     return dp[i] = ans;
+    // }
+    // int maxSumAfterPartitioning(vector<int>& arr, int k) {
+    //     int n = arr.size();
+    //     vector<int> dp(n , -1);
+    //     return solve(0 , k , arr , dp);
+    // }
+    
+    
+    
+    // Tabulation
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n = arr.size();
-        vector<int> dp(n , -1);
-        return solve(0 , k , arr , dp);
+        vector<int> dp(n+1 , 0);
+        for(int i=n-1;i>=0;i--){
+            int ans = INT_MIN;
+            int j=0,maxi = arr[i];
+            for(int index=i;index<arr.size() && j<k;index++){
+                maxi = max(maxi , arr[index]);
+                int temp = (index+1-i)*maxi + dp[index+1];
+                // cout<<temp<<" "<<i<<endl;
+                ans = max(ans , temp);
+                j++;
+            }
+            dp[i] = ans;
+        }
+        return dp[0];
     }
 };
