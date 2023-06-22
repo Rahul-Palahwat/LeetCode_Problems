b1156ab7bb939dcfa34f413a6f943b6eee34f00a
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-06-22 20:43:44 +0530

    Time: 286 ms (7.64%), Space: 94.8 MB (28.01%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
-    // Recursion + Memoization
+    int solve(int index , bool buy , vector<int>& prices , int fee , vector<vector<int>> &dp){
+        if(index == prices.size()){
+            return 0;
+        }
+        if(dp[index][buy] != -1){
+            return dp[index][buy];
+        }
+        if(buy){
+            return dp[index][buy] = max(-prices[index] + solve(index+1 , false , prices , fee,dp) , solve(index+1 , buy , prices , fee ,dp));
+        }
+        return dp[index][buy] = max(prices[index]-fee+solve(index+1 , true , prices , fee , dp) , solve(index+1 , buy , prices , fee , dp));
+    }
+    int maxProfit(vector<int>& prices, int fee) {
+        int n = prices.size();
+        vector<vector<int>> dp(n , vector<int>(2 , -1));
+        return solve(0 , true , prices , fee , dp);
+    }
+};
+
+
+
+
+// Recursion + Memoization
     // int solve(int index , bool buy , vector<int>& prices , int fee , vector<vector<int>>& dp){
     //     if(index == prices.size()){
     //         return 0;
@@ -21,18 +43,17 @@ public:
     
     
     // Tabulation
-    int maxProfit(vector<int>& prices, int fee) {
-        int n = prices.size();
-        vector<vector<int>> dp(n+1 , vector<int>(2,0));
-        for(int index = n-1;index>=0;index--){
-            for(int buy = 1;buy >=0;buy--){
-                if(buy){
-                    dp[index][buy] = max(-prices[index]+dp[index+1][false],dp[index+1][buy]);
-                }else{
-                    dp[index][buy] = max(prices[index]-fee+dp[index+1][true], dp[index+1][buy]);
-                }
-            }
-        }
-        return dp[0][true];
-    }
-};
+    // int maxProfit(vector<int>& prices, int fee) {
+    //     int n = prices.size();
+    //     vector<vector<int>> dp(n+1 , vector<int>(2,0));
+    //     for(int index = n-1;index>=0;index--){
+    //         for(int buy = 1;buy >=0;buy--){
+    //             if(buy){
+    //                 dp[index][buy] = max(-prices[index]+dp[index+1][false],dp[index+1][buy]);
+    //             }else{
+    //                 dp[index][buy] = max(prices[index]-fee+dp[index+1][true], dp[index+1][buy]);
+    //             }
+    //         }
+    //     }
+    //     return dp[0][true];
+    // }
