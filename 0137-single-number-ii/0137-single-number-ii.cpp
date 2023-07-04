a5d430a47d72d6e2b2ccdb5b38b1179ffa271a90
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-07-04 22:00:22 +0530

    Time: 7 ms (82.86%), Space: 9.6 MB (41.15%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,17 +1,46 @@
 class Solution {
 public:
-    int singleNumber(vector<int>& nums) {
-        int ans;
-        unordered_map<int,int> mp;
-        for(auto it: nums){
-            mp[it]++;
+    // BruteForce
+    // int singleNumber(vector<int>& nums) {
+    //     int ans;
+    //     unordered_map<int,int> mp;
+    //     for(auto it: nums){
+    //         mp[it]++;
+    //     }
+    //     for(auto it: nums){
+    //         if(mp[it] == 1){
+    //             ans = it;
+    //             break;
+    //         }
+    //     }
+    //     return ans;
+    // }
+    
+    
+    
+    int singleNumber(vector<int>& s) 
+    {
+    	vector<int> t(32);////Made a array contain 32 elements.
+    	int sz = s.size();
+    	int i, j, n;
+    	for (i = 0; i < sz; ++i)
+    	{
+    		n = s[i];
+    		for (j = 31; j >= 0; --j)
+    		{
+    			t[j] += n & 1;//Find the last digit.
+    			n >>= 1;
+    			if (!n)
+    				break;
+    	    }
         }
-        for(auto it: nums){
-            if(mp[it] == 1){
-                ans = it;
-                break;
-            }
-        }
-        return ans;
-    }
+	int res = 0;
+	for (j = 31; j >= 0; --j)
+	{
+		n = t[j] % 3;//"3" represents k times. 
+		if (n)
+			res += 1 << (31 - j);
+	}
+	return res;
+}
 };
