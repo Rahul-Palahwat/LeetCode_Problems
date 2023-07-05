8c84ec1ef9ed84f4ed203bf130b5d57bf1dbeab7
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-07-05 08:28:49 +0530

    Time: 36 ms (93.29%), Space: 36.4 MB (98.32%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -1,39 +1,61 @@
 class Solution {
 public:
+    // Brute force
+    // int longestSubarray(vector<int>& nums) {
+    //     int ans = 0 , n = nums.size();
+    //     int cnt=0 , flag=0;
+    //     vector<int> store;
+    //     for(int i=0;i<nums.size();i++){
+    //         if(flag==0 && nums[0] == 0){
+    //             flag=1;
+    //             while(nums[i] == 0){
+    //                 i++;
+    //             }
+    //         }
+    //         if(i == n){
+    //             break;
+    //         }
+    //         cnt=1;
+    //         while(i<n-1 && nums[i] == nums[i+1]){
+    //             i++;
+    //             cnt++;
+    //         }
+    //         store.push_back(cnt);
+    //     }
+    //     for(int i=0;i<store.size();i++){
+    //         if(i%2 == 0){
+    //             ans = max(ans , store[i]);
+    //         }else{
+    //             if(store[i] == 1){
+    //                 if(i<store.size()-1){
+    //                     ans = max(ans , store[i-1]+store[i+1]);
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     if(store.size() == 1 && flag == 0){
+    //         return ans-1;
+    //     }
+    //     return ans;
+    // }
+    
+    
+    
+    // By Sliding window
     int longestSubarray(vector<int>& nums) {
         int ans = 0 , n = nums.size();
-        int cnt=0 , flag=0;
-        vector<int> store;
-        for(int i=0;i<nums.size();i++){
-            if(flag==0 && nums[0] == 0){
-                flag=1;
-                while(nums[i] == 0){
-                    i++;
+        int k=1 , j=0;
+        for(int i=0;i<n;i++){
+            if(nums[i] == 0){
+                k--;
+            }
+            while(k<0){
+                if(nums[j] == 0){
+                    k++;
                 }
+                j++;
             }
-            if(i == n){
-                break;
-            }
-            cnt=1;
-            while(i<n-1 && nums[i] == nums[i+1]){
-                i++;
-                cnt++;
-            }
-            store.push_back(cnt);
-        }
-        for(int i=0;i<store.size();i++){
-            if(i%2 == 0){
-                ans = max(ans , store[i]);
-            }else{
-                if(store[i] == 1){
-                    if(i<store.size()-1){
-                        ans = max(ans , store[i-1]+store[i+1]);
-                    }
-                }
-            }
-        }
-        if(store.size() == 1 && flag == 0){
-            return ans-1;
+            ans = max(ans , i-j);
         }
         return ans;
     }
