29f98c5d905732cd7b8a64d0266af41b481d0bff
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-07-05 10:11:30 +0530

    Time: 11 ms (46.33%), Space: 9.5 MB (72.42%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -21,6 +21,7 @@ public:
     
     
     // Using concept of setBits
+    // Below(Solve) is the function to set bit for the given number
     void solve(long long n , vector<int>& bits){
         int i=0;
         while(n>0){
@@ -34,29 +35,30 @@ public:
     }
     int singleNumber(vector<int>& s) 
     {
+        // Vector to store the cout of set bits at particular index
     	vector<int> bits(32 , 0);
+        // Variable to store minimum from the array for handling negative number case
         int mini = 0;
-        
-        // For handling negative number case
         for(auto it: s){
             mini = min(it , mini);
         }
-        
+        // Loop to iterate over all elements to set their bit count 
         for(auto it: s){
             long long temp = it;
-            // Adding mini to all numbers so that we can get a positive number
+            // Adding mini(as itself a negative number so -) to all numbers so that we can get a positive number
             temp-=mini;
             solve(temp , bits);
         }
         long long two = 1;
         long long ans=0;
         for(int i=0;i<32;i++){
+            // Here we can keep K in place of 3 to handle case for K times
             if((bits[i]%3) != 0){
                 ans+=(two);
             }
-            two*=2;
+            two<<=1;
         }
-        // Returning the
+        // Returning the answer(mini is negative so +) after subtractions of added number 
         return ans+mini;
     }
 };
