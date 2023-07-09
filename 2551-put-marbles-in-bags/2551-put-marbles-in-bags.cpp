08ad537e4959396c055728a8fa346629263282e1
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-07-09 22:06:59 +0530

    Time: 215 ms (54.21%), Space: 69.1 MB (30.64%) - LeetHub

--- 2551-put-marbles-in-bags/2551-put-marbles-in-bags.cpp
+++ 2551-put-marbles-in-bags/2551-put-marbles-in-bags.cpp
@@ -1,38 +1,20 @@
 class Solution {
 public:
-    
-    long long int putMarbles(vector<int>& w, int k) {
-        if(k >= w.size()){
-            return 0;
-         }
-        if(k==1){
-            return 0;
+    long long putMarbles(vector<int>& weights, int k) {
+        long long ans = 0;
+        vector<long long> v;
+        int n = weights.size();
+        for(int i=1;i<n;i++){
+            v.push_back(weights[i]+weights[i-1]);
         }
-        k-=1;
-        long long int maxi = 0,mini = 0;
-        vector<long long int> fir , sec;
-        for(long long int i = 0;i<w.size()-1;i++){
-            int temp = w[i]+w[i+1];
-            // cout<<temp<<endl;
-            sec.push_back(temp);
+        sort(v.begin() , v.end());
+        long long first=0,second = 0;
+        int sz = v.size();
+        for(int i=0;i<min(k-1 , sz);i++){
+            first+=v[i];
+            second+=v[sz-i-1];
         }
-        sort(sec.begin() , sec.end());
-        for(long long int i = 0;i<w.size()-1;i++){
-            int temp = w[i]+w[i+1];
-            // cout<<temp<<endl;
-            fir.push_back(temp);
-        }
-        sort(fir.begin() , fir.end() , greater<long long int>());
-        for(long long int i =0;i<k;i++){
-            maxi = maxi + fir[i];
-            // cout<<maxi<<endl;
-        }
-        for(long long int i =0;i<k;i++){
-            mini = mini + sec[i];
-            // cout<<mini<<endl;
-        } 
-        long long int finalans = maxi - mini;
-        // cout<<finalans<<endl;
-        return finalans;
+        return second-first;
+        // return first-second > 0 ? first-second: 0;
     }
 };
