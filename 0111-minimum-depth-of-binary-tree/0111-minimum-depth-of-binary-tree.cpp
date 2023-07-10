bdd8f2be590a070efca7380d16e61324e81b7956
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-07-10 08:34:47 +0530

    Time: 291 ms (60.65%), Space: 144.6 MB (85.25%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
@@ -12,21 +12,48 @@
 class Solution {
 public:
     
+    // int solve(TreeNode* root){
+    //     if(!root){
+    //         return 0;
+    //     }
+    //     if(root->left == NULL && root->right == NULL){
+    //         return 1;
+    //     }
+    //     int l=INT_MAX, r = INT_MAX;
+    //     if(root->left){
+    //         l = solve(root->left);
+    //     }
+    //     if(root->right){
+    //         r = solve(root->right);
+    //     }
+    //     return 1+min(l,r);
+    // }
+    
     int solve(TreeNode* root){
         if(!root){
             return 0;
         }
-        if(root->left == NULL && root->right == NULL){
-            return 1;
+        queue<TreeNode*> q;
+        q.push(root);
+        int ans = 1;
+        while(!q.empty()){
+            int sz = q.size();
+            while(sz--){
+                auto top = q.front();
+                q.pop();
+                if(top->left == NULL && top->right == NULL){
+                    return ans;
+                }
+                if(top->left){
+                    q.push(top->left);
+                }
+                if(top->right){
+                    q.push(top->right);
+                }
+            }
+            ans++;
         }
-        int l=INT_MAX, r = INT_MAX;
-        if(root->left){
-            l = solve(root->left);
-        }
-        if(root->right){
-            r = solve(root->right);
-        }
-        return 1+min(l,r);
+        return ans;
     }
     int minDepth(TreeNode* root) {
         return solve(root);
