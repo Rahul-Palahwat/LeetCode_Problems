ec21213d771e139d000a1079b3c992ba133f68cc
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-07-15 10:29:38 +0530

    Time: 144 ms (92.10%), Space: 54.9 MB (68.34%) - LeetHub

--- 1218-longest-arithmetic-subsequence-of-given-difference/1218-longest-arithmetic-subsequence-of-given-difference.cpp
+++ 1218-longest-arithmetic-subsequence-of-given-difference/1218-longest-arithmetic-subsequence-of-given-difference.cpp
@@ -24,23 +24,19 @@ public:
     
     
     
-    int longestSubsequence(vector<int>& arr, int difference) {
+    int longestSubsequence(vector<int>& arr, int diff) {
         int n = arr.size();
-        unordered_map<int, int> dp; // Stores the maximum length at each index
-        
-        int ans = 1; // Initialize with the minimum length of 1
-        
-        for (int i = 0; i < n; i++) {
-            int num = arr[i];
-            if (dp.find(num - difference) != dp.end()) {
-                dp[num] = dp[num - difference] + 1;
-            } else {
-                dp[num] = 1;
+        unordered_map<int,int> mp;
+        int ans = 1;
+        for(int i=0;i<n;i++){
+            int back = arr[i]-diff;
+            if(mp.find(back) != mp.end()){
+                mp[arr[i]] = 1+mp[back];
+                ans = max(ans , mp[arr[i]]);
+            }else{
+                mp[arr[i]] = 1;
             }
-            
-            ans = max(ans, dp[num]);
         }
-        
         return ans;
     }
 };
