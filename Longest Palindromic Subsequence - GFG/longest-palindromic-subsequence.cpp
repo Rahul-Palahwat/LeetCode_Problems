af5f2bc8dfa013352dacaba563b7de35e2ac47f3
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-07-20 07:44:48 +0530

    Added solution - LeetHub

--- Longest Palindromic Subsequence - GFG/longest-palindromic-subsequence.cpp
+++ Longest Palindromic Subsequence - GFG/longest-palindromic-subsequence.cpp
@@ -10,27 +10,37 @@ using namespace std;
 
 class Solution{
   public:
-    int solve(int i , int j , string s1 , string s2 , vector<vector<int>> &dp){
-	        if(i == s1.size() || j == s2.size()){
-	            return 0;
-	        }
-	        if(dp[i][j] != -1){
-	            return dp[i][j];
-	        }
-	        int ans = 0;
-	        if(s1[i] == s2[j]){
-	            return dp[i][j] = 1+solve(i+1 , j+1 , s1 , s2 , dp);
-	        }else{
-	            return dp[i][j] = max(solve(i+1 , j , s1 , s2 , dp) , solve(i , j+1 , s1 , s2 , dp));
-	        }
-	}
+//     int solve(int i , int j , string s1 , string s2 , vector<vector<int>> &dp){
+// 	        if(i == s1.size() || j == s2.size()){
+// 	            return 0;
+// 	        }
+// 	        if(dp[i][j] != -1){
+// 	            return dp[i][j];
+// 	        }
+// 	        int ans = 0;
+// 	        if(s1[i] == s2[j]){
+// 	            return dp[i][j] = 1+solve(i+1 , j+1 , s1 , s2 , dp);
+// 	        }else{
+// 	            return dp[i][j] = max(solve(i+1 , j , s1 , s2 , dp) , solve(i , j+1 , s1 , s2 , dp));
+// 	        }
+// 	}
     int longestPalinSubseq(string str) {
         //code here
         int n = str.size();
-        vector<vector<int>> dp(n , vector<int>(n , -1));
+        vector<vector<int>> dp(n+1 , vector<int>(n+1 , 0));
         string s1 = str;
-		    reverse(str.begin() , str.end());
-		    return solve(0 , 0 ,str , s1 , dp);
+		reverse(str.begin() , str.end());
+		string s2 = str;
+		for(int i=n-1;i>=0;i--){
+		    for(int j=n-1;j>=0;j--){
+		        if(s1[i] == s2[j]){
+	                dp[i][j] = 1+dp[i+1][j+1];
+    	        }else{
+    	            dp[i][j] = max(dp[i+1][j] , dp[i][j+1]);
+    	        }
+		    }
+		}
+		return dp[0][0];
     }
 };
 
