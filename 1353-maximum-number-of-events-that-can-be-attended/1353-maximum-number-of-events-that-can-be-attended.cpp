27f103fd75457f769737496daa9cf3d367a592c5
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-07-20 12:34:55 +0530

    Time: 413 ms (53.65%), Space: 70.8 MB (74.02%) - LeetHub

---
+++ 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxEvents(vector<vector<int>>& events) {
+        sort(events.begin() , events.end());
+        int ans = 0 , n = events.size();
+        int i=0;
+        priority_queue<int, vector<int> , greater<int>> pq;
+        for(int d = 1;d<=1e5;d++){
+            while(!pq.empty() && pq.top() < d){
+                pq.pop();
+            }
+            while(i<n && events[i][0] == d){
+                pq.push(events[i][1]);
+                i++;
+            }
+            if(!pq.empty()){
+                pq.pop();
+                ans++;
+            }
+            //if all elements are processed then stop 
+            if(pq.empty()&&i==n)
+                break;
+        }
+        return ans;
+    }
+};
