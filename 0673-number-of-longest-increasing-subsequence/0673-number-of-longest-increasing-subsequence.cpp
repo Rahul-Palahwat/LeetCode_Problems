c8d9585250737f2b52bed8658ab89dbfc173fae4
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-07-22 15:56:15 +0530

    Time: 205 ms (64.96%), Space: 13.2 MB (66.29%) - LeetHub

--- 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
@@ -1,16 +1,30 @@
 class Solution {
 public:
+    int solve(int index , vector<int>& nums , int prevIndex){
+        if(index == nums.size()){
+            return 0;
+        }
+        if(prevIndex == -1){
+            return max(solve(index+1 , nums , prevIndex) , 1+solve(index+1 , nums , index));
+        }else{
+            int notTaken = solve(index+1 , nums , prevIndex);
+            int taken = INT_MIN;
+            if(nums[index] > nums[prevIndex]){
+                taken = 1+solve(index+1 , nums , index);
+            }
+            return max(taken , notTaken);
+        }
+    }
     int findNumberOfLIS(vector<int>& nums) {
-        int n = nums.size() , maxi = 1;
-        vector<int> dp(n , 1) , cnt(n,1);
+        int n = nums.size();
+        vector<int> dp(n , 1) , cnt(n , 1);
+        int maxi = 1;
         for(int i=1;i<n;i++){
             for(int j=0;j<i;j++){
-                if(nums[i]>nums[j] && 1+dp[j] > dp[i]){
+                if(nums[i] > nums[j] && 1+dp[j] > dp[i]){
                     dp[i] = 1+dp[j];
-                    // inHerit
                     cnt[i] = cnt[j];
                 }else if(nums[i] > nums[j] && 1+dp[j] == dp[i]){
-                    // Increase the cnt
                     cnt[i]+=cnt[j];
                 }
             }
@@ -19,9 +33,10 @@ public:
         int ans = 0;
         for(int i=0;i<n;i++){
             if(dp[i] == maxi){
-                ans+=cnt[i];
+                ans += cnt[i];
             }
         }
         return ans;
+        // return solve(0 , nums, -1);
     }
 };
