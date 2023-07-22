11fa2e9949e5e43772396eca1e3df19429b4461f
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-07-22 16:48:50 +0530

    Time: 20 ms (59.86%), Space: 8.7 MB (59.98%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.cpp
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.cpp
@@ -1,63 +1,24 @@
 class Solution {
 public:
     vector<pair<int,int>> moves = {{2 , 1} , {-2 , -1} , {-2 , 1}, {2 , -1} , {1 , 2}, {-1 , -2}, {-1 , 2}, {1 , -2}};
-    double solve(int n , int k , int r , int c){
-        queue<pair<double , pair<int,int>>> q;
-        q.push({1.0 , {r , c}});
-        double temp = 1.0;
-        while(k--){
-            int sz = q.size();
-            cout<<sz<<" sz"<<endl;
-            while(sz--){
-                auto top = q.front();
-                int i = top.second.first , j = top.second.second;
-                q.pop();
-                for(auto it: moves){
-                    if((i+it.first) >= n || (i+it.first) < 0){
-                        continue;
-                    }
-                    if((j+it.second) >= n || (j+it.second) < 0){
-                        continue;
-                    }
-                    temp = top.first;
-                    temp = (temp)/(8*1.0);
-                    // cout<<temp<<" temp"<<endl;
-                    q.push({temp , {i+it.first , j+it.second}});
-                }
-            }
+    double solve(int n , int k , int i , int j , vector<vector<vector<double>>> &dp){
+        if(i>=n || i<0 || j>=n || j<0){
+            return 0.0;
         }
-        int sz = q.size();
-        // int val = q.front().first;
-        // cout<<sz<<" sz "<<temp<<endl;
-        return sz*1.0*temp;
+        if(k == 0){
+            return 1.0;
+        }
+        if(dp[i][j][k] != -1){
+            return dp[i][j][k];
+        }
+        double ans = 0.0;
+        for(auto it: moves){
+            ans+=solve(n , k-1 , i+it.first , j+it.second , dp)/8.0;
+        }
+        return dp[i][j][k] = ans;
     }
-    double knightProbability(int N, int K, int r, int c) {
-        // return solve(n , k , r , c);
-        vector<vector<int>> dir{{1,2},{1,-2},{-1,2},{-1,-2},{2,1},{2,-1},{-2,1},{-2,-1}};
-		vector<vector<double>> dp(N,vector<double>(N,0));
-		dp[r][c]=1;
-		for(int i=1;i<=K;i++){
-			vector<vector<double>> tem(N,vector<double>(N,0));
-			for(int j=0;j<N;j++){
-				for(int k=0;k<N;k++){
-					for(int v=0;v<8;v++){
-						int x=j+dir[v][0];
-						int y=k+dir[v][1];
-						if(x<0 || y<0 || x>=N || y>=N){
-							continue;
-						}
-						tem[j][k]+=dp[x][y];
-					}
-				}
-			}
-			dp=tem;
-		}
-		double rec=0;
-		for(int i=0;i<N;i++)
-			for(int j=0;j<N;j++){
-				rec+=dp[i][j];
-			}
-		double res=rec/pow(8,K);
-		return res;
+    double knightProbability(int n, int k, int r, int c) {
+        vector<vector<vector<double>>> dp(n , vector<vector<double>>(n , vector<double>(k+1 , -1)));
+        return solve(n , k , r , c , dp);
     }
 };
