96db25fdca917574fe9e0b3cbb6362c5aabd2271
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-07-23 12:17:12 +0530

    Time: 101 ms (70.47%), Space: 27.9 MB (76.77%) - LeetHub

--- 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.cpp
+++ 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.cpp
@@ -11,14 +11,19 @@
  */
 class Solution {
 public:
+    map<int , vector<TreeNode*>> mp;
     vector<TreeNode*> allPossibleFBT(int n) {
         vector<TreeNode*> ans;
         if(n == 0 || n%2 == 0){
             return ans;
         }
+        if(mp.find(n) != mp.end()){
+            return mp[n];
+        }
         if(n == 1){
             TreeNode* node = new TreeNode(0);
             ans.push_back(node);
+            mp[1] = ans;
             return ans;
         }
         for(int i=1;i<n;i+=2){
@@ -34,6 +39,6 @@ public:
                 }
             }
         }
-        return ans;
+        return mp[n] = ans;
     }
 };
