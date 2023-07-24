5fa40e47b185648dd13a443846b220d2727d368e
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-07-24 10:23:26 +0530

    Time: 0 ms (100.00%), Space: 6 MB (28.53%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,33 @@
 class Solution {
 public:
+    double solve(double x , long long n){
+        if(n<=0){
+            return 1.0;
+        }
+        double ans = solve(x , n/2);
+        if(n%2 == 0){
+            return ans*ans;
+        }else{
+            return ans*ans*x;
+        }
+    }
     double myPow(double x, int n) {
-        return pow(x , n*1.0);
+        int flag = 0;
+        long long tt;
+        if(n< 0){
+            flag = 1;
+        }
+        if(n<0){
+            tt = n*1LL*(-1);
+        }else{
+            tt = n;
+        }
+        cout<<tt<<endl;
+        double ans = solve(x , tt);
+        cout<<ans<<endl;
+        if(flag == 1){
+            return 1.0/ans;
+        }
+        return ans;
     }
 };
