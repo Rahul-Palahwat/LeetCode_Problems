c3b579174e2757a274a4d3aac044708fd4b0e2ba
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-07-28 09:46:00 +0530

    Time: 8 ms (99.18%), Space: 11.5 MB (15.58%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -10,63 +10,27 @@
  */
 class Solution {
 public:
-    
-    // my approach
-        
-    // void reve(vector<int> v, ListNode* head){
-    //     reverse(v.begin(),v.end());
-    //     for(auto it: v){
-    //         head->val = it;
-    //         head=head->next;   
-    //     }
-    //     return;
-    // }
-    // ListNode* reverseKGroup(ListNode* head, int k) {
-    //     int i=0;
-    //     ListNode *ans=head,*temp=head;
-    //     vector<int> v;
-    //     while(ans){
-    //         i++;
-    //         v.push_back(ans->val);
-    //         if(i==k){
-    //             // cout<<i<<" "<<ans->val<<" "<<temp->val<<endl;
-    //             reve(v,temp);
-    //             v.clear();
-    //             if(ans->next != NULL)
-    //             temp=ans->next;
-    //             i=0;
-    //         }
-    //         ans=ans->next;
-    //     }
-    //     return head;
-    // }
-    
-    // striver's approach 
     ListNode* reverseKGroup(ListNode* head, int k) {
-        if(head == NULL || k==1){
-            return head;
-        }
         ListNode *dummy = new ListNode(0);
-        ListNode *cur=dummy , *nex=dummy , *pre=dummy;
         dummy->next = head;
-        int count=0;
-        
-        while(cur->next != NULL){
-            cur=cur->next;
-            count++;
+        ListNode *cur = dummy , *nex = dummy , *prev = dummy;
+        cur = head;
+        int cnt = 0;
+        while(cur){
+            cnt++;
+            cur = cur->next;
         }
-        
-        while(count>=k){
-            cur = pre->next;
+        while(cnt >= k){
+            cur = prev->next;;
             nex = cur->next;
             for(int i=1;i<k;i++){
                 cur->next = nex->next;
-                nex->next = pre->next;
-                pre->next=nex;
+                nex->next = prev->next;
+                prev->next = nex;
                 nex = cur->next;
             }
-            pre=cur;
-            count-=k;
+            prev = cur;
+            cnt-=k;
         }
         return dummy->next;
     }
