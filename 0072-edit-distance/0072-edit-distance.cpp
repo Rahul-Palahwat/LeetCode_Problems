c7edf6802c2c12b9b88cf07b0a3806d1733d6011
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-07-29 19:02:45 +0530

    Time: 0 ms (100.00%), Space: 9 MB (61.05%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -56,25 +56,76 @@ public:
     
     
     // Tabulation + Optimization
+    // int minDistance(string word1, string word2) {
+    //     int n = word1.size() , m = word2.size();
+    //     vector<int> dp(m+1 , 0) , cur(m+1 , 0);
+    //     for(int i=0;i<=m;i++){
+    //         dp[i] = i;
+    //     }
+    //     for(int i=1;i<=n;i++){
+    //         cur[0]=i;
+    //         for(int j=1;j<=m;j++){
+    //             if(word1[i-1] == word2[j-1]){
+    //                 cur[j] = dp[j-1];
+    //             }else{
+    //                 cur[j] = 1+min(dp[j-1] , min(dp[j] , cur[j-1]));
+    //             }
+    //             // cout<<cur[j]<<" ";
+    //         }
+    //         // cout<<endl;
+    //         dp = cur;
+    //     }
+    //     return dp[m];
+    // }
+    
+    
+    // Tabulation + 1D DP
+    // int minDistance(string word1, string word2) {
+    //     int n = word1.size() , m = word2.size();
+    //     vector<int> dp(m+1 , 0);
+    //     for(int i=0;i<=m;i++){
+    //         dp[i] = i;
+    //     }
+    //     for(int i=1;i<=n;i++){
+    //         for(int j=m;j>0;j--){
+    //             if(word1[i-1] == word2[j-1]){
+    //                 dp[j] = dp[j-1];
+    //             }else{
+    //                 dp[j] = 1+min(dp[j-1] , min(dp[j] , dp[j-1]));
+    //             }
+    //         }
+    //         dp[0]=i;
+    //     }
+    //     return dp[m];
+    // }
+    
+    
+    
+    
+    
+    
+    
+    int solve(int i, int j, string &s1 , string &s2 , vector<vector<int>> &dp){
+        if(j<0){
+            return i+1;
+        }
+        if(i<0){
+            return j+1;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        if(s1[i] == s2[j]){
+            return dp[i][j] = solve(i-1 , j-1 , s1 , s2 , dp);
+        }
+        int insert = 1+solve(i, j-1 , s1 , s2 , dp);
+        int del = 1+solve(i-1 , j , s1 , s2 , dp);
+        int replace = 1+solve(i-1 , j-1 , s1 , s2 , dp);
+        return dp[i][j] = min(insert , min(del , replace));
+    }
     int minDistance(string word1, string word2) {
         int n = word1.size() , m = word2.size();
-        vector<int> dp(m+1 , 0) , cur(m+1 , 0);
-        for(int i=0;i<=m;i++){
-            dp[i] = i;
-        }
-        for(int i=1;i<=n;i++){
-            cur[0]=i;
-            for(int j=1;j<=m;j++){
-                if(word1[i-1] == word2[j-1]){
-                    cur[j] = dp[j-1];
-                }else{
-                    cur[j] = 1+min(dp[j-1] , min(dp[j] , cur[j-1]));
-                }
-                // cout<<cur[j]<<" ";
-            }
-            // cout<<endl;
-            dp = cur;
-        }
-        return dp[m];
+        vector<vector<int>> dp(n , vector<int>(m , -1));
+        return solve(n-1 , m-1 , word1 , word2 , dp);
     }
 };
