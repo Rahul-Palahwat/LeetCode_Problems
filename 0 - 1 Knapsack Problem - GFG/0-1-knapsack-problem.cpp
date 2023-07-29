d80835cbf30be05529a2b4ff2abacedbeda7d8dd
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-07-29 18:42:56 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -8,25 +8,46 @@ class Solution
 {
     public:
     //Function to return max value that can be put in knapsack of capacity W.
-    int solve(int index , int W , int wt[] , int val[] , vector<vector<int>> &dp){
-        if(index < 0){
-            return 0;
-        }
-        if(dp[index][W] != -1){
-            return dp[index][W];
-        }
-        int taken = 0 , notTaken = 0;
-        notTaken = solve(index-1 , W , wt , val , dp);
-        if(wt[index] <= W){
-            taken = val[index]+solve(index-1 , W-wt[index] , wt , val , dp);
-        }
-        return dp[index][W] = max(taken , notTaken);
-    }
+    // Recursion + Memoization
+    // int solve(int index , int W , int wt[] , int val[] , vector<vector<int>> &dp){
+    //     if(index < 0){
+    //         return 0;
+    //     }
+    //     if(dp[index][W] != -1){
+    //         return dp[index][W];
+    //     }
+    //     int taken = 0 , notTaken = 0;
+    //     notTaken = solve(index-1 , W , wt , val , dp);
+    //     if(wt[index] <= W){
+    //         taken = val[index]+solve(index-1 , W-wt[index] , wt , val , dp);
+    //     }
+    //     return dp[index][W] = max(taken , notTaken);
+    // }
+    // int knapSack(int W, int wt[], int val[], int n) 
+    // { 
+    //   // Your code here
+    //   vector<vector<int>> dp(n , vector<int>(W+1 , -1));
+    //   return solve(n-1 , W , wt , val , dp);
+    // }
+    
+    
+    
+    // Tabulation
     int knapSack(int W, int wt[], int val[], int n) 
     { 
        // Your code here
-       vector<vector<int>> dp(n , vector<int>(W+1 , -1));
-       return solve(n-1 , W , wt , val , dp);
+       vector<vector<int>> dp(n+1 , vector<int>(W+1 , 0));
+       for(int index=1;index<=n;index++){
+           for(int w=0;w<=W;w++){
+               int taken = 0 , notTaken = 0;
+                notTaken = dp[index-1][w];
+                if(wt[index-1] <= w){
+                    taken = val[index-1]+dp[index-1][w-wt[index-1]];
+                }
+                dp[index][w] = max(taken , notTaken);
+           }
+       }
+       return dp[n][W];
     }
 };
 
