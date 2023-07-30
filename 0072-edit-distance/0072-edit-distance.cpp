78a5021a6ab8aec56f5ed5322f9310c71c6fc94a
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-07-30 15:51:22 +0530

    Time: 8 ms (89.90%), Space: 9 MB (61.04%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -104,28 +104,55 @@ public:
     
     
     
+    // Recursion + Optimization
+    // int solve(int i, int j, string &s1 , string &s2 , vector<vector<int>> &dp){
+    //     if(j<0){
+    //         return i+1;
+    //     }
+    //     if(i<0){
+    //         return j+1;
+    //     }
+    //     if(dp[i][j] != -1){
+    //         return dp[i][j];
+    //     }
+    //     if(s1[i] == s2[j]){
+    //         return dp[i][j] = solve(i-1 , j-1 , s1 , s2 , dp);
+    //     }
+    //     int insert = 1+solve(i, j-1 , s1 , s2 , dp);
+    //     int del = 1+solve(i-1 , j , s1 , s2 , dp);
+    //     int replace = 1+solve(i-1 , j-1 , s1 , s2 , dp);
+    //     return dp[i][j] = min(insert , min(del , replace));
+    // }
+    // int minDistance(string word1, string word2) {
+    //     int n = word1.size() , m = word2.size();
+    //     vector<vector<int>> dp(n , vector<int>(m , -1));
+    //     return solve(n-1 , m-1 , word1 , word2 , dp);
+    // }
     
-    int solve(int i, int j, string &s1 , string &s2 , vector<vector<int>> &dp){
-        if(j<0){
-            return i+1;
+    
+    
+    // Tabulation
+    int minDistance(string s1, string s2) {
+        int n = s1.size() , m = s2.size();
+        vector<vector<int>> dp(n+1 , vector<int>(m+1 , 0));
+        for(int i=0;i<=m;i++){
+            dp[0][i] = i; 
         }
-        if(i<0){
-            return j+1;
+        for(int i=0;i<=n;i++){
+            dp[i][0] = i;
         }
-        if(dp[i][j] != -1){
-            return dp[i][j];
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1[i-1] == s2[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                }else{
+                    int insert = 1+dp[i][j-1];
+                    int del = 1+dp[i-1][j];
+                    int replace = 1+dp[i-1][j-1];
+                    dp[i][j] = min(insert , min(del , replace));
+                }
+            }
         }
-        if(s1[i] == s2[j]){
-            return dp[i][j] = solve(i-1 , j-1 , s1 , s2 , dp);
-        }
-        int insert = 1+solve(i, j-1 , s1 , s2 , dp);
-        int del = 1+solve(i-1 , j , s1 , s2 , dp);
-        int replace = 1+solve(i-1 , j-1 , s1 , s2 , dp);
-        return dp[i][j] = min(insert , min(del , replace));
-    }
-    int minDistance(string word1, string word2) {
-        int n = word1.size() , m = word2.size();
-        vector<vector<int>> dp(n , vector<int>(m , -1));
-        return solve(n-1 , m-1 , word1 , word2 , dp);
+        return dp[n][m];
     }
 };
