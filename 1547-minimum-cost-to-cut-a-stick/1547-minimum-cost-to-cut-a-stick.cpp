d380e335ff37d2a671e84ea2fdd9e6e05f3aba72
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-07-30 21:06:38 +0530

    Time: 77 ms (66.85%), Space: 10.4 MB (25.46%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -27,22 +27,51 @@ public:
     
     
     // Tabulation
-    int minCost(int n, vector<int>& cuts) {
-        int sz = cuts.size();
-        cuts.push_back(n);
-        cuts.push_back(0);
-        sort(cuts.begin() , cuts.end());
-        vector<vector<int>> dp(sz+2 , vector<int>(sz+2 , 0));
-        for(int i=sz;i>0;i--){
-            for(int j=i;j<=sz;j++){
-                int mini = INT_MAX;
-                for(int k=i;k<=j;k++){
-                    int cost = cuts[j+1]-cuts[i-1]+dp[i][k-1]+dp[k+1][j];
-                    mini = min(mini , cost);
-                }
-                dp[i][j] = mini;
-            }
+    // int minCost(int n, vector<int>& cuts) {
+    //     int sz = cuts.size();
+    //     cuts.push_back(n);
+    //     cuts.push_back(0);
+    //     sort(cuts.begin() , cuts.end());
+    //     vector<vector<int>> dp(sz+2 , vector<int>(sz+2 , 0));
+    //     for(int i=sz;i>0;i--){
+    //         for(int j=i;j<=sz;j++){
+    //             int mini = INT_MAX;
+    //             for(int k=i;k<=j;k++){
+    //                 int cost = cuts[j+1]-cuts[i-1]+dp[i][k-1]+dp[k+1][j];
+    //                 mini = min(mini , cost);
+    //             }
+    //             dp[i][j] = mini;
+    //         }
+    //     }
+    //     return dp[1][sz];
+    // }
+    
+    
+    
+    
+    
+    
+    // Recursion + Memoization
+    int solve(int i , int j , vector<int>& cuts , vector<vector<int>> &dp){
+        if(i>j){
+            return 0;
         }
-        return dp[1][sz];
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        int ans = INT_MAX;
+        for(int k = i;k<=j;k++){
+            int temp = cuts[j+1]-cuts[i-1] + solve(i , k-1 , cuts , dp) + solve(k+1 , j , cuts , dp);
+            ans = min(ans , temp);
+        }
+        return dp[i][j] = ans;
+    }
+    int minCost(int n, vector<int>& cuts) {
+        cuts.push_back(0);
+        cuts.push_back(n);
+        sort(cuts.begin() , cuts.end());
+        int sz = cuts.size();
+        vector<vector<int>> dp(sz , vector<int>(sz , -1));
+        return solve(1 , sz-2, cuts , dp);
     }
 };
