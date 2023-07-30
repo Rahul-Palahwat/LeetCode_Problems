fa82de2ba160708dc11989aae88c220be3ed0af8
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-07-30 16:42:51 +0530

    Time: 11 ms (64.26%), Space: 10 MB (43.52%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -79,23 +79,51 @@ public:
     
     
     
-    // Now tabulation 
+    // // Now tabulation 
+    // int minPathSum(vector<vector<int>>& grid) {
+    //     int n = grid.size() , m = grid[0].size();
+    //     vector<vector<int>> dp(n , vector<int>(m , 0));
+    //     dp[0][0] = grid[0][0];
+    //     for(int i=0;i<n;i++){
+    //         for(int j=0;j<m;j++){
+    //             if(i==0 && j==0)
+    //                 continue;
+    //             else if(i == 0)
+    //                 dp[i][j] = grid[i][j]+dp[i][j-1];
+    //             else if(j==0)
+    //                 dp[i][j] = grid[i][j]+dp[i-1][j];
+    //             else
+    //                 dp[i][j] = grid[i][j]+min(dp[i-1][j] , dp[i][j-1]);
+    //         }
+    //     }
+    //     return dp[n-1][m-1];
+    // }
+    
+    
+    
+    
+    
+    
+    
+    
+    // Recursion + Tabulation 
+    int solve(int i, int j , vector<vector<int>>& grid , vector<vector<int>> &dp){
+        if(i==0 && j == 0){
+            return grid[i][j];
+        }
+        if(i<0 || j<0){
+            return 1e5;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        int left = solve(i-1 , j , grid , dp);
+        int top = solve(i , j-1 , grid , dp);
+        return dp[i][j] = grid[i][j]+min(left , top);
+    }
     int minPathSum(vector<vector<int>>& grid) {
         int n = grid.size() , m = grid[0].size();
-        vector<vector<int>> dp(n , vector<int>(m , 0));
-        dp[0][0] = grid[0][0];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(i==0 && j==0)
-                    continue;
-                else if(i == 0)
-                    dp[i][j] = grid[i][j]+dp[i][j-1];
-                else if(j==0)
-                    dp[i][j] = grid[i][j]+dp[i-1][j];
-                else
-                    dp[i][j] = grid[i][j]+min(dp[i-1][j] , dp[i][j-1]);
-            }
-        }
-        return dp[n-1][m-1];
+        vector<vector<int>> dp(n , vector<int>(m , -1));
+        return solve(n-1 , m-1 , grid , dp);
     }
 };
