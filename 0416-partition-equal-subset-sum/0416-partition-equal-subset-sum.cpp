90375c6f8f9dac492ff84ce0b708e3b5f1521a58
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-07-30 17:33:20 +0530

    Time: 245 ms (79.53%), Space: 95 MB (43.53%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -100,8 +100,54 @@ public:
     
     
     // Tabulation + Optimization
+    // bool canPartition(vector<int>& nums) {
+    //     int sum = 0 , n = nums.size();
+    //     for(auto it: nums){
+    //         sum+=it;
+    //     }
+    //     if(sum%2 != 0){
+    //         return false;
+    //     }
+    //     sum/=2;
+    //     vector<bool> dp(sum+1 , false);
+    //     dp[0] = true;
+    //     for(int i=0;i<n;i++){
+    //         vector<bool> temp(sum+1 , false);
+    //         temp[0] = true;
+    //         for(int j=1;j<sum+1;j++){
+    //             if(nums[i] <= j){
+    //                 temp[j] = dp[j] || dp[j-nums[i]];
+    //             }else{
+    //                 temp[j] = dp[j];
+    //             }
+    //         }
+    //         dp = temp;
+    //     }
+    //     return dp[sum];
+    // }
+    
+    
+    
+    
+    
+    // Recursion + Memoization
+    bool solve(int index , int sum , vector<int>& nums , vector<vector<int>> &dp){
+        if(index < 0){
+            return sum == 0;
+        }
+        if(dp[index][sum] != -1){
+            return dp[index][sum];
+        }
+        bool notTaken = solve(index-1 , sum , nums , dp);
+        bool taken = false;
+        if(sum >= nums[index]){
+            taken = solve(index-1 , sum-nums[index] , nums , dp);
+        } 
+        return dp[index][sum] = (taken || notTaken);
+    }
     bool canPartition(vector<int>& nums) {
-        int sum = 0 , n = nums.size();
+        int n = nums.size();
+        int sum = 0;
         for(auto it: nums){
             sum+=it;
         }
@@ -109,20 +155,7 @@ public:
             return false;
         }
         sum/=2;
-        vector<bool> dp(sum+1 , false);
-        dp[0] = true;
-        for(int i=0;i<n;i++){
-            vector<bool> temp(sum+1 , false);
-            temp[0] = true;
-            for(int j=1;j<sum+1;j++){
-                if(nums[i] <= j){
-                    temp[j] = dp[j] || dp[j-nums[i]];
-                }else{
-                    temp[j] = dp[j];
-                }
-            }
-            dp = temp;
-        }
-        return dp[sum];
+        vector<vector<int>> dp(n , vector<int>(sum+1 , -1));
+        return solve(n-1 , sum , nums , dp);
     }
 };
