708ba0782cb193e667f1457f14b5a55b7ee9ff04
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-07-30 16:31:24 +0530

    Added solution - LeetHub

--- Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
@@ -9,43 +9,24 @@ using namespace std;
 
 class Solution{
 public:
-
-    // Recursion + Memoization
-    // int f(int arr[] , int i , int j,vector<vector<int>> &dp){
-    //     if(i == j){
-    //         return 0;
-    //     }
-    //     if(dp[i][j] != -1){
-    //         return dp[i][j];
-    //     }
-    //     int steps = INT_MAX;
-    //     for(int k=i;k<j;k++){
-    //         steps = min(steps , arr[i-1]*arr[k]*arr[j] + f(arr , i , k ,dp) + f(arr, k+1 ,j ,dp));
-    //     }
-    //     return dp[i][j] = steps;
-    // }
-    // int matrixMultiplication(int N, int arr[])
-    // {
-    //     vector<vector<int>> dp(N , vector<int>(N,-1));
-    //     return f(arr , 1 , N-1 , dp);
-    // }
-    
-    
-    
-    // Tabulation
+    int solve(int i , int j , int arr[] , vector<vector<int>> &dp){
+        if(i >= j){
+            return 0;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        int steps = INT_MAX;
+        for(int k = i;k<j;k++){
+            steps = min(steps , arr[i-1]*arr[k]*arr[j]+solve(i , k , arr , dp)+solve(k+1 , j , arr , dp));
+        }
+        return dp[i][j] = steps;
+    }
     int matrixMultiplication(int N, int arr[])
     {
-        vector<vector<int>> dp(N , vector<int>(N,0));
-        for(int i=N-1;i>=1;i--){
-            for(int j=i+1;j<N;j++){
-                int steps = INT_MAX;
-                for(int k=i;k<j;k++){
-                    steps = min(steps , arr[i-1]*arr[k]*arr[j] + dp[i][k] + dp[k+1][j]);
-                }
-                dp[i][j] = steps;
-            }
-        }
-        return dp[1][N-1];
+        // code here
+        vector<vector<int>> dp(N, vector<int>(N , -1));
+        return solve(1 , N-1 , arr , dp);
     }
 };
 
