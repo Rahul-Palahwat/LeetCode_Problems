381975121887e69bb731eade659cdce988652e1b
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-07-30 16:37:53 +0530

    Added solution - LeetHub

--- Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
@@ -9,25 +9,50 @@ using namespace std;
 
 class Solution{
 public:
-    int solve(int i , int j , int arr[] , vector<vector<int>> &dp){
-        if(i >= j){
-            return 0;
-        }
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        int steps = INT_MAX;
-        for(int k = i;k<j;k++){
-            steps = min(steps , arr[i-1]*arr[k]*arr[j]+solve(i , k , arr , dp)+solve(k+1 , j , arr , dp));
-        }
-        return dp[i][j] = steps;
-    }
+    // Recursion + Memoization
+    // int solve(int i , int j , int arr[] , vector<vector<int>> &dp){
+    //     if(i >= j){
+    //         return 0;
+    //     }
+    //     if(dp[i][j] != -1){
+    //         return dp[i][j];
+    //     }
+    //     int steps = INT_MAX;
+    //     for(int k = i;k<j;k++){
+    //         steps = min(steps , arr[i-1]*arr[k]*arr[j]+solve(i , k , arr , dp)+solve(k+1 , j , arr , dp));
+    //     }
+    //     return dp[i][j] = steps;
+    // }
+    // int matrixMultiplication(int N, int arr[])
+    // {
+    //     // code here
+    //     vector<vector<int>> dp(N, vector<int>(N , -1));
+    //     return solve(1 , N-1 , arr , dp);
+    // }
+    
+    
+    
+    
+    
+    // Tabulation
     int matrixMultiplication(int N, int arr[])
     {
         // code here
-        vector<vector<int>> dp(N, vector<int>(N , -1));
-        return solve(1 , N-1 , arr , dp);
+        vector<vector<int>> dp(N, vector<int>(N , 0));
+        for(int i=N-1;i>=1;i--){
+            for(int j=i+1;j<N;j++){
+                int steps = INT_MAX;
+                for(int k=i;k<j;k++){
+                    steps = min(steps , arr[i-1]*arr[k]*arr[j]+dp[i][k]+dp[k+1][j]);
+                }
+                dp[i][j] = steps;
+            }
+        }
+        
+        return dp[1][N-1];
     }
+    
+    
 };
 
 //{ Driver Code Starts.
