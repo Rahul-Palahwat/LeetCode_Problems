42658cb91cc9396dd86b0af704c281fd100c06d1
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-08-05 17:14:52 +0530

    Time: 60 ms (93.20%), Space: 28.3 MB (66.78%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,48 +1,103 @@
 class Solution {
 public:
-    int solve(int i , int j ,vector<vector<int>>& grid ,int n ,int m){
+    // int solve(int i , int j ,vector<vector<int>>& grid ,int n ,int m){
+    //     queue<pair<int,int>> q;
+    //     q.push({i , j});
+    //     int ans = 0,flag=0;
+    //     while(!q.empty()){
+    //         auto top = q.front();
+    //         q.pop();
+    //         if((top.first <= 0) || (top.second <= 0)){
+    //             flag=1;
+    //             // return 0;
+    //         }
+    //         if((top.first >= (n-1)) || (top.second >= (m-1))){
+    //             flag=1;
+    //             // return 0;
+    //         }
+    //         if(grid[top.first][top.second] == 0){
+    //             continue;
+    //         }
+    //         ans++;
+    //         grid[top.first][top.second] = 0;
+    //         if((top.first-1 >= 0) && (grid[top.first-1][top.second]==1)){
+    //             q.push({top.first-1 ,top.second});
+    //         }
+    //         if((top.second-1 >= 0) && (grid[top.first][top.second-1]==1)){
+    //             q.push({top.first ,top.second-1});
+    //         }
+    //         if((top.first+1 < n) && (grid[top.first+1][top.second]==1)){
+    //             q.push({top.first+1 ,top.second});
+    //         }
+    //         if((top.second+1 < m) && (grid[top.first][top.second+1]==1)){
+    //             q.push({top.first ,top.second+1});
+    //         }
+    //     }
+    //     // cout<<flag<<" "<<ans<<endl;
+    //     return flag == 0 ? ans : 0;
+    // }
+    // int numEnclaves(vector<vector<int>>& grid) {
+    //     int n = grid.size() , m = grid[0].size() , ans = 0;
+    //     for(int i=0;i<n;i++){
+    //         for(int j=0;j<m;j++){
+    //             if(grid[i][j] == 1){
+    //                 ans+=solve(i , j , grid , n , m);
+    //             }
+    //         }
+    //     }
+    //     return ans;
+    // }
+    
+    
+    
+    
+    // Solving it again, simple using BFS and storing boundary elements in queue
+    
+    int numEnclaves(vector<vector<int>>& grid) {
+        int n = grid.size() , m = grid[0].size();
+        int ans = 0;
+        vector<vector<bool>> vis(n , vector<bool>(m , false));
         queue<pair<int,int>> q;
-        q.push({i , j});
-        int ans = 0,flag=0;
+        for(int i=0;i<n;i++){
+            if(grid[i][0] == 1){
+                q.push({i , 0});
+            }
+            if(grid[i][m-1] == 1){
+                q.push({i , m-1});
+            }
+        }
+        for(int i=0;i<m;i++){
+            if(grid[0][i] == 1){
+                q.push({0 , i});
+            }
+            if(grid[n-1][i] == 1){
+                q.push({n-1 , i});
+            }
+        }
+        // cout<<q.size()<<"sz"<<endl;
         while(!q.empty()){
             auto top = q.front();
             q.pop();
-            if((top.first <= 0) || (top.second <= 0)){
-                flag=1;
-                // return 0;
-            }
-            if((top.first >= (n-1)) || (top.second >= (m-1))){
-                flag=1;
-                // return 0;
-            }
-            if(grid[top.first][top.second] == 0){
+            int i = top.first , j = top.second;
+            if(i<0 || j<0 || i>=n || j>=m){
                 continue;
             }
-            ans++;
-            cout<<top.first<<" "<<top.second<<endl;
-            grid[top.first][top.second] = 0;
-            if((top.first-1 >= 0) && (grid[top.first-1][top.second]==1)){
-                q.push({top.first-1 ,top.second});
+            if(grid[i][j] == 0){
+                continue;
             }
-            if((top.second-1 >= 0) && (grid[top.first][top.second-1]==1)){
-                q.push({top.first ,top.second-1});
+            if(vis[i][j]){
+                continue;
             }
-            if((top.first+1 < n) && (grid[top.first+1][top.second]==1)){
-                q.push({top.first+1 ,top.second});
-            }
-            if((top.second+1 < m) && (grid[top.first][top.second+1]==1)){
-                q.push({top.first ,top.second+1});
-            }
+            vis[i][j] = true;
+            q.push({i+1 , j});
+            q.push({i-1 , j});
+            q.push({i , j-1});
+            q.push({i , j+1});
         }
-        // cout<<flag<<" "<<ans<<endl;
-        return flag == 0 ? ans : 0;
-    }
-    int numEnclaves(vector<vector<int>>& grid) {
-        int n = grid.size() , m = grid[0].size() , ans = 0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j] == 1){
-                    ans+=solve(i , j , grid , n , m);
+                if(!vis[i][j] && grid[i][j] == 1){
+                    ans++;
                 }
             }
         }
