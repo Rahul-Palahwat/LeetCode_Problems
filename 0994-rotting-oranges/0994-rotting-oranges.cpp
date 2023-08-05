9cdf5c867819800d4f7a56fe6cf7c9ec2a72e24d
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-08-05 14:25:11 +0530

    Time: 10 ms (39.26%), Space: 13.4 MB (20.05%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,69 +1,132 @@
 class Solution {
 public:
-    int n , m;
-    queue<vector<int>> q;
-    int ans = 0;
-    void solve(vector<vector<int>>& grid , vector<vector<bool>>& vis){
+//     int n , m;
+//     // using dfs for every rotten orange
+//     queue<vector<int>> q;
+//     int ans = 0;
+//     void solve(vector<vector<int>>& grid , vector<vector<bool>>& vis){
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<m;j++){
+//                 if(grid[i][j] == 2){
+//                     q.push({i,j,0});
+//                 }
+//             }
+//         }
+//         while(!q.empty()){
+//             int t = q.size();
+//             while(t--){
+//                 auto t = q.front();
+//                 int i = t[0],j = t[1] , time = t[2];
+//                 ans = max(ans , time);
+//                 if(vis[i][j]){
+//                     continue;
+//                 }
+//                 vis[t[0]][t[1]] = true;
+//                 q.pop();
+//                 if(i<n-1 && j<m){
+//                     if(grid[i+1][j] == 1){
+//                         q.push({i+1 , j , time+1});
+//                         grid[i+1][j] = 2;
+//                     }
+//                 }
+//                 if(i>0 && j<m){
+//                     if(grid[i-1][j] == 1){
+//                         q.push({i-1 , j , time+1});
+//                         grid[i-1][j] = 2;
+//                     }
+//                 }
+//                 if(i<n && j<m-1){
+//                     if(grid[i][j+1] == 1){
+//                         q.push({i , j+1 , time+1});
+//                         grid[i][j+1] = 2;
+//                     }
+//                 }
+//                 if(i<n && j>0){
+//                     if(grid[i][j-1] == 1){
+//                         q.push({i , j-1 , time+1});
+//                         grid[i][j-1] = 2;
+//                     }
+//                 }
+                
+//             }
+//         }
+//         return;
+//     }
+    
+//     int orangesRotting(vector<vector<int>>& grid) {
+//         n = grid.size();
+//         m = grid[0].size();
+//         vector<vector<bool>> vis(n , vector<bool>(m,false));
+//         solve(grid , vis);
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<m;j++){
+//                 if(grid[i][j] == 1){
+//                     return -1;
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+    
+    
+    
+    
+    
+    
+    // New try
+    
+    
+    int orangesRotting(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        vector<vector<int>> vis = grid;
+        queue<pair<int,int>> q;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j] == 2){
-                    q.push({i,j,0});
+                    q.push({i , j});
                 }
             }
         }
+        int ans = 0;
         while(!q.empty()){
-            int t = q.size();
-            while(t--){
-                auto t = q.front();
-                int i = t[0],j = t[1] , time = t[2];
-                ans = max(ans , time);
-                if(vis[i][j]){
+            int sz = q.size();
+            // cout<<sz<<" sz "<<endl;
+            bool flag = false;
+            while(sz--){
+                auto top = q.front();
+                q.pop();
+                int i = top.first , j = top.second;
+                // cout<<i<<" i "<<j<<endl;
+                if(vis[i][j] == 0){
                     continue;
                 }
-                vis[t[0]][t[1]] = true;
-                q.pop();
-                if(i<n-1 && j<m){
-                    if(grid[i+1][j] == 1){
-                        q.push({i+1 , j , time+1});
-                        grid[i+1][j] = 2;
-                    }
+                flag = true;
+                // cout<<vis[i][j]<<"prev"<<endl;
+                vis[i][j] = 0;
+                // cout<<vis[i][j]<<"after"<<endl;
+                if(i+1<n && vis[i+1][j] == 1){
+                    q.push({i+1 , j});
                 }
-                if(i>0 && j<m){
-                    if(grid[i-1][j] == 1){
-                        q.push({i-1 , j , time+1});
-                        grid[i-1][j] = 2;
-                    }
+                if(j+1<m && vis[i][j+1] == 1){
+                    q.push({i , j+1});
                 }
-                if(i<n && j<m-1){
-                    if(grid[i][j+1] == 1){
-                        q.push({i , j+1 , time+1});
-                        grid[i][j+1] = 2;
-                    }
+                if(i-1>=0 && vis[i-1][j] == 1){
+                    q.push({i-1 , j});
                 }
-                if(i<n && j>0){
-                    if(grid[i][j-1] == 1){
-                        q.push({i , j-1 , time+1});
-                        grid[i][j-1] = 2;
-                    }
+                if(j-1>=0 && vis[i][j-1] == 1){
+                    q.push({i , j-1});
                 }
-                
             }
+            if(flag)
+                ans++;
         }
-        return;
-    }
-    
-    int orangesRotting(vector<vector<int>>& grid) {
-        n = grid.size();
-        m = grid[0].size();
-        vector<vector<bool>> vis(n , vector<bool>(m,false));
-        solve(grid , vis);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j] == 1){
+        for(auto it: vis){
+            for(auto i: it){
+                if(i == 1){
                     return -1;
                 }
             }
         }
-        return ans;
+        return ans-1 >0 ? ans-1 : 0;
     }
 };
