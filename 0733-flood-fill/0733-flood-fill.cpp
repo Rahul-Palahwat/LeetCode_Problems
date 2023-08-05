061cf044193fbc6e2ac4e731091355c0bc78714e
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-08-05 13:19:57 +0530

    Time: 8 ms (71.37%), Space: 14.3 MB (26.05%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -5,12 +5,13 @@ public:
         int m = image[0].size();
         int col = image[sr][sc];
         vector<vector<bool>> vis(n , vector<bool>(m,false));
-        queue<vector<int>> q;
-        q.push({sr , sc , color , col});
+        vector<vector<int>> ans = image;
+        queue<pair<int,int>> q;
+        q.push({sr , sc});
         while(!q.empty()){
             auto top = q.front();
             q.pop();
-            int i = top[0] , j = top[1];
+            int i = top.first , j = top.second;
             if(i<0 || j<0 || i>=n || j>=m){
                 continue;
             }
@@ -18,14 +19,14 @@ public:
                 continue;
             }
             vis[i][j] = true;
-            if(image[i][j] == top[3]){
-                image[i][j] = top[2];
-                q.push({i+1 , j , top[2] , top[3]});
-                q.push({i-1 , j , top[2] , top[3]});
-                q.push({i , j+1 , top[2] , top[3]});
-                q.push({i , j-1 , top[2] , top[3]});
+            if(ans[i][j] == col){
+                ans[i][j] = color;
+                q.push({i+1 , j});
+                q.push({i-1 , j });
+                q.push({i , j+1});
+                q.push({i , j-1});
             }
         }
-        return image;
+        return ans;
     }
 };
