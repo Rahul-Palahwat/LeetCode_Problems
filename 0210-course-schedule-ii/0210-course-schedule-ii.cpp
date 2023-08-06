f3228db2c07e92d2f828eca7ad434ff6d5a61b1e
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-08-06 12:57:39 +0530

    Time: 19 ms (86.26%), Space: 13.8 MB (53.49%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,7 +1,45 @@
 class Solution {
 public:
+    // vector<int> findOrder(int numCourses, vector<vector<int>>& pre) {
+    //     vector<int> ans;
+    //     vector<int> g[numCourses];
+    //     for(auto it: pre){
+    //         g[it[1]].push_back(it[0]);
+    //     }
+    //     vector<int> indegree(numCourses , 0);
+    //     for(int i=0;i<numCourses;i++){
+    //         for(auto it: g[i]){
+    //             indegree[it]++;
+    //         }
+    //     }
+    //     queue<int> q;
+    //     for(int i=0;i<numCourses;i++){
+    //         if(indegree[i] == 0){
+    //             q.push(i);
+    //         }
+    //     }
+    //     while(!q.empty()){
+    //         auto top = q.front();
+    //         q.pop();
+    //         ans.push_back(top);
+    //         for(auto it: g[top]){
+    //             indegree[it]--;
+    //             if(indegree[it] == 0){
+    //                 q.push(it);
+    //             }
+    //         }
+    //     }
+    //     if(ans.size() == numCourses){
+    //         return ans;
+    //     }
+    //     return {};
+    // }
+    
+    
+    
+    
+    // 06/08/2023 using topological sort (kahn's algo)
     vector<int> findOrder(int numCourses, vector<vector<int>>& pre) {
-        vector<int> ans;
         vector<int> g[numCourses];
         for(auto it: pre){
             g[it[1]].push_back(it[0]);
@@ -14,24 +52,25 @@ public:
         }
         queue<int> q;
         for(int i=0;i<numCourses;i++){
-            if(indegree[i] == 0){
+            if(indegree[i] == 0)
                 q.push(i);
-            }
         }
+        vector<int> ans;
         while(!q.empty()){
             auto top = q.front();
             q.pop();
             ans.push_back(top);
             for(auto it: g[top]){
                 indegree[it]--;
-                if(indegree[it] == 0){
+                if(indegree[it] == 0)
                     q.push(it);
-                }
             }
         }
+        // cout<<ans.size()<<endl;;
         if(ans.size() == numCourses){
             return ans;
         }
         return {};
     }
+    
 };
