337eb536f544888de196ddef8c71730672523c0b
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-08-06 16:52:53 +0530

    Time: 161 ms (88.06%), Space: 58.8 MB (52.76%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,40 +1,82 @@
 class Solution {
 public:
-    bool dfs(int node, vector<vector<int>>&g, vector<bool>&vis, vector<bool>&pathVis, vector<bool>&check){
-        vis[node] = true;
-        pathVis[node] = true;
-        check[node] = false;
-        for(auto it: g[node]){
-            if(!vis[it]){
-                if(dfs(it , g , vis , pathVis , check)){
-                    check[node] = false;
-                    return true;
-                }
-            }else if(pathVis[it]){
-                check[node] = false;
-                return true;
-            }
-        }
-        check[node] = true;
-        pathVis[node] = false;
-        return false;
-    }
+    
+    
+    // using DFS for undirected graph
+    
+    // bool dfs(int node, vector<vector<int>>&g, vector<bool>&vis, vector<bool>&pathVis, vector<bool>&check){
+    //     vis[node] = true;
+    //     pathVis[node] = true;
+    //     check[node] = false;
+    //     for(auto it: g[node]){
+    //         if(!vis[it]){
+    //             if(dfs(it , g , vis , pathVis , check)){
+    //                 check[node] = false;
+    //                 return true;
+    //             }
+    //         }else if(pathVis[it]){
+    //             check[node] = false;
+    //             return true;
+    //         }
+    //     }
+    //     check[node] = true;
+    //     pathVis[node] = false;
+    //     return false;
+    // }
+    // vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+    //     int n = graph.size();
+    //     vector<bool> vis(n , false);
+    //     vector<bool> pathVis(n , false);
+    //     vector<bool> check(n , false);
+    //     vector<int> ans;
+    //     for(int i=0;i<n;i++){
+    //         if(!vis[i]){
+    //             dfs(i , graph , vis , pathVis, check);
+    //         }
+    //     }
+    //     for(int i=0;i<n;i++){
+    //         if(check[i]){
+    //             ans.push_back(i);
+    //         }
+    //     }
+    //     return ans;
+    // }
+    
+    
+    
+    // Now using topological sort using Kahn's algo (BFS)
+    // For this question, reverse all the edges
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<bool> vis(n , false);
-        vector<bool> pathVis(n , false);
-        vector<bool> check(n , false);
+        vector<int> g[n];
+        for(int i=0;i<n;i++){
+            for(auto it: graph[i]){
+                g[it].push_back(i);
+            }
+        }
+        vector<int> indegree(n , 0);
+        for(int i=0;i<n;i++){
+            for(auto it: g[i]){
+                indegree[it]++;
+            }
+        }
+        queue<int> q;
+        for(int i=0;i<n;i++){
+            if(indegree[i] == 0)
+                q.push(i);
+        }
         vector<int> ans;
-        for(int i=0;i<n;i++){
-            if(!vis[i]){
-                dfs(i , graph , vis , pathVis, check);
+        while(!q.empty()){
+            auto top = q.front();
+            q.pop();
+            ans.push_back(top);
+            for(auto it: g[top]){
+                indegree[it]--;
+                if(indegree[it] == 0)
+                    q.push(it);
             }
         }
-        for(int i=0;i<n;i++){
-            if(check[i]){
-                ans.push_back(i);
-            }
-        }
+        sort(ans.begin() , ans.end());
         return ans;
     }
 };
