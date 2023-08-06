e2e7ece4123a29fe0c26b655f2e370e3d1a6594f
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-08-06 09:44:42 +0530

    Time: 106 ms (99.86%), Space: 46.9 MB (89.09%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,52 +1,40 @@
 class Solution {
 public:
-    // void dfs(vector<bool> &vis , vector<int>& ans , int node, vector<vector<int>>& graph){
-    //     if(vis[node]){
-    //         return;
-    //     }
-    //     vis[node] = true;
-    //     possible[node] = false;
-    //     for(auto it: graph[node]){
-    //         dfs(vis , ans, it, graph);
-    //     }
-    //     possible[node] = true;
-    // }
-    // vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-    //     int n = graph.size();
-    //     vector<int> ans;
-    //     vector<bool> vis(n , false);
-    //     vector<bool> possible(n , false);
-    //     for(int i=1;i<n;i++){
-    //         if(vis[i] == false){
-    //             dfs(vis , ans , i, graph);
-    //         }
-    //     }
-    //     sort(ans.begin() , ans.end());
-    //     return ans;
-    // }
-    vector<int> eventualSafeNodes(vector<vector<int>>& G) {
-    int N = G.size();
-    vector<vector<int>> R(N);
-    vector<int> outdegree(N), safe(N), ans;
-    queue<int> q;
-    for (int i = 0; i < N; ++i) {
-        for (int v : G[i]) {
-            R[v].push_back(i);
+    bool dfs(int node, vector<vector<int>>&g, vector<bool>&vis, vector<bool>&pathVis, vector<bool>&check){
+        vis[node] = true;
+        pathVis[node] = true;
+        check[node] = false;
+        for(auto it: g[node]){
+            if(!vis[it]){
+                if(dfs(it , g , vis , pathVis , check)){
+                    check[node] = false;
+                    return true;
+                }
+            }else if(pathVis[it]){
+                check[node] = false;
+                return true;
+            }
         }
-        outdegree[i] = G[i].size();
-        if (outdegree[i] == 0) q.push(i);
+        check[node] = true;
+        pathVis[node] = false;
+        return false;
     }
-    while (q.size()) {
-        int u = q.front();
-        q.pop();
-        safe[u] = 1;
-        for (int v : R[u]) {
-            if (--outdegree[v] == 0) q.push(v);
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<bool> vis(n , false);
+        vector<bool> pathVis(n , false);
+        vector<bool> check(n , false);
+        vector<int> ans;
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                dfs(i , graph , vis , pathVis, check);
+            }
         }
+        for(int i=0;i<n;i++){
+            if(check[i]){
+                ans.push_back(i);
+            }
+        }
+        return ans;
     }
-    for (int i = 0; i < N; ++i) {
-        if (safe[i]) ans.push_back(i);
-    }
-    return ans;
-}
 };
