413a5146e5290170aad82fc2c8499df35542af85
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-08-23 17:35:42 +0530

    Time: 118 ms (73.47%), Space: 55.3 MB (58.52%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.cpp
+++ 0706-design-hashmap/0706-design-hashmap.cpp
@@ -1,20 +1,76 @@
 class MyHashMap {
 public:
-    vector<int> v;
+//     vector<int> v;
+//     MyHashMap() {
+//         v.resize(1e6+1 , -1);
+//     }
+    
+//     void put(int key, int value) {
+//         v[key] = value;
+//     }
+    
+//     int get(int key) {
+//         return v[key];
+//     }
+    
+//     void remove(int key) {
+//         v[key] = -1;
+//     }
+    
+    
+    
+    class Node{
+        public:
+        int val;
+        Node* next;
+        Node* prev;
+        Node(int v){
+            val = v;
+            next = NULL;
+            prev = NULL;
+        }
+    };
+    
+    unordered_map<int, Node*> mp;
+    Node* head;
     MyHashMap() {
-        v.resize(1e6+1 , -1);
+        head = NULL;
     }
     
     void put(int key, int value) {
-        v[key] = value;
+        if(mp.find(key) == mp.end()){
+            Node* temp = head;
+            Node* nn = new Node(value);
+            nn->next = head;
+            if(temp)
+            temp->prev = nn;
+            head = temp;
+            mp[key] = nn;
+        }else{
+            Node* temp = mp[key];
+            temp->val = value;
+        }
     }
     
     int get(int key) {
-        return v[key];
+        if(mp.find(key) != mp.end()){
+            return mp[key]->val;
+        }
+        return -1;
     }
     
     void remove(int key) {
-        v[key] = -1;
+        if(mp.find(key) != mp.end()){
+            Node* temp = mp[key];
+            if(temp->prev == NULL){
+                if(head)
+                head = head->next;
+            }else{
+                temp->prev->next = temp->next;
+                temp->next->prev = temp->prev;
+            }
+            mp.erase(key);
+        }
     }
 };
 
