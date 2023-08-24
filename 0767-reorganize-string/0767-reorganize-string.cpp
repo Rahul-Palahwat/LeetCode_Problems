eb22db5d82e23fd4cfe80c0732115f109aa66fb1
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-08-24 11:34:51 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (21.42%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.cpp
+++ 0767-reorganize-string/0767-reorganize-string.cpp
@@ -1,35 +1,72 @@
 class Solution {
 public:
+    // string reorganizeString(string s) {
+    //     string ans = "";
+    //     unordered_map<char , int> mp;
+    //     for(auto it: s){
+    //         mp[it]++;
+    //     }
+    //     priority_queue<pair<int, char>> pq;
+    //     for(auto it: mp){
+    //         pq.push({it.second , it.first});
+    //     }
+    //     int sz = pq.top().first;
+    //     if(sz*2 -1 > s.size()){
+    //         return "";
+    //     }
+    //     while(!pq.empty()){
+    //         auto top = pq.top();
+    //         pq.pop();
+    //         ans+=top.second;
+    //         if(pq.size() > 0){
+    //             auto sec = pq.top();
+    //             pq.pop();
+    //             ans+=sec.second;
+    //             if(sec.first > 1){
+    //                 pq.push({sec.first-1 , sec.second});
+    //             }
+    //         }
+    //         if(top.first > 1){
+    //             pq.push({top.first-1 , top.second});
+    //         }
+    //     }
+    //     return ans;
+    // }
+    
+    
+    
+    
+    
+    // New approach O(26*N)
     string reorganizeString(string s) {
-        string ans = "";
         unordered_map<char , int> mp;
         for(auto it: s){
             mp[it]++;
         }
-        priority_queue<pair<int, char>> pq;
+        vector<pair<int, char>> v;
         for(auto it: mp){
-            pq.push({it.second , it.first});
+            v.push_back({it.second , it.first});
         }
-        int sz = pq.top().first;
-        if(sz*2 -1 > s.size()){
+        sort(v.begin() , v.end() , greater<pair<int, char>>());
+        if(v[0].first*2 -1 > s.size()){
             return "";
         }
-        while(!pq.empty()){
-            auto top = pq.top();
-            pq.pop();
-            ans+=top.second;
-            if(pq.size() > 0){
-                auto sec = pq.top();
-                pq.pop();
-                ans+=sec.second;
-                if(sec.first > 1){
-                    pq.push({sec.first-1 , sec.second});
+        string temp = s;
+        int cnt = 0 , i = 0 , j = 0;
+        while(cnt < 2){
+            while(i<s.size()){
+                if(v[j].first == 0){
+                    j++;
                 }
+                temp[i] = v[j].second;
+                // cout<<v[j].first<<"second"<<endl;
+                v[j].first -= 1;
+                // cout<<v[j].first<<"after second"<<endl;
+                i+=2;
             }
-            if(top.first > 1){
-                pq.push({top.first-1 , top.second});
-            }
+            i=1;
+            cnt++;
         }
-        return ans;
+        return temp;
     }
 };
