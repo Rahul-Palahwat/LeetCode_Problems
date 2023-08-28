6b7474ad7142c289489625e53b4213b6194e7d5b
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-08-28 11:58:41 +0530

    Time: 560 ms (86.47%), Space: 70.3 MB (72.60%) - LeetHub

--- 2035-partition-array-into-two-arrays-to-minimize-sum-difference/2035-partition-array-into-two-arrays-to-minimize-sum-difference.cpp
+++ 2035-partition-array-into-two-arrays-to-minimize-sum-difference/2035-partition-array-into-two-arrays-to-minimize-sum-difference.cpp
@@ -1,39 +1,37 @@
 class Solution {
 public:
     int minimumDifference(vector<int>& nums) {
-        int ans = 0 , n = nums.size() , sum = 0 , res = 0;
-        sum = accumulate(nums.begin() , nums.end() , 0);
-        int N = n/2;
-        vector<vector<int>> left(N+1) , right(N+1);
-        for(int mask = 0;mask < (1<<N);mask++){
-            int sz = 0 , l = 0 , r = 0;
-            for(int i=0;i<N;i++){
+        int n = nums.size();
+        int sum = accumulate(nums.begin() , nums.end() , 0);
+        int sz = n/2;
+        // vector<int> left[sz+1] , right[sz+1];
+        vector<vector<int>> left(sz+1) , right(sz+1);
+        for(int mask = 0;mask < (1<<sz) ; mask++){
+            int cnt = 0 , l = 0 , r = 0;
+            for(int i=0;i<sz;i++){
                 if(mask & (1<<i)){
-                    sz++;
+                    cnt++;
                     l+=nums[i];
-                    r+=nums[i+N];
+                    r+=nums[sz+i];
                 }
             }
-            left[sz].push_back(l);
-            right[sz].push_back(r);
+            left[cnt].push_back(l);
+            right[cnt].push_back(r);
         }
-        
-        for(int i=0;i<=N;i++){
+        for(int i=0;i<=sz;i++){
             sort(right[i].begin() , right[i].end());
         }
-        
-        res = min(abs(sum - 2*left[N][0]) , abs(sum - 2*right[N][0]));
-        
-        for(int sz = 1;sz<N;sz++){
-            for(auto &it: left[sz]){
-                auto &v = right[N-sz];
+        int ans = min(abs(sum - 2*left[sz][0]) , abs(sum - 2*right[sz][0]));
+        for(int i=1;i<sz;i++){
+            for(auto &it: left[i]){
+                auto &temp = right[sz-i];
                 int find = (sum - 2*it)/2;
-                auto itr = lower_bound(v.begin() , v.end() , find);
-                if(itr != v.end()){
-                    res = min(res , abs(sum - 2*(it+(*itr))));
+                auto itr = lower_bound(temp.begin() , temp.end() , find);
+                if(itr != temp.end()){
+                    ans = min(ans , abs(sum - 2*(it+(*itr))));
                 }
             }
         }
-        return res;
+        return ans;
     }
 };
