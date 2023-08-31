cbafb64468ae48abc798503c7d537b31a48d5333
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-08-31 22:25:48 +0530

    Time: 28 ms (82.96%), Space: 18.9 MB (72.14%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+    vector<vector<int>> merge(vector<vector<int>>& in) {
+        sort(in.begin() , in.end());
+        int n = in.size();
         vector<vector<int>> ans;
-        sort(intervals.begin(),intervals.end());
-        int start=intervals[0][0],end=intervals[0][1];
-        for(int i=0;i<intervals.size()-1;i++){
-            // if(i<intervals.size()-1)
-            if(end < intervals[i+1][0]){
-                // cout<<end<<"end"<<endl;
-                vector<int> temp;
-                end = max(intervals[i][1],end);
-                temp.push_back(start);
-                temp.push_back(end);
-                ans.push_back(temp);
-                start=intervals[i+1][0];
-                end = intervals[i+1][1];
+        int start = in[0][0] , end = in[0][1];
+        for(int i=1;i<n;i++){
+            if(end < in[i][0]){
+                ans.push_back({start , end});
+                start = in[i][0];
+                end = in[i][1];
+            }else{
+                end = max(end , in[i][1]);
             }
-            
-            end = max(intervals[i+1][1],end);
-            end = max(intervals[i][1],end);
         }
-        vector<int> temp;
-        temp.push_back(start);
-        temp.push_back(end);
-        ans.push_back(temp);
+        ans.push_back({start , end});
         return ans;
     }
 };
