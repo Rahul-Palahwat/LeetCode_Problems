99e27d5661825eda57b29223d338d112388222ad
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-08-31 12:16:52 +0530

    Time: 198 ms (80.86%), Space: 134.6 MB (72.85%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,26 +1,60 @@
 class Solution {
 public:
+    // vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+    //     vector<int> ans;
+    //     int n = nums.size();
+    //     map<int,int, greater<int>> mp;
+    //     int i=0;
+    //     while(i<k){
+    //         mp[nums[i]]++;
+    //         i++;
+    //     }
+    //     int j=0;
+    //     int maxi = 0;
+    //     maxi = (*mp.begin()).first;
+    //     ans.push_back(maxi);
+    //     while(i<n){
+    //         mp[nums[i]]++;
+    //         mp[nums[j]]--;
+    //         if(mp[nums[j]] == 0){
+    //             mp.erase(nums[j]);
+    //         }
+    //         maxi = (*mp.begin()).first;
+    //         ans.push_back(maxi);
+    //         i++;
+    //         j++;
+    //     }
+    //     return ans;
+    // }
+    
+    
+    
+    // Using dequeue
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         vector<int> ans;
         int n = nums.size();
-        map<int,int, greater<int>> mp;
-        int i=0;
+        deque<int> dq;
+        int i=0 , j = 0;
         while(i<k){
-            mp[nums[i]]++;
+            while(!dq.empty() && nums[i] > dq.back()){
+                dq.pop_back();
+            }
+            dq.push_back(nums[i]);
             i++;
         }
-        int j=0;
-        int maxi = 0;
-        maxi = (*mp.begin()).first;
-        ans.push_back(maxi);
+        ans.push_back(dq.front());
         while(i<n){
-            mp[nums[i]]++;
-            mp[nums[j]]--;
-            if(mp[nums[j]] == 0){
-                mp.erase(nums[j]);
+            if(dq.front() == nums[j]){
+                dq.pop_front();
             }
-            maxi = (*mp.begin()).first;
-            ans.push_back(maxi);
+            while(!dq.empty() && nums[i] > dq.back()){
+                dq.pop_back();
+            }
+            dq.push_back(nums[i]);
+            // while(!dq.empty() && dq.front() < dq.back()){
+            //     dq.pop_front();
+            // }
+            ans.push_back(dq.front());
             i++;
             j++;
         }
