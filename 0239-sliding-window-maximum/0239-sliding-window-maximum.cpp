480565a164b2dc90b02b6961ea71b8f2a63cbe83
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-08-31 11:00:17 +0530

    Time: 529 ms (11.73%), Space: 175.5 MB (16.62%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,47 +1,28 @@
 class Solution {
 public:
-    
-    // using map
-//     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-//         vector<int> ans;
-//         map<int,int , greater<int>> mp;
-//         int j=0,i=0;
-//         for(i=0;i<k;i++){
-//             mp[nums[i]]++;
-//         }
-        
-//         ans.push_back(mp.begin()->first);
-//         while(i<nums.size()){
-//             mp[nums[i]]++;
-//             mp[nums[j]]--;
-//             if(mp[nums[j]] == 0){
-//                 mp.erase(nums[j]);
-//             }
-//             ans.push_back(mp.begin()->first);
-//             i++;
-//             j++;
-//         }
-//         return ans;
-//     }
-    
-    
-    // using queue and dequeue
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        deque<int> dq;
         vector<int> ans;
-        for(int i=0;i<nums.size();i++){
-            // to remove out of bound elements
-            if(!dq.empty() && dq.front() == i-k){
-                dq.pop_front();
+        int n = nums.size();
+        map<int,int, greater<int>> mp;
+        int i=0;
+        while(i<k){
+            mp[nums[i]]++;
+            i++;
+        }
+        int j=0;
+        int maxi = 0;
+        maxi = (*mp.begin()).first;
+        ans.push_back(maxi);
+        while(i<n){
+            mp[nums[i]]++;
+            mp[nums[j]]--;
+            if(mp[nums[j]] == 0){
+                mp.erase(nums[j]);
             }
-            // remove all elements which are smaller than nums[i]
-            while(!dq.empty() && nums[dq.back()] <= nums[i]){
-                dq.pop_back();
-            }
-            dq.push_back(i);
-            if(i>=k-1){
-                ans.push_back(nums[dq.front()]);
-            }
+            maxi = (*mp.begin()).first;
+            ans.push_back(maxi);
+            i++;
+            j++;
         }
         return ans;
     }
