eb1d71bf555b3446fc030627509c5d38c7f8ce19
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-09-05 15:02:06 +0530

    Time: 4 ms (31.55%), Space: 8.5 MB (50.92%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -11,25 +11,59 @@
  */
 class Solution {
 public:
+    // vector<int> postorderTraversal(TreeNode* root) {
+    //     vector<int> ans;
+    //     if(!root){
+    //         return ans;
+    //     }
+    //     stack<TreeNode*> st;
+    //     st.push(root);
+    //     while(!st.empty()){
+    //         auto top = st.top();
+    //         st.pop();
+    //         ans.push_back(top->val);
+    //         if(top->left != NULL){
+    //             st.push(top->left);
+    //         }
+    //         if(top->right != NULL){
+    //             st.push(top->right);
+    //         }
+    //     }
+    //     reverse(ans.begin(), ans.end());
+    //     return ans;
+    // }
+    
+    
+    
+    
+    // All traversal in one go
     vector<int> postorderTraversal(TreeNode* root) {
-        vector<int> ans;
+        vector<int> ans[3];
         if(!root){
-            return ans;
+            return ans[2];
         }
-        stack<TreeNode*> st;
-        st.push(root);
+        // ans[0] -> PreOrder
+        // ans[1] -> InOrder
+        // ans[2] -> PostOrder
+        stack<pair<TreeNode* , int>> st;
+        st.push({root , 1});
         while(!st.empty()){
             auto top = st.top();
             st.pop();
-            ans.push_back(top->val);
-            if(top->left != NULL){
-                st.push(top->left);
-            }
-            if(top->right != NULL){
-                st.push(top->right);
+            if(top.second == 1){
+                ans[0].push_back(top.first->val);
+                st.push({top.first , 2});
+                if(top.first->left)
+                    st.push({top.first->left , 1});
+            }else if(top.second == 2){
+                ans[1].push_back(top.first->val);
+                st.push({top.first , 3});
+                if(top.first->right)
+                    st.push({top.first->right , 1});
+            }else{
+                ans[2].push_back(top.first->val);
             }
         }
-        reverse(ans.begin(), ans.end());
-        return ans;
+        return ans[2];
     }
 };
