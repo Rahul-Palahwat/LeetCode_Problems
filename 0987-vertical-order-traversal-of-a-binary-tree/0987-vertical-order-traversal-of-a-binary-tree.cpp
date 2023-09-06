e9a2d982bc08dc62a77b75c08e7bdfee1360711c
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-09-06 10:26:00 +0530

    Time: 0 ms (100.00%), Space: 12 MB (95.48%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -12,39 +12,36 @@
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        map<int , vector<int>> mp;
         vector<vector<int>> ans;
-        if(!root){
-            return ans;
-        }
+        map<int, vector<pair<int,int>>> mp;
         queue<pair<TreeNode* , int>> q;
-        q.push({root , 0});
+        TreeNode* node = root;
+        q.push({node , 0});
+        int level = 0;
         while(!q.empty()){
-            int t = q.size();
-            unordered_map<int,vector<int>> tmap;
-            while(t--){
-                TreeNode* node = q.front().first;
-                int pos = q.front().second;
-                // cout<<pos<<" "<<node->val<<endl;
+            int sz = q.size();
+            while(sz--){
+                auto top = q.front();
                 q.pop();
-                tmap[pos].push_back(node->val);
-                if(node->left){
-                    q.push({node->left , pos-1});
+                TreeNode *temp = top.first;
+                int pos = top.second;
+                mp[pos].push_back({level ,temp->val});
+                if(temp->left){
+                    q.push({temp->left , pos-1});
                 }
-                if(node->right){
-                    q.push({node->right , pos+1});
-                }
-            }
-            for(auto it: tmap){
-                sort(it.second.begin() , it.second.end());
-                for(auto i: it.second){
-                    mp[it.first].push_back(i);
+                if(temp->right){
+                    q.push({temp->right , pos+1});
                 }
             }
+            level++;
         }
-        for(auto it: mp){
-            // sort(it.second.begin() , it.second.end());
-            ans.push_back(it.second);
+        for(auto &it: mp){
+            sort(it.second.begin() , it.second.end());
+            vector<int> temp;
+            for(auto i: it.second){
+                temp.push_back(i.second);
+            }
+            ans.push_back(temp);
         }
         return ans;
     }
