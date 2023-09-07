da93b143fee307e549bc5d20491b07a373bc362b
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-09-07 17:16:06 +0530

    Time: 31 ms (39.57%), Space: 30.8 MB (84.55%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -11,42 +11,37 @@
  */
 class Solution {
 public:
-    // brute force 
-    // int countNodes(TreeNode* root) {
-    //     if(!root){
-    //         return 0;
-    //     }
-    //     if(root->left){
-    //         return 1+countNodes(root->left)+countNodes(root->right);
-    //     }else{
-    //         return 1;
-    //     }
-    // }
-    int ans=0;
-    int level=0;
-    int solveRight(TreeNode* root){
+    int findLeftHeight(TreeNode* root){
         if(!root){
             return 0;
         }
-        return 1+solveRight(root->right);
+        int ans =0;
+        while(root){
+            root = root->left;
+            ans++;
+        }
+        return ans;
     }
-    int solveLeft(TreeNode* root){
+    int findRightHeight(TreeNode* root){
         if(!root){
             return 0;
         }
-        return 1+solveLeft(root->left);
+        int ans = 0;
+        while(root){
+            root = root->right;
+            ans++;
+        }
+        return ans;
     }
-    
     int countNodes(TreeNode* root) {
         if(!root){
             return 0;
         }
-        int left = solveLeft(root->left);
-        int right = solveRight(root->right);
-        // cout<<left<<right<<endl;
-        if(left == right){
-            // cout<<(pow(2,left))*2 - 1<<"ANs"<<endl;
-            return (pow(2,left))*2 - 1;
+        int ans = 0;
+        int l = findLeftHeight(root->left);
+        int r = findRightHeight(root->right);
+        if(l==r){
+            return 1+2*((1<<l)-1);
         }else{
             return 1+countNodes(root->left)+countNodes(root->right);
         }
