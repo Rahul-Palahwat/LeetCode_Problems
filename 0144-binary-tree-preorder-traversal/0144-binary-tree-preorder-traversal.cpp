543105814298e0a269ebbc185c0298e515f37957
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-09-08 11:51:25 +0530

    Time: 4 ms (31.67%), Space: 8.3 MB (66.52%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -11,20 +11,53 @@
  */
 class Solution {
 public:
+    
+    // using stack Iterative
+    // vector<int> preorderTraversal(TreeNode* root) {
+    //     vector<int> ans;
+    //     stack<TreeNode*> st;
+    //     st.push(root);
+    //     while(!st.empty()){
+    //         auto t = st.top();
+    //         st.pop();
+    //         if(t == NULL){
+    //             continue;
+    //         }
+    //         ans.push_back(t->val);
+    //         st.push(t->right);
+    //         st.push(t->left);
+    //     }
+    //     return ans;
+    // }
+    
+    
+    
+    
+    
+    // Morris traversal
     vector<int> preorderTraversal(TreeNode* root) {
-        vector<int> ans;
-        stack<TreeNode*> st;
-        st.push(root);
-        while(!st.empty()){
-            auto t = st.top();
-            st.pop();
-            if(t == NULL){
-                continue;
+        vector<int> preorder;
+        TreeNode *curr = root;
+        while(curr){
+            if(curr->left == NULL){
+                preorder.push_back(curr->val);
+                curr = curr->right;
+            }else{
+                TreeNode *prev = curr->left;
+                while(prev->right && prev->right != curr){
+                    prev = prev->right;
+                }
+                
+                if(prev->right == NULL){
+                    prev->right = curr;
+                    preorder.push_back(curr->val);
+                    curr = curr->left;
+                }else{
+                    prev->right = NULL;
+                    curr = curr->right;
+                }
             }
-            ans.push_back(t->val);
-            st.push(t->right);
-            st.push(t->left);
         }
-        return ans;
+        return preorder;
     }
 };
