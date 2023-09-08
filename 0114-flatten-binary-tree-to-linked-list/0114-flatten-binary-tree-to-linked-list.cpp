a6d30c14c30158bd753d1b3982f8e2dc88ea42ec
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-09-08 12:50:47 +0530

    Time: 6 ms (48.78%), Space: 12.8 MB (33.56%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -11,28 +11,51 @@
  */
 class Solution {
 public:
+    
     void flatten(TreeNode* root) {
         if(!root){
             return;
         }
-        stack<TreeNode*> st;
-        st.push(root);
-        while(!st.empty()){
-            auto top = st.top();
-            st.pop();
-            if(top->right){
-                st.push(top->right);
-            }
-            if(top->left){
-                st.push(top->left);
-            }
-            top->left = NULL;
-            if(st.size() > 0){
-                top->right = st.top();
-            }else{
-                top->right = NULL;
-            }
+        flatten(root->left);
+        flatten(root->right);
+        TreeNode *temp = root;
+        TreeNode *left = root->left, *right = root->right;
+        temp->left = NULL;
+        temp->right = left;
+        while(temp->right){
+            temp = temp->right;
         }
+        temp->right = right;
+        
         return;
     }
+    
+    
+    
+    
+    // Iterative approach using stack i.e preorder traversal
+    // void flatten(TreeNode* root) {
+    //     if(!root){
+    //         return;
+    //     }
+    //     stack<TreeNode*> st;
+    //     st.push(root);
+    //     while(!st.empty()){
+    //         auto top = st.top();
+    //         st.pop();
+    //         if(top->right){
+    //             st.push(top->right);
+    //         }
+    //         if(top->left){
+    //             st.push(top->left);
+    //         }
+    //         top->left = NULL;
+    //         if(st.size() > 0){
+    //             top->right = st.top();
+    //         }else{
+    //             top->right = NULL;
+    //         }
+    //     }
+    //     return;
+    // }
 };
