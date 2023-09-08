ba02d22f0099a7333805c2d7ef4cba00b169f931
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-09-09 00:08:32 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (49.34%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -2,17 +2,24 @@ class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> ans;
+        // Loop to insert numRows vector's in ans
         for(int i=0;i<numRows;i++){
+            // Creating a temporary vector to store value for that row
             vector<int> temp;
+            // Starting value to be 1
             temp.push_back(1);
+            // Base case when there is no previous row
             if(i == 0){
                 ans.push_back(temp);
                 continue;
             }
+            // Inserting data to temp from prev temp
             for(int j=0;j<ans[i-1].size()-1;j++){
                 temp.push_back(ans[i-1][j]+ans[i-1][j+1]);
             }
+            // Ending value to be 1
             temp.push_back(1);
+            // Inserting temp vector in ans
             ans.push_back(temp);
         }
         return ans;
