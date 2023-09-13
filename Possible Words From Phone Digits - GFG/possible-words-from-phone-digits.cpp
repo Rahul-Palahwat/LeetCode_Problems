634a503995cff3c144fb8b854b6674faa35d7d82
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-09-13 12:41:12 +0530

    Added solution - LeetHub

---
+++ Possible Words From Phone Digits - GFG/possible-words-from-phone-digits.cpp
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+#include <string>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    //Function to find list of all words possible by pressing given numbers.
+    vector<string> ans;
+    map<int, string> mp;
+    void initialize(){
+        mp[2] = "abc";
+        mp[3] = "def";
+        mp[4] = "ghi";
+        mp[5] = "jkl";
+        mp[6] = "mno";
+        mp[7] = "pqrs";
+        mp[8] = "tuv";
+        mp[9] = "wxyz";
+    }
+    void solve(int a[], int index, int n, string temp){
+        if(index >= n){
+            ans.push_back(temp);
+            return;
+        }
+        for(auto it : mp[a[index]]){
+            solve(a , index+1 , n , temp+it);
+        }
+        return;
+    }
+    vector<string> possibleWords(int a[], int N)
+    {
+        //Your code here
+        string temp="";
+        initialize();
+        solve(a , 0 , N , temp);
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+  
+	int T;
+	
+	cin >> T; //testcases
+	
+	while(T--){ //while testcases exist
+	   int N;
+	    
+	   cin >> N; //input size of array
+	   
+	   int a[N]; //declare the array
+	   
+	   for(int i =0;i<N;i++){
+	       cin >> a[i]; //input the elements of array that are keys to be pressed
+	   }
+	   
+	   Solution obj;
+	   
+	  vector <string> res = obj.possibleWords(a,N);
+	  for (string i : res) cout << i << " ";
+	   cout << endl;
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
