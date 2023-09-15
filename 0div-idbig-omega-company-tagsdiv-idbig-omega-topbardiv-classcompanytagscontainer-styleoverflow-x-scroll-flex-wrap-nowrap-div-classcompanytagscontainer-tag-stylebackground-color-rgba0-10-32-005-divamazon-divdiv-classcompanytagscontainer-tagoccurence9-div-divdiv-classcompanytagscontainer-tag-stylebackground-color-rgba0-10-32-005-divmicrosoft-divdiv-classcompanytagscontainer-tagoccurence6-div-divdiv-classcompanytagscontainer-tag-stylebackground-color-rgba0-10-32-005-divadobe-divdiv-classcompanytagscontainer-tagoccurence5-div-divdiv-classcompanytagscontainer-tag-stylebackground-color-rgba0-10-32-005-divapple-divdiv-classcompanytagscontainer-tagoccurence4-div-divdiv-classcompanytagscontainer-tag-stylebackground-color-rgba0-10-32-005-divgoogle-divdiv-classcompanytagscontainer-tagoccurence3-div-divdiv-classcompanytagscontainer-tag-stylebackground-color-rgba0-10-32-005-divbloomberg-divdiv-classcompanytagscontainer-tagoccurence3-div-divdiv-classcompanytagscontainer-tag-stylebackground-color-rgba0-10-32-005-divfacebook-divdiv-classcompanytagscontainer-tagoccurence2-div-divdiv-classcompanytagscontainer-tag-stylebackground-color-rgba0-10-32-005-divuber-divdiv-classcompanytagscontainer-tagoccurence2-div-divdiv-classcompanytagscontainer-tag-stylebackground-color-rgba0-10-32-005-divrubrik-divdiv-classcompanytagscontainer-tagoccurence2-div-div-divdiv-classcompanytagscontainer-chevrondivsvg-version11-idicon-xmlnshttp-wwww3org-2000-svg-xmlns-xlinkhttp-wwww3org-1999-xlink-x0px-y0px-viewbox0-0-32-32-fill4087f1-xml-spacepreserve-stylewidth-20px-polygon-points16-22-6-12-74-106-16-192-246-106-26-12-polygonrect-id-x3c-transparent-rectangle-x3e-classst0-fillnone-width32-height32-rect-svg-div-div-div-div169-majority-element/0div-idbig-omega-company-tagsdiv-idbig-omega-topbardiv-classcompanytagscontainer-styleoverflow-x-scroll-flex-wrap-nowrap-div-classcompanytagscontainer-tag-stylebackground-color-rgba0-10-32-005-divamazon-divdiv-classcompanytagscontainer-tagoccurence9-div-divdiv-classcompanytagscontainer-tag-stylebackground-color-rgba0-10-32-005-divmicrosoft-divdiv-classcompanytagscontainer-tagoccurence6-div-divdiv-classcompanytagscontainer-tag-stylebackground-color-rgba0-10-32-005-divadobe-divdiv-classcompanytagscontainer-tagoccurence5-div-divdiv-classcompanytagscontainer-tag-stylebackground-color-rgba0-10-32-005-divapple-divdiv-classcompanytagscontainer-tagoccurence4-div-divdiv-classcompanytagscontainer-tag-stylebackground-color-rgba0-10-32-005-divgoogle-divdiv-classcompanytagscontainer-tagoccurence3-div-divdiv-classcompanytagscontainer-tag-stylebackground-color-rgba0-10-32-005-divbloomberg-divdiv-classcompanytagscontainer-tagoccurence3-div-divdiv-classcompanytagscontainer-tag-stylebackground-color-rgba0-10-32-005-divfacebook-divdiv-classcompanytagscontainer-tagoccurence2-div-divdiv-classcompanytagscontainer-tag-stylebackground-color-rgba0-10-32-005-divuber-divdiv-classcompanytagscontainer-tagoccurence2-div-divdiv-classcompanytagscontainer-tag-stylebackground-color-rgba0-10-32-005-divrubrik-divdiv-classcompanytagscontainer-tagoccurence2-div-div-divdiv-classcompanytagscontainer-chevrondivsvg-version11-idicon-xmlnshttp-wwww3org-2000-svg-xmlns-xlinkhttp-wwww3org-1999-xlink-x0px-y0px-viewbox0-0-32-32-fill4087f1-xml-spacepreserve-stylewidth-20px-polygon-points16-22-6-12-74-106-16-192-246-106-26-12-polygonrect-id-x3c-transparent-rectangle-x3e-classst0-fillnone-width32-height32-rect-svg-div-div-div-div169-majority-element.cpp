ebb4e33b6acee9576164d5c9cd5d61ee68a5cd54
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-09-15 20:41:29 +0530

    Time: 7 ms (98.00%), Space: 19.9 MB (8.83%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence9-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divadobe-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divrubrik-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div169-majority-element/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence9-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divadobe-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divrubrik-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div169-majority-element.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    // Bruteforce TC -> O(N^2)
+    // int majorityElement(vector<int>& nums) {
+    //     int cnt = 0;
+    //     int n = nums.size();
+    //     for(int i=0;i<n;i++){
+    //         cnt=1;
+    //         for(int j=i+1;j<n;j++){
+    //             if(nums[i] == nums[j]){
+    //                 cnt++;
+    //             }
+    //         }
+    //         if(cnt > n/2){
+    //             return nums[i];
+    //         }
+    //     }
+    //     return -1;
+    // }
+    
+    
+    // HashMap TC-> O(NlogN) , SC-> O(N)
+    // int majorityElement(vector<int>& nums) {
+    //     int n = nums.size();
+    //     map<int,int> mp;
+    //     for(auto it: nums){
+    //         mp[it]++;
+    //     }
+    //     for(auto it: mp){
+    //         if(it.second > n/2){
+    //             return it.first;
+    //         }
+    //     }
+    //     return -1;
+    // }
+    
+    
+    // Using Moore's Voting Algorithm
+    int majorityElement(vector<int>& nums) {
+        int cnt = 0;
+        int n = nums.size();
+        int ele;
+        for(int i=0;i<n;i++){
+            if(cnt == 0){
+                ele = nums[i];
+                cnt++;
+            }else if(nums[i] != ele){
+                cnt--;
+            }else{
+                cnt++;
+            }
+        }
+        cnt = 0;
+        for(auto it: nums){
+            if(it == ele){
+                cnt++;
+            }
+        }
+        if(cnt > n/2){
+            return ele;
+        }
+        return -1;
+    }
+        
+};
