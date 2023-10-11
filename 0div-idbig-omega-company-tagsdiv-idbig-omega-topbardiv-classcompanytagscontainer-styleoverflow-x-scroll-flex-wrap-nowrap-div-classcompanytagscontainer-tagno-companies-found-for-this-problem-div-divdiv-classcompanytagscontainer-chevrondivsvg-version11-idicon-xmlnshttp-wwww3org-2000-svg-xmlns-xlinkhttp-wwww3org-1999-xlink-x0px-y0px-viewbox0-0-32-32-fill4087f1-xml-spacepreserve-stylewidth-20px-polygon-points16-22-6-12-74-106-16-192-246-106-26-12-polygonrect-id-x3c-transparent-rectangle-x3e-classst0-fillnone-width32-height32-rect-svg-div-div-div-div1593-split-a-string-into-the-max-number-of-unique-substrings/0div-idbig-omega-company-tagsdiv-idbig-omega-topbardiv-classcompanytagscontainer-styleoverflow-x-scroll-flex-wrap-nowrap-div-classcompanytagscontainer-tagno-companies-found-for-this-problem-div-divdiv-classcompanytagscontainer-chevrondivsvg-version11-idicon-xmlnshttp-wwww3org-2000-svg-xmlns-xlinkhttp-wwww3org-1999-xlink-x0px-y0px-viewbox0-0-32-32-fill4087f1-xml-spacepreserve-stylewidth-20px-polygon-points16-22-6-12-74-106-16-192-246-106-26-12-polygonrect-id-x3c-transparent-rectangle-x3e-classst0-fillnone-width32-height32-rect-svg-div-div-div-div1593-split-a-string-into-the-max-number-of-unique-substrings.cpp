d233081c745007bd9e2e474711f63148c42e27d4
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-10-11 13:36:02 +0530

    Time: 2097 ms (5.23%), Space: 495.1 MB (5.23%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1593-split-a-string-into-the-max-number-of-unique-substrings/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1593-split-a-string-into-the-max-number-of-unique-substrings.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int ans = 1;
+    void solve(int index , string &s, unordered_map<string, int> mp){
+        int n = s.size();
+        if(index >= n){
+            int sz = mp.size();
+            ans = max(ans , sz);
+            return;
+        }
+        string temp="";
+        for(int i=index;i<n;i++){
+            temp+=s[i];
+            mp[temp]++;
+            solve(i+1 , s , mp);
+            mp[temp]--;
+            if(mp[temp] == 0){
+                mp.erase(temp);
+            }
+            // temp.pop_back();
+        }
+        return;
+    }
+    int maxUniqueSplit(string s) {
+        unordered_map<string , int> mp;
+        string temp="";
+        solve(0 , s , mp);
+        return ans;
+    }
+};
