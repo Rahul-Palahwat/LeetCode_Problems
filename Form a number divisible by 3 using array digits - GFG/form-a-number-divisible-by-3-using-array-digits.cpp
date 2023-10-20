2b885784b821d5103a01096da3cff6aeee42366b
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-10-20 19:42:32 +0530

    Added solution - LeetHub

---
+++ Form a number divisible by 3 using array digits - GFG/form-a-number-divisible-by-3-using-array-digits.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+  int sumDigit(int d){
+      int sum = 0;
+      while(d > 0){
+          sum+=(d%10);
+          d/=10;
+      }
+      return sum;
+  }
+    int isPossible(int N, int arr[]) {
+        // code here
+        int sum = 0;
+        for(int i=0;i<N;i++){
+            sum+=sumDigit(arr[i]);
+        }
+        return sum%3 == 0;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        int arr[N];
+        for (int i = 0; i < N; i++) cin >> arr[i];
+
+        Solution ob;
+        cout << ob.isPossible(N, arr) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
