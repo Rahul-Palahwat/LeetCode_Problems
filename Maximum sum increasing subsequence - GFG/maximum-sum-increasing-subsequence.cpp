d55cd89accc1a2f6ca784745cbb1feaf98fb94cc
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-10-23 23:46:29 +0530

    Added solution - LeetHub

--- Maximum sum increasing subsequence - GFG/maximum-sum-increasing-subsequence.cpp
+++ Maximum sum increasing subsequence - GFG/maximum-sum-increasing-subsequence.cpp
@@ -7,30 +7,52 @@ class Solution{
 		
 
 	public:
-	int solve(int index , int prev , int arr[] , vector<vector<int>> &dp){
-	    if(index <= 0){
-	        return 0;
-	    }
-	    if(dp[index][prev] != -1){
-	        return dp[index][prev];
-	    }
-	    int notTaken = solve(index-1 , prev , arr ,dp);
-	    int taken = 0;
-	    if(prev == 0){
-	        taken = arr[index-1]+solve(index-1 , index , arr , dp);
-	    }else{
-	        if(arr[index-1] < arr[prev-1]){
-	            taken = arr[index-1]+solve(index-1 , index , arr , dp);
-	        }
-	    }
-	    return dp[index][prev] = max(taken , notTaken);
-	}
-	int maxSumIS(int arr[], int n)  
-	{  
-	    // Your code goes here
-	    vector<vector<int>> dp(n+1 , vector<int>(n+1 , -1));
-	    return solve(n , 0 , arr , dp);
-	}  
+// 	int solve(int index , int curr , int arr[] , int n, vector<vector<int>> &dp){
+// 	    if(index > n){
+// 	        return 0;
+// 	    }
+// 	    if(dp[index][curr] != -1){
+// 	        return dp[index][curr];
+// 	    }
+// 	    int ans = solve(index+1 , curr , arr , n , dp);
+// 	    if(curr == 0){
+// 	        ans = max(ans , arr[index-1]+solve(index+1 , index , arr , n , dp));
+// 	    }else if(arr[index-1] > arr[curr-1]){
+// 	        ans = max(ans , arr[index-1]+solve(index+1 , index , arr , n , dp));
+// 	    }
+// 	    return dp[index][curr] = ans;
+// 	}
+// 	int maxSumIS(int arr[], int n)  
+// 	{  
+// 	    // Your code goes here
+// 	    vector<vector<int>> dp(n+1 , vector<int>(n+1 , -1));
+// 	    return solve(1 , 0 , arr , n , dp);
+// 	}
+
+
+	int maxSumIS(int arr[], int n) {
+    // vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+        vector<int> dp(n+1 , 0);
+    
+        for (int i = 1; i <= n; i++) {
+            vector<int> prev(n+1 , 0);
+            for (int j = 0; j < i; j++) {
+                prev[j] = max(prev[j], dp[j]);
+    
+                if (j == 0 || arr[i - 1] > arr[j - 1]) {
+                    prev[i] = max(prev[i], arr[i - 1] + dp[j]);
+                }
+            }
+            dp = prev;
+        }
+    
+        int maxSum = 0;
+        for (int i = 0; i <= n; i++) {
+            maxSum = max(maxSum, dp[i]);
+        }
+    
+        return maxSum;
+    }
 };
 
 //{ Driver Code Starts.
