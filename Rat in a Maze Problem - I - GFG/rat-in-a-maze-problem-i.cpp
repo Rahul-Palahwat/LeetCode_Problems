776bfc43f240592ece2d9afece6b758387ef2449
Rahul Kumar <72152598+Rahul-Palahwat@users.noreply.github.com>
2023-11-12 12:01:10 +0530

    Added solution - LeetHub

--- Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
+++ Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
@@ -10,46 +10,44 @@ using namespace std;
 
 class Solution{
     public:
-    int vis[6][6];
-    void solve(vector<vector<int>> m,int n,int i,int j,vector<string> &ans, string s){
-        if(i==n-1 && j== n-1 && m[i][j] != 0){
+    vector<string> ans;
+    void solve(int i , int j , vector<vector<int>> &m, int n, string s, vector<vector<bool>> vis){
+        if(i>=n || j>=n || i<0 || j<0){
+            return;
+        }
+        if(m[i][j] == 0){
+            return;
+        }
+        if(i == n-1 && j == n-1){
             ans.push_back(s);
             return;
         }
-        if(i<0 || j<0){
+        if(vis[i][j]){
             return;
         }
-        if(i>=n || j>=n){
-            return;
-        }
-        if(vis[i][j] == 1){
-            return;
-        }
-        if(m[i][j] ==0){
-            return;
-        }
-        vis[i][j]=1;
+        vis[i][j] = true;
         s+='D';
-        solve(m,n,i+1,j,ans,s);
+        solve(i+1 , j , m , n , s , vis);
         s.pop_back();
         s+='R';
-        solve(m,n,i,j+1,ans,s);
+        solve(i , j+1 , m , n ,s , vis);
         s.pop_back();
         s+='U';
-        solve(m,n,i-1,j,ans,s);
+        solve(i-1 , j , m , n ,s , vis);
         s.pop_back();
         s+='L';
-        solve(m,n,i,j-1,ans,s);
+        solve(i , j-1 , m , n ,s , vis);
         s.pop_back();
-        vis[i][j] = -1;
         return;
     }
     vector<string> findPath(vector<vector<int>> &m, int n) {
         // Your code goes here
-        memset(vis,-1,sizeof(vis));
-        vector<string> ans;
-        solve(m,n,0,0,ans,"");
-        // sort(ans.begin(),ans.end());
+        string s = "";
+        vector<vector<bool>> vis(n , vector<bool> (n , false));
+        solve(0 , 0 , m , n , s, vis);
+        // for(auto it: ans){
+        //     cout<<it<<" ans"<<endl;
+        // }
         return ans;
     }
 };
